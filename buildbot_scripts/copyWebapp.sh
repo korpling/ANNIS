c109534a866bbc1300d33ac48dae71f1a0b32da8
Thomas Krause <thomas@berlin-reloaded.de>
2009-04-16 16:02:00 +0200

    copyWebapp should not try to modifiy chmod of parent folder

--- buildbot_scripts/copyWebapp.sh
+++ buildbot_scripts/copyWebapp.sh
@@ -9,5 +9,5 @@ unzip -d $ANNIS_WEBAPP_HOME Annis2-web/dist/Annis2-web.war
 cp AnnisService/annis-rmi-service-1.0.jar $ANNIS_WEBAPP_HOME/WEB-INF/lib
 cp AnnisService/annis-rmi-objects-1.0.jar $ANNIS_WEBAPP_HOME/WEB-INF/lib
 
-chmod -R ug+rw $ANNIS_WEBAPP_HOME
+chmod -R ug+rw $ANNIS_WEBAPP_HOME/*
 
