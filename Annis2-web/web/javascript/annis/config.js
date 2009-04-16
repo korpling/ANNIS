cc84bafab52e2e7fa7350c05035d385d0d283d0c
Thomas Krause <thomas@berlin-reloaded.de>
2009-04-16 19:16:36 +0200

    default context changed from Annis-dev -> Annis-trunk

--- Annis2-web/web/javascript/annis/config.js
+++ Annis2-web/web/javascript/annis/config.js
@@ -1,5 +1,5 @@
 // the context path used by this installation
-var conf_context='/Annis-dev';
+var conf_context='/Annis-trunk';
 var conf_revision='${SVN_REVISION}';
 Ext.BLANK_IMAGE_URL='images/s.gif';
 Ext.SSL_SECURE_URL='https://korpling.german.hu-berlin.de' + conf_context + '/empty.jsp';
