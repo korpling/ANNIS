449cc6386bf5f501d08042897d9e9dd64e49a80b
thomas <>
2009-04-16 14:59:24 +0000

    -removing old trunk version of Annis2-web

--- Annis2-web/src/java/annis/cache/CacheException.java
+++
@@ -1,9 +0,0 @@
-package annis.cache;
-
-public class CacheException extends Exception {
-	private static final long serialVersionUID = 8238088241213816466L;
-
-	public CacheException(String msg) {
-		super(msg);
-	}
-}
--- Annis2-web/web/javascript/annis/config.js
+++
@@ -1,5 +0,0 @@
-// the context path used by this installation
-var conf_context='/Annis-dev';
-var conf_revision='${SVN_REVISION}';
-Ext.BLANK_IMAGE_URL='images/s.gif';
-Ext.SSL_SECURE_URL='https://korpling.german.hu-berlin.de' + conf_context + '/empty.jsp';
