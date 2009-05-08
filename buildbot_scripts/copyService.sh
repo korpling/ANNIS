8ab2ea30ae7053a1c9136e6348b4428cbb18d7c7
Thomas Krause <thomas@berlin-reloaded.de>
2009-05-08 13:33:03 +0200

    - buildbot: also copy bin/* and scripts/* (this checkin is also a test...)

--- buildbot_scripts/copyService.sh
+++ buildbot_scripts/copyService.sh
@@ -4,4 +4,6 @@
 export ANNIS_HOME=$1
 
 #code
-cp AnnisService/annis.jar $ANNIS_HOME/lib 
+cp AnnisService/annis.jar $ANNIS_HOME/lib/
+cp AnnisService/scripts/* $ANNIS_HOME/scripts/
+cp AnnisService/bin/* $ANNIS_HOME/bin/
