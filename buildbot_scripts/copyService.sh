30ddd109d8e9ee54fcb79386a83069a33bf35f30
Thomas Krause <thomas@berlin-reloaded.de>
2009-05-08 19:03:55 +0200

    arg...  (testing the buildbot is only possible when comitting)

--- buildbot_scripts/copyService.sh
+++ buildbot_scripts/copyService.sh
@@ -5,5 +5,5 @@ export ANNIS_HOME=$1
 
 #code
 cp AnnisService/annis.jar $ANNIS_HOME/lib/
-cp -f AnnisService/scripts/* $ANNIS_HOME/scripts/
-cp -f AnnisService/bin/* $ANNIS_HOME/bin/
+cp -Rf AnnisService/scripts/* $ANNIS_HOME/scripts/
+cp -Rf AnnisService/bin/* $ANNIS_HOME/bin/
