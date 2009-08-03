b3ee1eb2d2b889b18e5e98df7f4f544d2d45dd96
Thomas Krause <thomas@tk-laptop>
2009-08-03 13:39:33 +0200

    - install new annis service jars to maven on every build

--- addServiceJarsToMaven.sh
+++ addServiceJarsToMaven.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 mvn install:install-file\
- -Dfile=../AnnisService/annis-rmi-service-1.0.jar\
+ -Dfile=AnnisService/annis-rmi-service-1.0.jar\
  -DgroupId=de.hu_berlin.german.korpling.annis\
  -DartifactId=AnnisService\
  -Dversion=2.1-SNAPSHOT\
  -Dpackaging=jar
 
 mvn install:install-file\
- -Dfile=../AnnisService/annis-rmi-objects-1.0.jar\
+ -Dfile=AnnisService/annis-rmi-objects-1.0.jar\
  -DgroupId=de.hu_berlin.german.korpling.annis\
  -DartifactId=AnnisServiceObjects\
  -Dversion=2.1-SNAPSHOT\
--- buildbot_scripts/copyWebapp.sh
+++ buildbot_scripts/copyWebapp.sh
@@ -6,8 +6,6 @@ ANNIS_WEBAPP_HOME=$1
 #code
 rm -R $ANNIS_WEBAPP_HOME/*
 unzip -d $ANNIS_WEBAPP_HOME Annis-web/target/Annis-web.war
-cp AnnisService/annis-rmi-service-1.0.jar $ANNIS_WEBAPP_HOME/WEB-INF/lib
-cp AnnisService/annis-rmi-objects-1.0.jar $ANNIS_WEBAPP_HOME/WEB-INF/lib
 
 chmod -R ug+rw $ANNIS_WEBAPP_HOME/*
 
