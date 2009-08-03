f4a0514ec553c416dc396a3b372016fb56c4f126
Thomas Krause <thomas@tk-laptop>
2009-08-03 13:28:47 +0200

    - using correct war file in unzip for buildbot

--- buildbot_scripts/copyWebapp.sh
+++ buildbot_scripts/copyWebapp.sh
@@ -5,7 +5,7 @@ ANNIS_WEBAPP_HOME=$1
 
 #code
 rm -R $ANNIS_WEBAPP_HOME/*
-unzip -d $ANNIS_WEBAPP_HOME Annis2-web/dist/Annis2-web.war
+unzip -d $ANNIS_WEBAPP_HOME Annis-web/target/Annis-web.war
 cp AnnisService/annis-rmi-service-1.0.jar $ANNIS_WEBAPP_HOME/WEB-INF/lib
 cp AnnisService/annis-rmi-objects-1.0.jar $ANNIS_WEBAPP_HOME/WEB-INF/lib
 
