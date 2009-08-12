f4032b17691ef4301b5b6e66eb5692b2c572beb8
Thomas Krause <thomas@tk-laptop>
2009-08-12 16:19:11 +0200

    - order the elements of the graph in JUNGTreeVisualizer

--- Annis-web/src/main/java/annis/frontend/servlets/visualizers/JUNGTreeVisualizer.java
+++ Annis-web/src/main/java/annis/frontend/servlets/visualizers/JUNGTreeVisualizer.java
@@ -2,25 +2,29 @@ package annis.frontend.servlets.visualizers;
 
 import edu.uci.ics.jung.algorithms.layout.DAGLayout;
 import edu.uci.ics.jung.graph.DirectedGraph;
+import edu.uci.ics.jung.graph.DirectedOrderedSparseMultigraph;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.decorators.EdgeShape;
-import edu.uci.ics.jung.visualization.renderers.DefaultVertexLabelRenderer;
-import edu.uci.ics.jung.visualization.renderers.VertexLabelAsShapeRenderer;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Paint;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import org.apache.commons.collections15.Transformer;
 import org.jdom.Document;
 import org.jdom.Element;
+import org.jdom.JDOMException;
 import org.jdom.Namespace;
 import org.jdom.filter.ElementFilter;
+import org.jdom.xpath.XPath;
 
 /**
  *
@@ -32,27 +36,69 @@ public class JUNGTreeVisualizer extends Visualizer
   @Override
   public void writeOutput(OutputStream outstream)
   {
-
-    Document doc = getPaulaJDOM();
+    final Document doc = getPaulaJDOM();
     DirectedGraph<PaulaVertex, PaulaEdge> g = generateGraphFromPaula(doc);
 
     DAGLayout<PaulaVertex, PaulaEdge> dagLayout = new DAGLayout<PaulaVertex, PaulaEdge>(g);
+    dagLayout.initialize();
+    dagLayout.done();
 
     VisualizationViewer<PaulaVertex, PaulaEdge> vv =
       new VisualizationViewer<PaulaVertex, PaulaEdge>(dagLayout);
 
-    vv.setSize(dagLayout.getSize().width + 10, dagLayout.getSize().height + 10);
+    vv.setSize(dagLayout.getSize().width, dagLayout.getSize().height);
     vv.setBackground(Color.WHITE);
     vv.setDoubleBuffered(false);
 
     // use id as vertex label (just as a test)
     vv.getRenderContext().setVertexLabelTransformer(new Transformer<PaulaVertex, String>()
     {
+
       public String transform(PaulaVertex input)
       {
-        return Long.toString(input.id);
+        if(input.cat != null)
+        {
+          return input.cat;
+        }
+        else if(input.value != null)
+        {
+          return input.value;
+        }
+        else
+        {
+          return Long.toString(input.id);
+        }
       }
     });
+
+    final Map<String, String> markableMapFinal = this.markableMap;
+
+    vv.getRenderContext().setVertexFillPaintTransformer(new Transformer<PaulaVertex, Paint>()
+    {
+
+      public Paint transform(PaulaVertex input)
+      {
+        String idAsString = Long.toString(input.id);
+
+        if(markableMapFinal.containsKey(idAsString))
+        {
+          String markerColor = markableMapFinal.get(idAsString);
+          if("red".equalsIgnoreCase(markerColor))
+          {
+            return Color.red;
+          }
+          else
+          {
+            return Color.gray;
+          }
+        }
+        else
+        {
+          return Color.lightGray;
+        }
+      }
+    });
+
     // render edge labels
     vv.getRenderContext().setEdgeLabelTransformer(new Transformer<PaulaEdge, String>()
     {
@@ -65,7 +111,7 @@ public class JUNGTreeVisualizer extends Visualizer
     vv.getRenderContext().getEdgeLabelRenderer().setRotateEdgeLabels(false);
 
     // straight lines
-    vv.getRenderContext().setEdgeShapeTransformer(new EdgeShape.Line<PaulaVertex,PaulaEdge>());
+    vv.getRenderContext().setEdgeShapeTransformer(new EdgeShape.Line<PaulaVertex, PaulaEdge>());
 
     // create new image to paint on
     BufferedImage image = new BufferedImage(vv.getWidth(), vv.getHeight(),
@@ -104,12 +150,34 @@ public class JUNGTreeVisualizer extends Visualizer
 
   public DirectedGraph<PaulaVertex, PaulaEdge> generateGraphFromPaula(Document paula)
   {
-    DirectedGraph<PaulaVertex, PaulaEdge> g = new DirectedSparseMultigraph<PaulaVertex, PaulaEdge>();
+    DirectedGraph<PaulaVertex, PaulaEdge> g = 
+      new DirectedOrderedSparseMultigraph<PaulaVertex, PaulaEdge>();
 
     // matching namespace
     Namespace nsTree = Namespace.getNamespace(this.namespace, this.namespace);
 
-    // get all edges
+    // get all token (so that they have the right order)
+    Iterator<Element> itToken = paula.getRootElement().getDescendants(new ElementFilter("tok"));
+    while(itToken.hasNext())
+    {
+      Element e = itToken.next();
+      PaulaVertex v = new PaulaVertex();
+      String idAsString = e.getAttributeValue("_id");
+      if(idAsString != null)
+      {
+        try
+        {
+          v.id = Long.parseLong(idAsString);
+          g.addVertex(v);
+        }
+        catch(NumberFormatException ex)
+        {
+          // ignore
+        }
+      }
+    }
+
+    // get all edges (and other nodes that you may find on your way)
     Iterator<Element> itRel = paula.getRootElement().getDescendants(new ElementFilter("_rel"));
     while(itRel.hasNext())
     {
@@ -146,6 +214,26 @@ public class JUNGTreeVisualizer extends Visualizer
         {
           // ignore
         }
+      }
+    }
+
+    // find cat or value for every node
+    for(PaulaVertex v : g.getVertices())
+    {
+      try
+      {
+        // try to find element with that id
+        XPath p = XPath.newInstance("//*[@_id=" + v.id + "]");
+        Element e = (Element) p.selectSingleNode(paula);
+        if(e != null)
+        {
+          v.cat = e.getAttributeValue("cat", nsTree);
+          v.value = e.getTextTrim();
+        }
+      }
+      catch(JDOMException ex)
+      {
+        Logger.getLogger(JUNGTreeVisualizer.class.getName()).log(Level.SEVERE, null, ex);
       }
     }
 
@@ -196,6 +284,8 @@ public class JUNGTreeVisualizer extends Visualizer
   {
 
     public long id;
+    public String cat;
+    public String value;
 
     @Override
     public boolean equals(Object obj)
--- Annis-web/src/main/java/annis/frontend/servlets/visualizers/WriterVisualizer.java
+++ Annis-web/src/main/java/annis/frontend/servlets/visualizers/WriterVisualizer.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 
 /**
