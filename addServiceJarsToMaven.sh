7cebe784267bc1f74bfc5706bb0a6ab6cdc4903b
Thomas Krause <thomas@berlin-reloaded.de>
2009-08-31 18:29:38 +0200

    - using own repository for annis, by this we are able to deploy our own copy of Postgresql JDBC driver (with the copy command implemented)

--- addServiceJarsToMaven.sh
+++ addServiceJarsToMaven.sh
@@ -13,3 +13,6 @@ mvn install:install-file\
  -DartifactId=AnnisServiceObjects\
  -Dversion=2.1-SNAPSHOT\
  -Dpackaging=jar
+
+# on server for postgres
+# mvn deploy:deploy-file -Dfile=/tmp/postgresql-jdbc-8.2-505-copy-20070719.jdbc3.jar -DgroupId=postgresql -DartifactId=postgresql-copy -Dversion=8.2-505.jdbc3 -Dpackaging=jar -DgeneratePom=true -Durl=file:///home/annis/public_html/maven2/ -Did=annis
