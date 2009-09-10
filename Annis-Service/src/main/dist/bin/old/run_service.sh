4fc7b635a68fc068af44860bed90ec81a8bc97c8
Thomas Krause <thomas@berlin-reloaded.de>
2009-09-10 16:12:44 +0200

    - making the scripts executable

--- Annis-Service/src/main/dist/bin/old/run_service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-classpath="classes"
-for lib in lib/*; do
-	classpath="$classpath:$lib"
-done
-
-java -Dfile.encoding=UTF-8 -cp $classpath de.deutschdiachrondigital.dddquery.helper.Service $* 
--- Annis-Service/src/main/dist/bin/old/run_shell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-classpath="classes"
-for lib in lib/*; do
-	classpath="$classpath:$lib"
-done
-
-java -cp $classpath de.deutschdiachrondigital.dddquery.helper.Shell $*
