5788a8a7ba46a2fa753e5a4b7084c948a58878f9
Thomas Krause <thomas@berlin-reloaded.de>
2009-09-10 22:28:52 +0200

    another try...

--- buildbot_scripts/copyService.sh
+++ buildbot_scripts/copyService.sh
@@ -4,12 +4,12 @@
 export ANNIS_HOME=$1
 
 ANNIS_VERSION=2.1-SNAPSHOT
-DIST_DIR=Annis-Service/target/annis-service-$ANNIS_VERSION-distribution.dir/annis-service-$ANNIS_VERSION/
+DIST_DIR=Annis-Service/target/annis-service-$ANNIS_VERSION-distribution.dir/annis-service-$ANNIS_VERSION
 
 #code
-rm $ANNIS_HOME/lib/*
-rm $ANNIS_HOME/scripts/*
-rm $ANNIS_HOME/bin/*
+rm -f $ANNIS_HOME/lib/*
+rm -f $ANNIS_HOME/scripts/*
+rm -f $ANNIS_HOME/bin/*
 cp $DIST_DIR/lib/* $ANNIS_HOME/lib/
 cp -Rf $DIST_DIR/scripts/* $ANNIS_HOME/scripts/
 cp -Rf $DIST_DIR/bin/* $ANNIS_HOME/bin/
