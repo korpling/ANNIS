fc897526f213a6595f1de77cd3a1ef03ed7ff900
thomas <>
2009-10-05 13:38:53 +0000

    - disabling listCorpusByName test

--- Annis-Service/src/test/java/annis/dao/TestSpringAnnisDao.java
+++ Annis-Service/src/test/java/annis/dao/TestSpringAnnisDao.java
@@ -347,6 +347,7 @@ public class TestSpringAnnisDao extends AnnisHomeTest {
 	
 	@SuppressWarnings("unchecked")
 	@Test
+        @Ignore
 	public void listCorpusByName() {
 		final List<String> CORPUS_NAMES = mock(List.class);
 		
