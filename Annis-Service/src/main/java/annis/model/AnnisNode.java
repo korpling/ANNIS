7b91d8c1bf2606b82dce238b0c6129f23f8a99cc
Thomas Krause <thomas@berlin-reloaded.de>
2009-12-14 15:18:18 +0100

    - backport exact match bug solution from viktor_sa branch

--- Annis-Service/src/main/java/annis/model/AnnisNode.java
+++ Annis-Service/src/main/java/annis/model/AnnisNode.java
@@ -57,7 +57,7 @@ public class AnnisNode implements Serializable {
 	private String marker;
 
 	public enum TextMatching {
-		EXACT("~=~", "\""), REGEXP("~", "/");
+		EXACT("=", "\""), REGEXP("~", "/");
 
 		private String sqlOperator;
 		private String annisQuote;
