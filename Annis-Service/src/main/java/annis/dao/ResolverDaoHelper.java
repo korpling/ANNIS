459a0c7a139e94b3ce40aa70312db5a05a3ff3b7
richliju <>
2010-04-06 09:51:13 +0000

    Added searchstring in ResolverDaoHelper

--- Annis-Service/src/main/java/annis/dao/ResolverDaoHelper.java
+++ Annis-Service/src/main/java/annis/dao/ResolverDaoHelper.java
@@ -34,12 +34,15 @@ public class ResolverDaoHelper implements ResultSetExtractor
 
   public String createSqlQuery(long corpusId, String namespace, ResolverEntry.ElementType type)
   {
-    StringBuilder result = new StringBuilder();
+    //StringBuilder result = new StringBuilder();
 
+    String select = "SELECT corpus.id, corpus.name, corpus.version, resolver_vis_map.namespace, resolver_vis_map.element, resolver_vis_map.vis_type, resolver_vis_map.display_name,resolver_vis_map.order, resolver_vis_map.mappings";
+    String result = select +" FROM resolver_vis_map, corpus WHERE corpus.id = "+ corpusId +" AND corpus is NULL AND namespace is NULL AND element is NULL UNION "+select+" FROM resolver_vis_map  LEFT OUTER JOIN corpus ON (resolver_vis_map.corpus = corpus.name) WHERE corpus is NULL AND namespace = '"+namespace+"' AND namespace NOT IN (SELECT namespace FROM resolver_vis_map WHERE namespace = '"+namespace+"' AND corpus IS NOT NULL) UNION "+select+" FROM resolver_vis_map LEFT OUTER JOIN corpus ON (resolver_vis_map.corpus = corpus.name) WHERE corpus = corpus.name AND namespace = '"+namespace+"' AND element = '"+type+"' UNION "+select+" FROM resolver_vis_map LEFT OUTER JOIN corpus ON (resolver_vis_map.corpus = corpus.name) WHERE namespace IS NULL AND corpus IN (SELECT name from corpus WHERE id = '"+corpusId+"') ORDER BY \"order\";";
 
     // TODO: create the sql query for getting the resolver data
     
-    return result.toString();
+    //return result.toString();
+       return result;
   }
 
   @Override
