07c5df9e577284bb88427e791c5788496d474f9c
Thomas Krause <thomas@berlin-reloaded.de>
2010-06-07 11:53:11 +0200

    - changing the Info in the Annis-Web interface to match the next released version

--- Annis-web/src/main/webapp/javascript/annis/config.js
+++ Annis-web/src/main/webapp/javascript/annis/config.js
@@ -1,6 +1,6 @@
 // the context path used by this installation
 var conf_context='/Annis-web';
-var conf_revision='Annis 2.1 beta5 release 2009-12-15';
+var conf_revision='Annis 2.1.6 (development version) release 2010-06-07';
 var global_timeout=60000;
 Ext.BLANK_IMAGE_URL='images/s.gif';
 Ext.SSL_SECURE_URL='https://korpling.german.hu-berlin.de' + conf_context + '/empty.html';
