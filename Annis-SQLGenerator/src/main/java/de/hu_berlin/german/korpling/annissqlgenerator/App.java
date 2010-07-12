d4854caf1f0e39717f19ebf49c1ee8cad7d51436
Thomas Krause <thomas@berlin-reloaded.de>
2010-07-12 15:41:46 +0200

    - adding (empty) Annis-SQLGenerator project

---
+++ Annis-SQLGenerator/src/main/java/de/hu_berlin/german/korpling/annissqlgenerator/App.java
@@ -0,0 +1,13 @@
+package de.hu_berlin.german.korpling.annissqlgenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
