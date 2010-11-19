7dcfda7cf8e60de467fa3ef2da0c7a4760b43e09
Thomas Krause <thomas@berlin-reloaded.de>
2010-11-19 10:26:04 +0100

    - removed the 2.1.8 - added "-Xmx128m" to kickstarter startup scripts

--- Annis-Kickstarter/src/main/startscripts/AnnisKickstarter.sh
+++ Annis-Kickstarter/src/main/startscripts/AnnisKickstarter.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -splash:splashscreen.gif -Djava.security.manager -Djava.security.policy=all_allowed.policy -cp 'lib/*' de.hu_berlin.german.korpling.annis.kickstarter.MainFrame
+java -Xmx128m -splash:splashscreen.gif -Djava.security.manager -Djava.security.policy=all_allowed.policy -cp 'lib/*' de.hu_berlin.german.korpling.annis.kickstarter.MainFrame
