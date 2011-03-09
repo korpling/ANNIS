1dfae0883f39322b714db13f9b273062b022d4f2
Benjamin Weißenfels kontakt@pixeldrama.de <>
2011-03-09 17:12:17 +0100

    put the attribute output on the MetadataWindow

--- Annis-Web/src/main/webapp/javascript/annis/MetaDataWindow.js
+++ Annis-Web/src/main/webapp/javascript/annis/MetaDataWindow.js
@@ -1,7 +1,9 @@
 Ext.onReady(function()
 {
-  Ext.Ajax.defaultHeaders = 
-    { "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"};
+  Ext.Ajax.defaultHeaders =
+  {
+    "Content-Type" : "application/x-www-form-urlencoded; charset=utf-8"
+  };
 
   MetaDataWindow = function(id)
   {
@@ -9,58 +11,97 @@ Ext.onReady(function()
 
     var grid = null;
 
-
     // init config
     config.title = 'Meta Data for id ' + id;
     config.width = 400;
     config.height = 200;
 
     // get datastore
-    var storeMeta = new Ext.data.JsonStore({
-      url: conf_context + '/secure/MetaData?mID=' + id,
-      totalProperty: 'size',
-      root: 'metadata',
-      id: 'id',
-      fields: [
-        'key',
-        'value'
-      ],
+    var storeMeta = new Ext.data.JsonStore(
+    {
+      url : conf_context + '/secure/MetaData?mID=' + id,
+      totalProperty : 'size',
+      root : 'metadata',
+      id : 'id',
+      fields : [ 'key', 'value' ],
       // turn on remote sorting
-      remoteSort: true
+      remoteSort : true
     });
 
     var colModel = new Ext.grid.ColumnModel([
+    {
+      header : "Name",
+      dataIndex : 'key'
+    },
+
+    {
+      header : "Value",
+      dataIndex : 'value'
+    } ]);
+
+    var gridMeta = new Ext.grid.GridPanel(
+    {
+      ds : storeMeta,
+      cm : colModel,
+      loadMask : true,
+      viewConfig :
       {
-        header: "Name",
-        dataIndex: 'key'
+        forceFit : true,
+        autoFill : true
       },
+      autoHeight : true,
+      autoWidth : true
+    });
 
+    var storeNodeAttributes = new Ext.data.JsonStore(
+    {
+      url : conf_context + '/secure/AttributeList?noprefix',
+      // turn on remote sorting
+      remoteSort : false,
+      fields : [ 'name', 'values' ]
+    });
+
+    var colModelAttribute = new Ext.grid.ColumnModel([
+    {
+      header : "Name",
+      dataIndex : "name"
+    },
+    {
+      header : "Value",
+      dataIndex : "values"
+    } ]);
+
+    var gridAttribute = new Ext.grid.GridPanel(
+    {
+      ds : storeNodeAttributes,
+      cm : colModelAttribute,
+      loadMask : true,
+      viewConfig :
       {
-        header: "Value",
-        dataIndex: 'value'
-      }
-    ]);
-
-    grid = new Ext.grid.GridPanel({
-      ds: storeMeta,
-      cm: colModel,
-      loadMask: true,
-      viewConfig: {
-        forceFit:true,
-        autoFill: true
+        forceFit : true,
+        autoFill : true
       },
-      autoHeight: true,
-      autoWidth: true
-
+      autoHeight : true,
+      autoWidth : true
     });
 
     storeMeta.load();
+    storeNodeAttributes.setDefaultSort('name', 'asc');
+    storeNodeAttributes.load(
+    {
+      params :
+      {
+        corpusIdList : '',
+        type : 'node'
+      }
+    });
 
-    config.items = [grid];
+    config.items = [ gridMeta, gridAttribute ];
     config.autoScroll = true;
     config.layout = 'fit';
 
     MetaDataWindow.superclass.constructor.call(this, config);
   };
+
   Ext.extend(MetaDataWindow, Ext.Window);
 });
