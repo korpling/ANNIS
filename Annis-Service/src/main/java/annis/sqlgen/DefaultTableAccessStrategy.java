15e25523eed0a8c2bafb9ece9c8760bb13e2eb3b
Thomas Krause <thomas@berlin-reloaded.de>
2011-03-21 18:38:49 +0100

    - unused variable - transient logger - inefficient string concatetenation - exclude filter file for FindBugs

--- Annis-Service/src/main/java/annis/sqlgen/DefaultTableAccessStrategy.java
+++ Annis-Service/src/main/java/annis/sqlgen/DefaultTableAccessStrategy.java
@@ -9,9 +9,6 @@ import org.apache.log4j.Logger;
 public class DefaultTableAccessStrategy extends TableAccessStrategy
 {
 
-	private Logger log = Logger.getLogger(this.getClass());
-
-
 	public DefaultTableAccessStrategy() {
 		super();
 	}
