7628bf39003fe5bc2a93e00c6299f3d632a7d64e
Thomas Krause <thomas@berlin-reloaded.de>
2011-08-01 17:12:02 +0200

    - also output meta (and distinguish them with "meta_" and "anno_" prefix)

--- Annis-Service/src/main/java/annis/WekaHelper.java
+++ Annis-Service/src/main/java/annis/WekaHelper.java
@@ -26,7 +26,6 @@ import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
-import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
 
 /**
@@ -38,7 +37,7 @@ public class WekaHelper
 
   public String exportAsArff(List<AnnotatedMatch> annotatedMatches)
   {
-    StringBuffer sb = new StringBuffer();
+    StringBuilder sb = new StringBuilder();
 
     // header: relation name (unused)
     sb.append("@relation name\n");
@@ -46,59 +45,80 @@ public class WekaHelper
 
     // figure out what annotations are used at each match position
     SortedMap<Integer, SortedSet<String>> columnsByNodePos = new TreeMap<Integer, SortedSet<String>>();
-    for (int i = 0; i < annotatedMatches.size(); ++i)
+        
+    for(int i = 0; i < annotatedMatches.size(); ++i)
     {
       AnnotatedMatch match = annotatedMatches.get(i);
-      for (int j = 0; j < match.size(); ++j)
+      for(int j = 0; j < match.size(); ++j)
       {
         AnnotatedSpan span = match.get(j);
-        if (columnsByNodePos.get(j) == null)
+        if(columnsByNodePos.get(j) == null)
         {
           columnsByNodePos.put(j, new TreeSet<String>());
         }
-        for (Annotation annotation : span.getAnnotations())
+        for(Annotation annotation : span.getAnnotations())
         {
-          columnsByNodePos.get(j).add(annotation.getQualifiedName());
+          columnsByNodePos.get(j).add("anno_" + annotation.getQualifiedName());
         }
+        
+        for(Annotation meta : span.getMetadata())
+        {
+          columnsByNodePos.get(j).add("meta_" + meta.getQualifiedName());
+        }
+        
       }
     }
 
+
     // print column names and data types
     int count = columnsByNodePos.keySet().size();
-    for (int j = 0; j < count; ++j)
+    
+    for(int j = 0; j < count; ++j)
     {
-      sb.append("@attribute " + fullColumnName(j + 1, "id") + " string\n");
-      sb.append("@attribute " + fullColumnName(j + 1, "span") + " string\n");
+      sb.append("@attribute ").append(fullColumnName(j + 1, "id")).append(" string\n");
+      sb.append("@attribute ").append(fullColumnName(j + 1, "span")).append(" string\n");
       SortedSet<String> annotationNames = columnsByNodePos.get(j);
-      for (String name : annotationNames)
+      for(String name : annotationNames)
       {
-        sb.append("@attribute " + fullColumnName(j + 1, name) + " string\n");
+        sb.append("@attribute ").append(fullColumnName(j + 1, name)).append(" string\n");
       }
     }
     sb.append("\n@data\n\n");
 
     // print values
-    for (AnnotatedMatch match : annotatedMatches)
+    for(AnnotatedMatch match : annotatedMatches)
     {
       List<String> line = new ArrayList<String>();
       int k = 0;
-      for (; k < match.size(); ++k)
+      for(; k < match.size(); ++k)
       {
         AnnotatedSpan span = match.get(k);
         Map<String, String> valueByName = new HashMap<String, String>();
-        if (span != null && span.getAnnotations() != null)
+
+        if(span != null)
         {
-          for (Annotation annotation : span.getAnnotations())
+          if(span.getAnnotations() != null)
           {
-            valueByName.put(annotation.getQualifiedName(), annotation.getValue());
+            for(Annotation annotation : span.getAnnotations())
+            {
+              valueByName.put("anno_" + annotation.getQualifiedName(), annotation.getValue());
+            }
           }
+          if(span.getMetadata() != null)
+          {
+            for(Annotation meta : span.getMetadata())
+            {
+              valueByName.put("meta_" + meta.getQualifiedName(), meta.getValue());
+            }
+          }
+
           line.add("'" + span.getId() + "'");
           line.add("'" + span.getCoveredText().replace("'", "\\'") + "'");
         }
 
-        for (String name : columnsByNodePos.get(k))
+        for(String name : columnsByNodePos.get(k))
         {
-          if (valueByName.containsKey(name))
+          if(valueByName.containsKey(name))
           {
             line.add("'" + valueByName.get(name).replace("'", "\\'") + "'");
           }
@@ -108,10 +128,10 @@ public class WekaHelper
           }
         }
       }
-      for (int l = k; l < count; ++l)
+      for(int l = k; l < count; ++l)
       {
         line.add("'NULL'");
-        for (int m = 0; m <= columnsByNodePos.get(l).size(); ++m)
+        for(int m = 0; m <= columnsByNodePos.get(l).size(); ++m)
         {
           line.add("'NULL'");
         }
