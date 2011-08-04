dc8f825d1e1360af1e302e326d980f8556186025
Thomas Krause <thomas@berlin-reloaded.de>
2011-08-04 18:06:34 +0200

    - adding empty project for the interfaces

---
+++ Annis-Service/annis-service-interface/src/main/java/de/hu-berlin/german/korpling/annis/App.java
@@ -0,0 +1,13 @@
+package de.hu-berlin.german.korpling.annis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
