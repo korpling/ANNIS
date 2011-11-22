d75242fc8b38792ca3868aa1cde89d0ccceb4d4e
Thomas Krause <thomas@berlin-reloaded.de>
2011-11-22 12:22:34 +0100

    cleanup

--- Annis-Service/annis-service-impl/src/test/java/annis/querymodel/TestDataObject.java
+++ Annis-Service/annis-service-impl/src/test/java/annis/querymodel/TestDataObject.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package annis.model;
+package annis.querymodel;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
