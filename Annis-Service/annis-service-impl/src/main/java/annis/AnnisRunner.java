ffd38f227f886c9c37a8a1a74ad07fd447174dfb
Viktor Rosenfeld <rosenfel@informatik.hu-berlin.de>
2011-11-22 09:43:03 +0100

    display full milliseconds for average runtime when benchmarking

--- Annis-Service/annis-service-impl/src/main/java/annis/AnnisRunner.java
+++ Annis-Service/annis-service-impl/src/main/java/annis/AnnisRunner.java
@@ -100,7 +100,7 @@ public class AnnisRunner extends AnnisBaseRunner
 	private static class Benchmark {
 		private String functionCall;
 		private QueryData queryData;
-		private float avgTimeInMilliseconds;
+		private long avgTimeInMilliseconds;
 		private long bestTimeInMilliseconds;
 		private String sql;
 		private String plan;
@@ -370,7 +370,7 @@ public class AnnisRunner extends AnnisBaseRunner
 		out.println();
 		out.println("---> benchmark complete");
 		for (Benchmark benchmark : benchmarks) {
-			benchmark.avgTimeInMilliseconds = benchmark.avgTimeInMilliseconds / benchmark.runs;
+			benchmark.avgTimeInMilliseconds = Math.round(benchmark.avgTimeInMilliseconds / benchmark.runs);
 			String options = benchmarkOptions(benchmark.queryData);
 			out.println(benchmark.avgTimeInMilliseconds + " ms (avg for " + benchmark.runs + " runs" + (benchmark.errors > 0 ? ", " + benchmark.errors + " errors)" : ")") + " for '" + benchmark.functionCall + ("".equals(options) ? "'" : "' with " + options));
 		}
