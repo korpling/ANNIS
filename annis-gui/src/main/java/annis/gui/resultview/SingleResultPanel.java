4445806c328ce38d1e1c6bf59b703c639f5b41bc
Thomas Krause <thomas@berlin-reloaded.de>
2011-11-30 16:19:01 +0100

    - use a single GridLayout instead of nested Horizonteal/VerticalLayout

--- annis-gui/src/main/java/annis/gui/resultview/SingleResultPanel.java
+++ annis-gui/src/main/java/annis/gui/resultview/SingleResultPanel.java
@@ -24,11 +24,14 @@ import annis.model.AnnotationGraph;
 import annis.resolver.ResolverEntry;
 import annis.service.AnnisService;
 import annis.service.ifaces.AnnisResult;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.themes.ChameleonTheme;
 import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
@@ -57,18 +60,20 @@ import org.apache.commons.lang.StringUtils;
 public class SingleResultPanel extends Panel implements Button.ClickListener
 {
 
-  private static final ThemeResource ICON_RESOURCE = new ThemeResource("info.gif");
+  private static final ThemeResource ICON_RESOURCE = new ThemeResource(
+    "info.gif");
   private AnnisResult result;
   private Map<AnnisNode, Long> markedAndCovered;
   private Map<String, String> markedCoveredMap;
   private Map<String, String> markedExactMap;
   private Set<Long> containedTexts;
-  private VerticalLayout vLayout;
   private ResolverProvider resolverProvider;
   private PluginSystem ps;
   private List<KWICPanel> kwicPanels;
   private Button btInfo;
-
+  private GridLayout gridLayout;
+  private int resultNumber;
+  
   public SingleResultPanel(final AnnisResult result, int resultNumber,
     ResolverProvider resolverProvider, PluginSystem ps,
     Set<String> visibleTokenAnnos)
@@ -76,7 +81,8 @@ public class SingleResultPanel extends Panel implements Button.ClickListener
     this.ps = ps;
     this.result = result;
     this.resolverProvider = resolverProvider;
-
+    this.resultNumber = resultNumber;
+    
     calculateHelperVariables();
 
     setWidth("100%");
@@ -84,80 +90,85 @@ public class SingleResultPanel extends Panel implements Button.ClickListener
 
     setScrollable(true);
 
-    HorizontalLayout hLayout = new HorizontalLayout();
-    setContent(hLayout);
+    gridLayout = new GridLayout(2, 3);
+    setContent(gridLayout);
 
-    hLayout.setWidth("100%");
-    hLayout.setHeight("-1px");
-    hLayout.setMargin(false);
-    hLayout.setSpacing(true);
+    gridLayout.setWidth("100%");
+    gridLayout.setHeight("-1px");
+    gridLayout.setMargin(false);
+    gridLayout.setSpacing(false);
 
-    VerticalLayout actionLayout = new VerticalLayout();
-    actionLayout.setSizeUndefined();
-    hLayout.addComponent(actionLayout);
-    Label lblNumber = new Label("" + (resultNumber + 1));
-    actionLayout.addComponent(lblNumber);
-    lblNumber.setSizeUndefined();
 
-    btInfo = new Button();
-    btInfo.setStyleName(ChameleonTheme.BUTTON_LINK);
-    btInfo.setIcon(ICON_RESOURCE);
-    btInfo.addListener((Button.ClickListener) this);
-    actionLayout.addComponent(btInfo);
-
-    actionLayout.setComponentAlignment(lblNumber, Alignment.TOP_CENTER);
-    actionLayout.setComponentAlignment(btInfo, Alignment.TOP_CENTER);
-    
-    vLayout = new VerticalLayout();
-    hLayout.addComponent(vLayout);
-
-    List<String> path = new LinkedList<String>(Arrays.asList(result.getGraph().getPath()));
+    List<String> path = new LinkedList<String>(Arrays.asList(result.getGraph().
+      getPath()));
     Collections.reverse(path);
-    Label lblPath = new Label("Path: " + StringUtils.join(path , " > "));
+    Label lblPath = new Label("Path: " + StringUtils.join(path, " > "));
     lblPath.addStyleName("docPath");
     lblPath.setWidth("100%");
     lblPath.setHeight("-1px");
-    vLayout.addComponent(lblPath);
-    
+    gridLayout.addComponent(lblPath, 1, 0);
+
     kwicPanels = new ArrayList<KWICPanel>();
-    for(long textId : containedTexts)
+    for (long textId : containedTexts)
     {
-      
+
       KWICPanel kwic = new KWICPanel(result, visibleTokenAnnos, markedAndCovered,
         textId);
-      vLayout.addComponent(kwic);
+      gridLayout.addComponent(kwic, 1, 1);
       kwicPanels.add(kwic);
     }
-    vLayout.setWidth("100%");
-    vLayout.setHeight("-1px");
-    vLayout.setMargin(false);
-    vLayout.setSpacing(false);
-
-    hLayout.setExpandRatio(vLayout, 1.0f);
+    
+    gridLayout.setColumnExpandRatio(1, 1.0f);
   }
 
   @Override
   public void attach()
   {
     AnnisService service = Helper.getService(getApplication(), getWindow());
-    if(service != null && resolverProvider != null)
+    if (service != null && resolverProvider != null)
     {
       try
       {
         ResolverEntry[] entries =
           resolverProvider.getResolverEntries(result, service);
-        for(ResolverEntry e : entries)
+
+        int rows = 2 + entries.length;
+        gridLayout.setRows(rows);
+        
+        CssLayout actionLayout = new CssLayout()
         {
-          vLayout.addComponent(new VisualizerPanel(e, result, ps, markedExactMap,
-            markedCoveredMap));
+
+          @Override
+          protected String getCss(Component c)
+          {
+            return "text-align: center;";
+          }
+          
+        };
+        gridLayout.addComponent(actionLayout, 0, 0, 0, rows-1);
+        Label lblNumber = new Label("" + (resultNumber + 1));
+        actionLayout.addComponent(lblNumber);
+        lblNumber.setSizeUndefined();
+
+        btInfo = new Button();
+        btInfo.setStyleName(ChameleonTheme.BUTTON_LINK);
+        btInfo.setIcon(ICON_RESOURCE);
+        btInfo.addListener((Button.ClickListener) this);
+        actionLayout.addComponent(btInfo);
+
+        int row=2;
+        for (ResolverEntry e : entries)
+        {
+          gridLayout.addComponent(new VisualizerPanel(e, result, ps, markedExactMap,
+            markedCoveredMap), 1, row++);
         }
       }
-      catch(RemoteException ex)
+      catch (RemoteException ex)
       {
         Logger.getLogger(SingleResultPanel.class.getName()).log(Level.SEVERE,
           "could not get resolver entries", ex);
-        getWindow().showNotification("could not get resolver entries: "
-          , ex.getLocalizedMessage(), Notification.TYPE_TRAY_NOTIFICATION);
+        getWindow().showNotification("could not get resolver entries: ", ex.
+          getLocalizedMessage(), Notification.TYPE_TRAY_NOTIFICATION);
       }
     }
 
@@ -166,9 +177,9 @@ public class SingleResultPanel extends Panel implements Button.ClickListener
 
   public void setVisibleTokenAnnosVisible(Set<String> annos)
   {
-    if(kwicPanels != null)
+    if (kwicPanels != null)
     {
-      for(KWICPanel kwic : kwicPanels)
+      for (KWICPanel kwic : kwicPanels)
       {
         kwic.setVisibleTokenAnnosVisible(annos);
       }
@@ -180,13 +191,13 @@ public class SingleResultPanel extends Panel implements Button.ClickListener
     markedExactMap = new HashMap<String, String>();
     markedCoveredMap = new HashMap<String, String>();
     containedTexts = new TreeSet<Long>();
-    
-    for(AnnisNode n : result.getGraph().getNodes())
+
+    for (AnnisNode n : result.getGraph().getNodes())
     {
       containedTexts.add(n.getTextId());
-      
+
       Long match = n.getMatchedNodeInQuery();
-      if(match != null)
+      if (match != null)
       {
         int color = Math.max(0, Math.min((int) match.longValue() - 1,
           MatchedNodeColors.values().length - 1));
@@ -198,24 +209,26 @@ public class SingleResultPanel extends Panel implements Button.ClickListener
 
     markedAndCovered = calculateMarkedAndCoveredIDs(result.getGraph());
 
-    for(Entry<AnnisNode, Long> markedEntry : markedAndCovered.entrySet())
+    for (Entry<AnnisNode, Long> markedEntry : markedAndCovered.entrySet())
     {
-      int color = Math.max(0, Math.min((int) markedEntry.getValue().longValue() - 1,
+      int color = Math.max(0, Math.min((int) markedEntry.getValue().longValue()
+        - 1,
         MatchedNodeColors.values().length - 1));
       markedCoveredMap.put("" + markedEntry.getKey().getId(),
         MatchedNodeColors.values()[color].name());
     }
   }
 
-  private Map<AnnisNode, Long> calculateMarkedAndCoveredIDs(AnnotationGraph graph)
+  private Map<AnnisNode, Long> calculateMarkedAndCoveredIDs(
+    AnnotationGraph graph)
   {
     Set<Long> matchedNodes = graph.getMatchedNodeIds();
     Map<AnnisNode, Long> matchedAndCovered = new HashMap<AnnisNode, Long>();
 
     // add all covered nodes
-    for(AnnisNode n : graph.getNodes())
+    for (AnnisNode n : graph.getNodes())
     {
-      if(matchedNodes.contains(n.getId()) && n.getMatchedNodeInQuery() != null)
+      if (matchedNodes.contains(n.getId()) && n.getMatchedNodeInQuery() != null)
       {
         Long matchPosition = n.getMatchedNodeInQuery();
         matchedAndCovered.put(n, matchPosition);
@@ -223,12 +236,13 @@ public class SingleResultPanel extends Panel implements Button.ClickListener
         long left = n.getLeftToken();
         long right = n.getRightToken();
 
-        for(long i = left; i <= right; i++)
+        for (long i = left; i <= right; i++)
         {
           AnnisNode tok = graph.getToken(i);
           Long oldTokenPosition = matchedAndCovered.get(tok);
-          if(oldTokenPosition == null
-            || (!matchedNodes.contains(tok.getId()) && matchPosition.compareTo(oldTokenPosition) >= 0))
+          if (oldTokenPosition == null
+            || (!matchedNodes.contains(tok.getId()) && matchPosition.compareTo(
+            oldTokenPosition) >= 0))
           {
             matchedAndCovered.put(tok, matchPosition);
           }
@@ -242,21 +256,22 @@ public class SingleResultPanel extends Panel implements Button.ClickListener
   @Override
   public void buttonClick(ClickEvent event)
   {
-    if(event.getButton() == btInfo)
+    if (event.getButton() == btInfo)
     {
       AnnotationGraph graph = result.getGraph();
-      
-      long textId = graph.getNodes().size() > 0 ? graph.getNodes().get(0).getTextId()
+
+      long textId = graph.getNodes().size() > 0 ? graph.getNodes().get(0).
+        getTextId()
         : -1;
-      
+
       Window infoWindow = new Window("Info for " + graph.getDocumentName());
-      
+
       infoWindow.setModal(false);
       MetaDataPanel meta = new MetaDataPanel(textId);
       infoWindow.setContent(meta);
       infoWindow.setWidth("400px");
       infoWindow.setHeight("400px");
-      
+
       getWindow().addWindow(infoWindow);
     }
   }
