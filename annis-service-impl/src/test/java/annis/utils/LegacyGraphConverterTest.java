074842650ce1e61b51183a3e4ac9221a66b521ec
Thomas Krause <thomas@berlin-reloaded.de>
2011-12-01 19:00:13 +0100

    - rudimentary test code for AnnotationGraph, ignores edges for now

--- annis-service-impl/src/test/java/annis/utils/LegacyGraphConverterTest.java
+++ annis-service-impl/src/test/java/annis/utils/LegacyGraphConverterTest.java
@@ -4,15 +4,22 @@
  */
 package annis.utils;
 
+import annis.model.Annotation;
+import java.util.Set;
+import annis.model.AnnisNode;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import annis.model.AnnotationGraph;
+import annis.model.Edge;
 import annis.sqlgen.AOMAnnotateSqlGenerator;
 import annis.sqlgen.SaltAnnotateSqlGenerator;
 import annis.test.CSVResultSetProvider;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.SaltProject;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -28,11 +35,15 @@ import static org.junit.Assert.*;
  * @author thomas
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:annis/sqlgen/SqlGenerator-context.xml"})
+@ContextConfiguration(locations =
+{
+  "classpath:annis/sqlgen/SqlGenerator-context.xml"
+})
 public class LegacyGraphConverterTest
 {
-  
-  @Autowired AOMAnnotateSqlGenerator aomSqlGen;
+
+  @Autowired
+  AOMAnnotateSqlGenerator aomSqlGen;
 
   public LegacyGraphConverterTest()
   {
@@ -62,21 +73,95 @@ public class LegacyGraphConverterTest
    * Test of convertToAOM method, of class LegacyGraphConverter.
    */
   @Test
-  @Ignore
   public void testConvertToAOM() throws SQLException
   {
-    
+
     SaltAnnotateSqlGenerator saltSqlGen = new SaltAnnotateSqlGenerator();
-    
-    
-    SaltProject p = saltSqlGen.extractData(new CSVResultSetProvider(annis.sqlgen.SaltAnnotateSqlGeneratorTest.class.
+
+
+    SaltProject p =
+      saltSqlGen.extractData(new CSVResultSetProvider(annis.sqlgen.SaltAnnotateSqlGeneratorTest.class.
       getResourceAsStream("SampleAnnotateResult.csv")).getResultSet());
-    
-    List<AnnotationGraph> expected = aomSqlGen.extractData(new CSVResultSetProvider(annis.sqlgen.SaltAnnotateSqlGeneratorTest.class.
+
+    List<AnnotationGraph> expected =
+      aomSqlGen.extractData(new CSVResultSetProvider(annis.sqlgen.SaltAnnotateSqlGeneratorTest.class.
       getResourceAsStream("SampleAnnotateResult.csv")).getResultSet());
-    
+
     List<AnnotationGraph> result = LegacyGraphConverter.convertToAOM(p);
-    
-    assertEquals(expected, result);
+
+    assertEquals(expected.size(), result.size());
+    Iterator<AnnotationGraph> itGraphExpected = expected.iterator();
+    Iterator<AnnotationGraph> itGraphResult = result.iterator();
+
+    while (itGraphExpected.hasNext() && itGraphResult.hasNext())
+    {
+
+      AnnotationGraph graphExpected = itGraphExpected.next();
+      AnnotationGraph graphResult = itGraphResult.next();
+
+      List<AnnisNode> nodeListExpected = graphExpected.getNodes();
+      List<AnnisNode> nodeListResult = graphResult.getNodes();
+
+      assertEquals(nodeListExpected.size(), nodeListResult.size());
+
+      Collections.sort(nodeListExpected, new Comparator<AnnisNode>()
+      {
+
+        @Override
+        public int compare(AnnisNode arg0, AnnisNode arg1)
+        {
+          return Long.compare(arg0.getId(), arg1.getId());
+        }
+      });
+      Collections.sort(nodeListResult, new Comparator<AnnisNode>()
+      {
+
+        @Override
+        public int compare(AnnisNode arg0, AnnisNode arg1)
+        {
+          return Long.compare(arg0.getId(), arg1.getId());
+        }
+      });
+      
+      Iterator<AnnisNode> itNodeExpected = nodeListExpected.iterator();
+      Iterator<AnnisNode> itNodeResult = nodeListResult.iterator();
+      
+      while(itNodeExpected.hasNext() && itNodeResult.hasNext())
+      {
+        checkAnnisNodeEqual(itNodeExpected.next(), itNodeResult.next());
+      }
+
+    }
+
+  }
+
+  private void checkAnnisNodeEqual(AnnisNode n1, AnnisNode n2)
+  {
+
+    checkAnnotationSetEqual(n1.getNodeAnnotations(), n2.getNodeAnnotations());
+    checkAnnotationSetEqual(n1.getEdgeAnnotations(), n2.getEdgeAnnotations());
+
+  }
+
+  private void checkAnnisEdgeEqual(Edge n1, Edge n2)
+  {
+    checkAnnotationSetEqual(n1.getAnnotations(), n2.getAnnotations());
+
+  }
+
+  private void checkAnnotationSetEqual(Set<Annotation> annos1,
+    Set<Annotation> annos2)
+  {
+    for (Annotation a : annos1)
+    {
+      assertTrue("annotation " + a.getQualifiedName() + "->" + a.getName()
+        + " from set 1 is not included in set 2", annos2.contains(a));
+    }
+
+    for (Annotation a : annos2)
+    {
+      assertTrue("annotation " + a.getQualifiedName() + "->" + a.getName()
+        + " from set 2 is not included in set 1", annos2.contains(a));
+    }
   }
 }
