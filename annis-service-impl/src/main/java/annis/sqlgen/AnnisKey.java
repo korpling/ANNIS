409f863271d73ad1465a579de0d7cea74acfb4da
Viktor Rosenfeld <rosenfel@informatik.hu-berlin.de>
2012-01-10 14:22:13 +0100

    add key columns to outer annotate query

--- annis-service-impl/src/main/java/annis/sqlgen/AnnisKey.java
+++ annis-service-impl/src/main/java/annis/sqlgen/AnnisKey.java
@@ -8,6 +8,15 @@ import static annis.sqlgen.TableAccessStrategy.NODE_TABLE;
 public class AnnisKey
 {
 
+  /**
+   * Generate list of column aliases that are used to identify a node
+   * in a matching solution in the inner query of an ANNOTATE function query.
+   *  
+   * @param tableAccessStrategy TODO
+   * @param index TODO
+   * @return  A list of column aliases that are used in the SELECT clause of
+   *          the inner query.
+   */
   public List<String> generateInnerQueryColumns(
       TableAccessStrategy tableAccessStrategy, int index)
   {
@@ -19,4 +28,45 @@ public class AnnisKey
     return columns;
   }
 
+  /**
+   * Generate the key(s) for an annotation graph.
+   * 
+   * @param tableAccessStrategy TODO
+   * @param size TODO
+   * @return A list of column aliases that are used in the SELECT clause of
+   *         the outer ANNOTATE query.
+   */
+  public List<String> generateOuterQueryColumns(
+      TableAccessStrategy tableAccessStrategy, int size)
+  {
+    String idAlias = tableAccessStrategy.aliasedColumn("solutions", "id");
+    String nameAlias = tableAccessStrategy.aliasedColumn("solutions", "name");
+    List<String> columns = new ArrayList<String>();
+    columns.add(createKeyArray(idAlias, "key", size));
+    columns.add(createKeyArray(nameAlias, "key_names", size));
+    return columns;
+  }
+
+  // create a key array
+  // example: ARRAY[solutions.id1, solutions.id2, solutions.id3] AS key
+  private String createKeyArray(String column, String alias, int size)
+  {
+    StringBuilder sb = new StringBuilder();
+    sb.append("ARRAY[");
+    sb.append(column);
+    sb.append(1);
+    for (int i = 2; i <= size; ++i) {
+      sb.append(", ");
+      sb.append(column);
+      sb.append(i);
+    }
+    sb.append("]");
+    sb.append(" AS ");
+    sb.append(alias);
+    String s = sb.toString();
+    return s;
+  }
+
+  
+  
 }
--- annis-service-impl/src/test/java/annis/sqlgen/TestAnnisKey.java
+++ annis-service-impl/src/test/java/annis/sqlgen/TestAnnisKey.java
@@ -1,5 +1,6 @@
 package annis.sqlgen;
 
+import static annis.sqlgen.TableAccessStrategy.NODE_TABLE;
 import static annis.test.TestUtils.uniqueInt;
 import static annis.test.TestUtils.uniqueString;
 import static java.util.Arrays.asList;
@@ -7,8 +8,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.MockitoAnnotations.initMocks;
-
-import static annis.sqlgen.TableAccessStrategy.NODE_TABLE;
 
 import java.util.List;
 
@@ -29,6 +28,10 @@ public class TestAnnisKey
     initMocks(this);
   }
 
+  /**
+   * ANNOTATE requires columns identifying each node of a matching solution
+   * in the inner query.
+   */
   @Test
   public void shouldGenerateColumnsForInnerQuery()
   {
@@ -47,4 +50,27 @@ public class TestAnnisKey
     assertThat(actual, is(expected));
   }
 
+  /**
+   * The key for an annotation graph is an array containing the IDs and/or 
+   * names of the matched nodes in a solution. 
+   */
+  @Test
+  public void shouldGenerateColumnsForOuterQuery()
+  {
+    // given
+    String idAlias = uniqueString(3);
+    String nameAlias = uniqueString(3);
+    given(tableAccessStrategy.aliasedColumn("solutions", "id")).willReturn(idAlias);
+    given(tableAccessStrategy.aliasedColumn("solutions", "name")).willReturn(nameAlias);
+    int size = 3;
+    // when
+    List<String> actual = key.generateOuterQueryColumns(tableAccessStrategy, size);
+    // then
+    // FIXME: key und key_names sind hardcodiert
+    List<String> expected = asList(
+        "ARRAY[" + idAlias + "1" + ", " + idAlias + "2" + ", " + idAlias + "3" + "] AS key",
+        "ARRAY[" + nameAlias + "1" + ", " + nameAlias + "2" + ", " + nameAlias + "3" + "] AS key_names");
+    assertThat(actual, is(expected));    
+  }
+  
 }
