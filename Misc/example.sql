152499a0093a683f4dbaa00ecd179497ca8cfc88
Thomas Krause <thomas@berlin-reloaded.de>
2012-01-25 17:16:45 +0100

    - updated example

--- Misc/example.sql
+++ Misc/example.sql
@@ -4,12 +4,12 @@ SELECT
   count(*)
 FROM
   (
-    SELECT
+    SELECT 
       facts1.id AS id1, facts2.id AS id2, facts3.id AS id3, facts1.toplevel_corpus
     FROM
-      s_facts AS facts1,
-      s_facts AS facts2,
-      s_facts AS facts3
+      facts AS facts1,
+      facts AS facts2,
+      facts AS facts3
     WHERE
       (facts1.sample & B'01000') = B'01000' AND
       (facts2.sample & B'01000') = B'01000' AND
@@ -17,17 +17,18 @@ FROM
       -- annotations can always only be inside a subcorpus/document AND
       -- artificial node-node_annotation subview AND
       facts1.corpus_ref = facts3.corpus_ref AND
-      facts1.node_anno = ANY(getNodeAnno('pos', 'ART', ARRAY[2015])) AND
+      facts1.node_anno_ref= ANY(getAnnoByNameVal('pos', 'ART', ARRAY[1974], 'node')) AND
       facts1.right_token = facts2.left_token - 1 AND
       facts1.text_ref = facts2.text_ref AND
-      facts1.toplevel_corpus IN (2015) AND
-      facts2.node_anno = ANY(getNodeAnno('pos', 'NN', ARRAY[2015])) AND
+      facts1.toplevel_corpus IN (1974) AND
+      facts2.node_anno_ref= ANY(getAnnoByNameVal('pos', 'NN', ARRAY[1974], 'node')) AND
       facts2.right_token BETWEEN SYMMETRIC facts3.left_token - 1 AND facts3.left_token - 30 AND
       facts2.text_ref = facts3.text_ref AND
-      facts2.toplevel_corpus IN (2015) AND
-      facts3.node_anno = ANY(getNodeAnno('pos', 'VAPP', ARRAY[2015])) AND
-      facts3.toplevel_corpus IN (2015)
+      facts2.toplevel_corpus IN (1974) AND
+      facts3.node_anno_ref= ANY(getAnnoByNameVal('pos', 'VAPP', ARRAY[1974], 'node')) AND
+      facts3.toplevel_corpus IN (1974)
   ) AS solutions
+
 ;
 
 -- count pos="ART" & pos="NN" & pos="VAPP" & cat="NP" &  #1 . #2 & #2 .1,30 #3 & #4 >* #3
@@ -36,14 +37,12 @@ SELECT
 FROM
   (
     SELECT DISTINCT
-      facts1.id AS id1, facts2.id AS id2, facts3.id AS id3,
-      facts4.id AS id4, 
-      facts1.toplevel_corpus
+      facts1.id AS id1, facts2.id AS id2, facts3.id AS id3, facts4.id AS id4, facts1.toplevel_corpus
     FROM
-      s_facts AS facts1,
-      s_facts AS facts2,
-      s_facts AS facts3,
-      s_facts AS facts4
+      facts AS facts1,
+      facts AS facts2,
+      facts AS facts3,
+      facts AS facts4
     WHERE
       (facts1.sample & B'01000') = B'01000' AND
       (facts2.sample & B'01000') = B'01000' AND
@@ -54,24 +53,24 @@ FROM
       -- artificial node-rank-component-node_annotation subview AND
       facts1.corpus_ref = facts3.corpus_ref AND
       facts1.corpus_ref = facts4.corpus_ref AND
-      facts1.node_anno = ANY(getNodeAnno('pos', 'ART', ARRAY[2015])) AND
+      facts1.node_anno_ref= ANY(getAnnoByNameVal('pos', 'ART', ARRAY[1974], 'node')) AND
       facts1.right_token = facts2.left_token - 1 AND
       facts1.text_ref = facts2.text_ref AND
-      facts1.toplevel_corpus IN (2015) AND
+      facts1.toplevel_corpus IN (1974) AND
       facts2.corpus_ref = facts4.corpus_ref AND
-      facts2.node_anno = ANY(getNodeAnno('pos', 'NN', ARRAY[2015])) AND
+      facts2.node_anno_ref= ANY(getAnnoByNameVal('pos', 'NN', ARRAY[1974], 'node')) AND
       facts2.right_token BETWEEN SYMMETRIC facts3.left_token - 1 AND facts3.left_token - 30 AND
       facts2.text_ref = facts3.text_ref AND
-      facts2.toplevel_corpus IN (2015) AND
+      facts2.toplevel_corpus IN (1974) AND
       facts3.corpus_ref = facts4.corpus_ref AND
       facts3.edge_name IS NULL AND
       facts3.edge_type = 'd' AND
-      facts3.node_anno = ANY(getNodeAnno('pos', 'VAPP', ARRAY[2015])) AND
+      facts3.node_anno_ref= ANY(getAnnoByNameVal('pos', 'VAPP', ARRAY[1974], 'node')) AND
       facts3.pre < facts4.post AND
-      facts3.toplevel_corpus IN (2015) AND
-      facts4.node_anno = ANY(getNodeAnno('cat', 'NP', ARRAY[2015])) AND
+      facts3.toplevel_corpus IN (1974) AND
+      facts4.node_anno_ref= ANY(getAnnoByNameVal('cat', 'NP', ARRAY[1974], 'node')) AND
       facts4.pre < facts3.pre AND
-      facts4.toplevel_corpus IN (2015)
+      facts4.toplevel_corpus IN (1974)
   ) AS solutions
 ;
 --  select * from annotations where "type"='node' and toplevel_corpus='2014' and "name"='cat' order by occurences;
