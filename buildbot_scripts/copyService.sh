63b9c7bad454a9df0a9887893ff2eaf606c9d919
Thomas Krause <thomas@berlin-reloaded.de>
2012-02-01 17:16:22 +0100

    - updating build bot script to remove everything the in lib/ folder of the service and to copy the annis-service.properties

--- buildbot_scripts/copyService.sh
+++ buildbot_scripts/copyService.sh
@@ -7,6 +7,10 @@ ANNIS_VERSION=2.3.0-SNAPSHOT
 DIST_DIR=annis-service-impl/target/annis-service-impl-$ANNIS_VERSION-distribution/annis-service-impl-$ANNIS_VERSION
 
 #code
+# delete all old libraries so that they don't get into the classpath
+rm -fR $DIST_DIR/lib/
+# copy new files
 cp -Rf $DIST_DIR/lib/* $ANNIS_HOME/lib/
 cp -Rf $DIST_DIR/sql/* $ANNIS_HOME/sql/
 cp -Rf $DIST_DIR/bin/* $ANNIS_HOME/bin/
+cp $DIST_DIR/conf/annis-service.properties $ANNIS_HOME/conf/
