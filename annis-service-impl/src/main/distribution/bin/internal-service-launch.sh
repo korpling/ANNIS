a01c6ef9828999e8c23f3031b88d80b2e7c56ab8
Thomas Krause <thomas@berlin-reloaded.de>
2012-08-16 19:56:42 +0200

    better comment

--- annis-service-impl/src/main/distribution/bin/internal-service-launch.sh
+++ annis-service-impl/src/main/distribution/bin/internal-service-launch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# execute service, redirect stdout to stderr in order not to disturb our output 
+# IMPORANT: our service is only allowed to output messages on stderr and 
+# not stdout in order not to disturb our output 
 # (which should be only the echo $1) 
 java -Dfile.encoding=UTF-8 -Dannis.home="$ANNIS_HOME" -cp "$classpath" $jvm_args annis.service.internal.AnnisServiceRunner $service_args <&- &
 echo $!
