ea3659e261934dd9c9490bd7cd4734e6871a915c
Thomas Krause <thomas@berlin-reloaded.de>
2012-09-18 12:30:27 +0200

    build bot must get more reliable in case of non-writable files

--- buildbot_scripts/copyWebapp.sh
+++ buildbot_scripts/copyWebapp.sh
@@ -4,7 +4,17 @@
 ANNIS_WEBAPP_HOME=$1
 
 #code
-rm -R $ANNIS_WEBAPP_HOME/*
+rm -Rf $ANNIS_WEBAPP_HOME/META-INF/*
+rm -Rf $ANNIS_WEBAPP_HOME/THIRD-PARTY/*
+rm -Rf $ANNIS_WEBAPP_HOME/VAADIN/*
+rm -Rf $ANNIS_WEBAPP_HOME/WEB-INF/*
+rm -Rf $ANNIS_WEBAPP_HOME/annis.gui.widgets.gwt.AnnisWidgetSet/*
+rm -Rf $ANNIS_WEBAPP_HOME/jquery/*
+rm -Rf $ANNIS_WEBAPP_HOME/tutorial/*
+rm -f $ANNIS_WEBAPP_HOME/CHANGELOG
+rm -f $ANNIS_WEBAPP_HOME/LICENSE
+rm -f $ANNIS_WEBAPP_HOME/NOTICE
+
 unzip -d $ANNIS_WEBAPP_HOME annis-gui/target/annis-gui.war
 
 chmod -Rf ug+rw $ANNIS_WEBAPP_HOME/*
