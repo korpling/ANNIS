11291afad93674715e9e2cb41e4d7c8e3db54dc9
Thomas Krause <thomas@berlin-reloaded.de>
2012-10-17 20:16:46 +0200

    use a more distinctive name for the password field of the login form (should help firefox to distinguish which password to save)

--- annis-gui/src/main/java/annis/gui/SearchWindow.java
+++ annis-gui/src/main/java/annis/gui/SearchWindow.java
@@ -28,12 +28,14 @@ import com.vaadin.data.validator.EmailValidator;
 import com.vaadin.event.ShortcutListener;
 import com.vaadin.terminal.ParameterHandler;
 import com.vaadin.terminal.ThemeResource;
+import com.vaadin.terminal.gwt.client.ApplicationConnection;
 import com.vaadin.terminal.gwt.server.WebApplicationContext;
 import com.vaadin.terminal.gwt.server.WebBrowser;
+import com.vaadin.ui.*;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.LoginForm.LoginEvent;
-import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ChameleonTheme;
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -407,7 +409,71 @@ public class SearchWindow extends Window
 
     if (windowLogin == null)
     {
-      LoginForm login = new LoginForm();
+      LoginForm login = new LoginForm()
+      {
+
+        /**
+         * Custom implementation which uses a more distinctive name for the password field
+         */
+        @Override
+        protected byte[] getLoginHTML()
+        {
+           String appUri = getApplication().getURL().toString()
+            + getWindow().getName() + "/";
+
+          try
+          {
+            return ("<!DOCTYPE html PUBLIC \"-//W3C//DTD "
+              + "XHTML 1.0 Transitional//EN\" "
+              + "\"http://www.w3.org/TR/xhtml1/"
+              + "DTD/xhtml1-transitional.dtd\">\n" + "<html>"
+              + "<head><script type='text/javascript'>"
+              + "var setTarget = function() {" + "var uri = '"
+              + appUri
+              + "loginHandler"
+              + "'; var f = document.getElementById('loginf');"
+              + "document.forms[0].action = uri;document.forms[0].username.focus();};"
+              + ""
+              + "var styles = window.parent.document.styleSheets;"
+              + "for(var j = 0; j < styles.length; j++) {\n"
+              + "if(styles[j].href) {"
+              + "var stylesheet = document.createElement('link');\n"
+              + "stylesheet.setAttribute('rel', 'stylesheet');\n"
+              + "stylesheet.setAttribute('type', 'text/css');\n"
+              + "stylesheet.setAttribute('href', styles[j].href);\n"
+              + "document.getElementsByTagName('head')[0].appendChild(stylesheet);\n"
+              + "}"
+              + "}\n"
+              + "function submitOnEnter(e) { var keycode = e.keyCode || e.which;"
+              + " if (keycode == 13) {document.forms[0].submit();}  } \n"
+              + "</script>"
+              + "</head><body onload='setTarget();' style='margin:0;padding:0; background:transparent;' class=\""
+              + ApplicationConnection.GENERATED_BODY_CLASSNAME
+              + "\">"
+              + "<div class='v-app v-app-loginpage' style=\"background:transparent;\">"
+              + "<iframe name='logintarget' style='width:0;height:0;"
+              + "border:0;margin:0;padding:0;'></iframe>"
+              + "<form id='loginf' target='logintarget' onkeypress=\"submitOnEnter(event)\" method=\"post\">"
+              + "<div>"
+              + getUsernameCaption()
+              + "</div><div >"
+              + "<input class='v-textfield' style='display:block;' type='text' name='username'></div>"
+              + "<div>"
+              + getPasswordCaption()
+              + "</div>"
+              + "<div><input class='v-textfield' style='display:block;' type='password' name='annis-gui-password'></div>"
+              + "<div><div onclick=\"document.forms[0].submit();\" tabindex=\"0\" class=\"v-button\" role=\"button\" ><span class=\"v-button-wrap\"><span class=\"v-button-caption\">"
+              + getLoginButtonCaption()
+              + "</span></span></div></div></form></div>" + "</body></html>")
+              .getBytes("UTF-8");
+          }
+          catch (UnsupportedEncodingException e)
+          {
+            throw new RuntimeException("UTF-8 encoding not avalable", e);
+          }
+        }
+        
+      };
       login.addListener((LoginForm.LoginListener) this);
 
       windowLogin = new Window("Login");
@@ -428,7 +494,7 @@ public class SearchWindow extends Window
     {
       AnnisUser newUser = securityManager.login(event.getLoginParameter(
         "username"),
-        event.getLoginParameter("password"), true);
+        event.getLoginParameter("annis-gui-password"), true);
       getApplication().setUser(newUser);
       showNotification("Logged in as \"" + newUser.getUserName() + "\"",
         Window.Notification.TYPE_TRAY_NOTIFICATION);
