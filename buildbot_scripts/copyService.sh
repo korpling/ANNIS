a6b3c3887207a0809b30f4451bcebb0bf0213a2c
Thomas Krause <thomas@berlin-reloaded.de>
2012-11-07 14:42:26 +0100

    also copy spring configuration

--- buildbot_scripts/copyService.sh
+++ buildbot_scripts/copyService.sh
@@ -13,4 +13,5 @@ rm -fR $ANNIS_HOME/lib/*
 cp -Rf $DIST_DIR/lib/* $ANNIS_HOME/lib/
 cp -Rf $DIST_DIR/sql/* $ANNIS_HOME/sql/
 cp -Rf $DIST_DIR/bin/* $ANNIS_HOME/bin/
+cp -Rf $DIST_DIR/conf/spring/* $ANNIS_HOME/conf/spring
 cp $DIST_DIR/conf/annis-service.properties $ANNIS_HOME/conf/
