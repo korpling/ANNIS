5a760bf0890a5504f57e6382cfaf39ea9848d17e
Thomas Krause <thomas@berlin-reloaded.de>
2012-12-07 12:38:19 +0100

    we have to update the _node table as well

--- annis-service/src/main/distribution/sql/adjusttextid.sql
+++ annis-service/src/main/distribution/sql/adjusttextid.sql
@@ -11,10 +11,14 @@ CREATE UNLOGGED TABLE _textid_min (
 );
 
 INSERT INTO _textid_min(corpus_ref, min_id)
-SELECT corpus_ref, min(id) as min_id FROM _text GROUP BY corpus_ref;
+  SELECT corpus_ref, min(id) as min_id FROM _text GROUP BY corpus_ref;
 
 UPDATE _text AS t SET 
   id = id - (SELECT min_id FROM _textid_min AS m WHERE t.corpus_ref = m.corpus_ref)
+;
+
+UPDATE _node AS n SET
+  text_ref = text_ref - (SELECT min_id FROM _textid_min AS m WHERE n.corpus_ref = m.corpus_ref)
 ;
 
 DROP TABLE _textid_min;
