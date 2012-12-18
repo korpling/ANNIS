638caa6222a28d143e3736d5dcdd82a60719793c
Benjamin Weißenfels <b.pixeldrama@gmail.com>
2012-12-18 14:28:08 +0100

    Vakyartha visualizer does not use any deprecated methods anymore.

--- annis-gui/src/main/java/annis/gui/visualizers/iframe/dependency/VakyarthaDependencyTree.java
+++ annis-gui/src/main/java/annis/gui/visualizers/iframe/dependency/VakyarthaDependencyTree.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2011 Collaborative Research Centre SFB 632 
+ * Copyright 2009-2011 Collaborative Research Centre SFB 632
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,17 +15,32 @@
  */
 package annis.gui.visualizers.iframe.dependency;
 
+import annis.gui.MatchedNodeColors;
 import annis.gui.visualizers.VisualizerInput;
 import annis.gui.visualizers.iframe.WriterVisualizer;
-import annis.model.AnnisNode;
-import annis.model.Annotation;
-import annis.model.Edge;
+import static annis.model.AnnisConstants.*;
 import annis.service.ifaces.AnnisResult;
+import de.hu_berlin.german.korpling.saltnpepper.salt.graph.Edge;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SDataSourceSequence;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SDocumentGraph;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SPointingRelation;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.STYPE_NAME;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.STextualDS;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SToken;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SAnnotation;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SFeature;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SGraph;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SNode;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SRelation;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Set;
+import java.util.Map;
+import java.util.TreeMap;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.LoggerFactory;
@@ -33,13 +48,18 @@ import org.slf4j.LoggerFactory;
 /**
  *
  * @author Thomas Krause <krause@informatik.hu-berlin.de>
+ * @author Benjamin Weißenfels<b.pixeldrama@gmail.com>
  */
 @PluginImplementation
 public class VakyarthaDependencyTree extends WriterVisualizer
 {
 
-  private static final org.slf4j.Logger log = LoggerFactory.getLogger(VakyarthaDependencyTree.class);
+  private static final org.slf4j.Logger log = LoggerFactory.
+    getLogger(VakyarthaDependencyTree.class);
+
   private Writer theWriter;
+
+  private VisualizerInput input;
 
   @Override
   public String getShortName()
@@ -47,24 +67,79 @@ public class VakyarthaDependencyTree extends WriterVisualizer
     return "arch_dependency";
   }
 
-  
-  
   @Override
   public void writeOutput(VisualizerInput input, Writer writer)
   {
     theWriter = writer;
+    this.input = input;
+
+    printHTMLOutput();
+  }
+
+  public void printHTMLOutput()
+  {
+    SDocumentGraph sDocumentGraph = input.getSResult().getSDocumentGraph();
+    Map<SToken, Integer> sToken = new TreeMap<SToken, Integer>(new Comparator<SToken>()
+    {
+      private int getIdx(SToken tok)
+      {
+        SFeature sF = tok.getSFeature(ANNIS_NS, FEAT_TOKENINDEX);
+        return sF != null ? (int) (long) sF.getSValueSNUMERIC() : -1;
+      }
+
+      @Override
+      public int compare(SToken o1, SToken o2)
+      {
+        int tok1 = getIdx(o1);
+        int tok2 = getIdx(o2);
+
+        if (tok1 < tok2)
+        {
+          return -1;
+        }
+
+        if (tok1 == tok2)
+        {
+          return 0;
+        }
+        else
+        {
+          return 1;
+        }
+
+      }
+    });
+
+    for (SNode n : sDocumentGraph.getSNodes())
+    {
+      if (n instanceof SToken)
+      {
+        SFeature sFeature = n.getSFeature(ANNIS_NS, FEAT_TOKENINDEX);
+        int tokenIdx = sFeature != null ? (int) (long) sFeature.
+          getSValueSNUMERIC() : -1;
+        sToken.put((SToken) n, tokenIdx);
+      }
+    }
+
+    Map<SToken, Integer> tok2Int = new HashMap<SToken, Integer>();
+    int count = 0;
+    for (SToken tok : sToken.keySet())
+    {
+      tok2Int.put(tok, count++);
+    }
+
     try
     {
-      AnnisResult result = input.getResult();
-
       println("<html>");
       println("<head>");
 
-      println("<script type=\"text/javascript\" src=\"" 
-        + input.getResourcePath("vakyartha/jquery-1.4.2.min.js") +"\"></script>");
-      println("<script type=\"text/javascript\" src=\"" 
+      println(
+        "<script type=\"text/javascript\" src=\""
+        + input.getResourcePath("vakyartha/jquery-1.4.2.min.js") + "\"></script>");
+      println("<script type=\"text/javascript\" src=\""
         + input.getResourcePath("vakyartha/raphael.js") + "\"></script>");
-      println("<script type=\"text/javascript\" src=\"" 
+      println(
+        "<script type=\"text/javascript\" src=\""
         + input.getResourcePath("vakyartha/vakyarthaDependency.js") + "\"></script>");
 
       // output the data for the javascript
@@ -74,56 +149,55 @@ public class VakyarthaDependencyTree extends WriterVisualizer
       println("tokens=new Object();");
 
 
-      HashMap<Long,Integer> id2pos = new HashMap<Long, Integer>();
-      int counter = 0;
-      for(AnnisNode tok : result.getGraph().getTokens())
-      {
-        id2pos.put(tok.getId(), counter);
-        counter++;
-      }
-
-
-      counter = 0;
-      for(AnnisNode tok : result.getGraph().getTokens())
+      count = 0;
+      for (SToken tok : sToken.keySet())
       {
         JSONObject o = new JSONObject();
-        o.put("t", tok.getSpannedText());
+        o.put("t", getText(tok));
 
         JSONObject govs = new JSONObject();
-        Set<Edge> edges = tok.getIncomingEdges();
-        for(Edge e : edges)
+        EList<Edge> sEdges = tok.getSGraph().getInEdges(tok.getSId());
+
+        for (Edge e : sEdges)
         {
-          if(e.getEdgeType() == Edge.EdgeType.POINTING_RELATION)
+          if (!(e instanceof SRelation) || !(e.getSource() instanceof SToken))
+          {
+            continue;
+          }
+
+          SRelation sRelation = (SRelation) e;
+          SToken sTokSource = (SToken) sRelation.getSource();
+
+          if (sRelation instanceof SPointingRelation)
           {
             String label = "";
-            for(Annotation anno : e.getAnnotations())
+            for (SAnnotation anno : sRelation.getSAnnotations())
             {
-              if(anno.getNamespace() != null)
-              {
-                label = anno.getValue();
-                break;
-              }
+              label = anno.getSValueSTEXT();
+              break;
             }
 
-            if(e.getSource() == null)
+            if (sRelation.getSource() == null
+              || !tok2Int.containsKey(sTokSource))
             {
               govs.put("root", label);
             }
             else
             {
-              govs.put("" + id2pos.get(e.getSource().getId()), label);
+              govs.put(String.valueOf(tok2Int.get(sTokSource)), label);
             }
           }
         }
-        o.put("govs", govs);
 
+        o.put("govs", govs);
         JSONObject attris = new JSONObject();
 
         JSONObject tAttris = new JSONObject();
         String tokenColor = "black";
-        if(input.getMarkableExactMap().containsKey("" + tok.getId()))
+        if (input.getMarkedAndCovered().containsKey(tok))
         {
-          tokenColor = input.getMarkableExactMap().get("" + tok.getId());
+          int colorNumber = ((int) (long) input.getMarkedAndCovered().get(tok)) - 1;
+          tokenColor = MatchedNodeColors.values()[colorNumber].getHTMLColor();
         }
         tAttris.put("fill", tokenColor);
         tAttris.put("font", "11px Arial,Tahoma,Helvetica,Sans-Serif");
@@ -131,10 +205,9 @@ public class VakyarthaDependencyTree extends WriterVisualizer
         attris.put("t", tAttris);
         o.put("attris", attris);
 
-        theWriter.append("tokens[").append("" + counter).append("]=");
+        theWriter.append("tokens[").append("" + count++).append("]=");
         theWriter.append(o.toString().replaceAll("\n", " "));
         theWriter.append(";\n");
-        counter++;
       }
 
       println("</script>");
@@ -143,16 +216,17 @@ public class VakyarthaDependencyTree extends WriterVisualizer
       println("<body>");
 
       // the div to render the javascript to
-      println("<div id=\"holder\" style=\"background:white; position:relative;\"> </div>");
+      println(
+        "<div id=\"holder\" style=\"background:white; position:relative;\"> </div>");
 
       println("</body>");
       println("</html>");
     }
-
     catch (JSONException ex)
     {
       log.error(null, ex);
-    }    catch (IOException ex)
+    }
+    catch (IOException ex)
     {
       log.error(null, ex);
     }
@@ -171,5 +245,22 @@ public class VakyarthaDependencyTree extends WriterVisualizer
     }
     theWriter.append(s);
     theWriter.append("\n");
+  }
+
+  private String getText(SToken tok)
+  {
+    SDocumentGraph sDocumentGraph = input.getSResult().getSDocumentGraph();
+    EList<STYPE_NAME> textRelations = new BasicEList<STYPE_NAME>();
+    textRelations.add(STYPE_NAME.STEXT_OVERLAPPING_RELATION);
+    EList<SDataSourceSequence> sequences = sDocumentGraph.
+      getOverlappedDSSequences(tok, textRelations);
+
+    if (sequences != null && sequences.size() > 0)
+    {
+      return ((STextualDS) sequences.get(0).getSSequentialDS()).getSText().
+        substring(sequences.get(0).getSStart(), sequences.get(0).getSEnd());
+    }
+
+    return "qurar";
   }
 }
