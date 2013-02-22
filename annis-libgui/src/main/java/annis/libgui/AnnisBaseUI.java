ff7527bcf931bcd52501fa0ade21ab2b3afd0a4a
Thomas Krause <thomas@berlin-reloaded.de>
2013-02-22 18:51:12 +0100

    load visualizer jar by dynamic version number

--- annis-libgui/src/main/java/annis/libgui/AnnisBaseUI.java
+++ annis-libgui/src/main/java/annis/libgui/AnnisBaseUI.java
@@ -363,7 +363,8 @@ public class AnnisBaseUI extends UI implements PluginSystem, Serializable
 
     File baseDir = VaadinService.getCurrent().getBaseDirectory();
     
-    File builtin = new File(baseDir, "WEB-INF/lib/annis-visualizers-3.0.0-SNAPSHOT.jar");
+    File builtin = new File(baseDir, "WEB-INF/lib/annis-visualizers-" 
+      + getVersionNumber() + ".jar");
     pluginManager.addPluginsFrom(builtin.toURI());
     log.info("added plugins from {}", builtin.getPath());
     
