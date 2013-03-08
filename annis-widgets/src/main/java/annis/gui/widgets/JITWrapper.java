6458297596b0793878686f902516e15499ddfe75
ruettet <tom.ruette@gmail.com>
2013-03-08 14:27:43 +0100

    update to latest korpling file

--- annis-widgets/src/main/java/annis/gui/widgets/JITWrapper.java
+++ annis-widgets/src/main/java/annis/gui/widgets/JITWrapper.java
@@ -1,36 +1,32 @@
 /*
- * Copyright 2012 Corpuslinguistic working group Humboldt University Berlin.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2012 Corpuslinguistic working group Humboldt University Berlin.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package annis.gui.widgets;
 
 import com.vaadin.annotations.JavaScript;
 import com.vaadin.server.PaintException;
 import com.vaadin.server.PaintTarget;
 import com.vaadin.ui.AbstractComponent;
-<<<<<<< HEAD
-//import com.vaadin.ui.ClientWidget;
-=======
->>>>>>> ca2adc85c57b03bbd79ff57ef1e28a61736cd0ff
 import com.vaadin.ui.LegacyComponent;
 import java.util.Map;
 import java.util.Properties;
 
 /**
- *
- * @author Benjamin Weißenfels <b.pixeldrama@gmail.com>
- */
+*
+* @author Benjamin Weißenfels <b.pixeldrama@gmail.com>
+*/
 @JavaScript({"jquery-1.8.2.min.js", "rst_vis.js"})
 public class JITWrapper extends AbstractComponent implements LegacyComponent
 {
@@ -73,8 +69,4 @@ public class JITWrapper extends AbstractComponent implements LegacyComponent
   }
   
   
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ca2adc85c57b03bbd79ff57ef1e28a61736cd0ff
