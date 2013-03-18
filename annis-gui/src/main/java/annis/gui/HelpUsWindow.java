c15bff36d696c8cb7b5d1c4bfd0cede3e6df2a46
Benjamin Weißenfels <b.pixeldrama@gmail.com>
2013-03-18 18:41:36 +0100

    Make the help window a little little bit nicer.

--- annis-gui/src/main/java/annis/gui/HelpUsWindow.java
+++ annis-gui/src/main/java/annis/gui/HelpUsWindow.java
@@ -24,75 +24,79 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * A window displaying an invitation to participate in the development of ANNIS. 
+ * A window displaying an invitation to participate in the development of ANNIS.
+ *
  * @author Thomas Krause <thomas.krause@alumni.hu-berlin.de>
  */
 public class HelpUsWindow extends Window
 {
-  
+
   private static final Logger log = LoggerFactory.getLogger(HelpUsWindow.class);
-    
+
   private VerticalLayout layout;
-  
+
   public HelpUsWindow()
   {
     setSizeFull();
     layout = new VerticalLayout();
     setContent(layout);
-    
+
     layout.setSizeFull();
     layout.setMargin(new MarginInfo(false, false, true, false));
-    
+
     HorizontalLayout hLayout = new HorizontalLayout();
     hLayout.setSizeFull();
     hLayout.setMargin(false);
-    
+
     VerticalLayout labelLayout = new VerticalLayout();
     labelLayout.setMargin(true);
     labelLayout.setSizeFull();
-    
+
     Label lblOpenSource = new Label();
-    
+
     lblOpenSource.setValue(
-      "<p>ANNIS is <a href=\"http://opensource.org/osd\">Open Source</a> software. "
-      + "This means you are free to download the source code and add new features or make other adjustments to ANNIS on your own.<p/>"
+      "<h1>ANNIS is <a href=\"http://opensource.org/osd\">Open Source</a> "
+      + "software.</h1>"
+      + "<p>This means you are free to download the source code and add new "
+      + "features or make other adjustments to ANNIS on your own.<p/>"
       + "Here are some examples how you can help ANNIS:"
       + "<ul>"
       + "<li>Fix or report problems (bugs) you encounter when using the ANNIS software.</li>"
       + "<li>Add new features.</li>"
       + "<li>Enhance the documentation</li>"
       + "</ul>"
-      + "<p>Feel free to visit our GitHub page for more information: <a href=\"https://github.com/korpling/ANNIS\" target=\"_blank\">https://github.com/korpling/ANNIS</a></p>"
-    );
+      + "<p>Feel free to visit our GitHub page for more information: <a href=\"https://github.com/korpling/ANNIS\" target=\"_blank\">https://github.com/korpling/ANNIS</a></p>");
     lblOpenSource.setContentMode(ContentMode.HTML);
     lblOpenSource.setStyleName("opensource");
     lblOpenSource.setWidth("100%");
     lblOpenSource.setHeight("-1px");
     labelLayout.addComponent(lblOpenSource);
-    
+
     Link lnkFork = new Link();
-    lnkFork.setResource(new ExternalResource("https://github.com/korpling/ANNIS"));
-    lnkFork.setIcon(new ExternalResource("https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"));
+    lnkFork.setResource(
+      new ExternalResource("https://github.com/korpling/ANNIS"));
+    lnkFork.setIcon(new ExternalResource(
+      "https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"));
     lnkFork.setTargetName("_blank");
-    
+
     hLayout.addComponent(labelLayout);
     hLayout.addComponent(lnkFork);
     hLayout.setComponentAlignment(labelLayout, Alignment.TOP_LEFT);
     hLayout.setComponentAlignment(lnkFork, Alignment.TOP_RIGHT);
     hLayout.setExpandRatio(labelLayout, 1.0f);
-    
+
     layout.addComponent(hLayout);
-    
+
     final HelpUsWindow finalThis = this;
-    
+
     Button btOK = new Button("OK");
     btOK.addClickListener(new OkClickListener(finalThis));
     layout.addComponent(btOK);
-    
+
     layout.setComponentAlignment(hLayout, Alignment.MIDDLE_CENTER);
     layout.setComponentAlignment(btOK, Alignment.MIDDLE_CENTER);
     layout.setExpandRatio(hLayout, 1.0f);
-    
+
   }
 
   private static class OkClickListener implements Button.ClickListener
