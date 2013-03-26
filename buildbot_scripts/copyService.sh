aa9d81ed7691daf9ef1e9b24dc52713c026ff95f
Thomas Krause <thomas@berlin-reloaded.de>
2013-03-26 16:00:23 +0100

    allow to set service port in build bot

--- buildbot_scripts/copyService.sh
+++ buildbot_scripts/copyService.sh
@@ -2,6 +2,7 @@
 
 #conf
 export ANNIS_HOME=$1
+export ANNIS_PORT=$2
 
 ANNIS_VERSION=3.0.0-SNAPSHOT
 DIST_DIR=annis-service/target/annis-service-$ANNIS_VERSION-distribution/annis-service-$ANNIS_VERSION
@@ -15,3 +16,9 @@ cp -Rf $DIST_DIR/sql/* $ANNIS_HOME/sql/
 cp -Rf $DIST_DIR/bin/* $ANNIS_HOME/bin/
 cp -Rf $DIST_DIR/conf/spring/* $ANNIS_HOME/conf/spring
 cp $DIST_DIR/conf/annis-service.properties $ANNIS_HOME/conf/
+
+# replace the port if requested
+if [ ! -z "$ANNIS_PORT" ]; then
+    echo "patching annis service port"
+	sed -i 's/^annis\.webservice-port=[0-9]\+\s*$/annis.webservice-port=$ANNIS_PORT/' conf/annis-service.properties
+fi  
