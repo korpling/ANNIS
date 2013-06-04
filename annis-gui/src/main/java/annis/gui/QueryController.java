088108122c79f21cb4a80b7d78ad56f17a8bedc6
martink <martin.klotz@hu-berlin.de>
2013-06-04 16:16:35 +0200

    new method: getQueryDraft() returns text in query text field

--- annis-gui/src/main/java/annis/gui/QueryController.java
+++ annis-gui/src/main/java/annis/gui/QueryController.java
@@ -437,6 +437,11 @@ public class QueryController implements PagingCallback, Refresher.RefreshListene
     ui.getControlPanel().getQueryPanel().setCountIndicatorEnabled(false);
     return true;
   }
+  
+  public String getQueryDraft()
+  {
+    return ui.getControlPanel().getQueryPanel().getQuery();    
+  }
 
   private static class MatchListType extends GenericType<List<Match>>
   {
