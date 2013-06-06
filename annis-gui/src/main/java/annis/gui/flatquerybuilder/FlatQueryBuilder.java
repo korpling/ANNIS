c2a06fb63047eb6c0b3a9e758ddcd674b73f42c5
ruettet <tom.ruette@gmail.com>
2013-06-06 11:53:15 +0200

    no need for unused imports

--- annis-gui/src/main/java/annis/gui/flatquerybuilder/FlatQueryBuilder.java
+++ annis-gui/src/main/java/annis/gui/flatquerybuilder/FlatQueryBuilder.java
@@ -27,7 +27,6 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.NativeSelect;
-import com.vaadin.ui.OptionGroup;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ChameleonTheme;
