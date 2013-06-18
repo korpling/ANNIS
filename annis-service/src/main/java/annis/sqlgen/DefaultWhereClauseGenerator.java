8770ad1fb56caca20031d8ef02778ca50e6da493
Thomas Krause <thomas@berlin-reloaded.de>
2013-06-18 10:56:32 +0200

    "pre" is not unique any longer, we also have to check for component id and corpus_ref
    fixes #138

--- annis-service/src/main/java/annis/sqlgen/DefaultWhereClauseGenerator.java
+++ annis-service/src/main/java/annis/sqlgen/DefaultWhereClauseGenerator.java
@@ -540,12 +540,22 @@ public class DefaultWhereClauseGenerator extends AbstractWhereClauseGenerator
     List<Long> corpusList = queryData.getCorpusList();
     TableAccessStrategy tas = tables(null);
     String pre1 = tables(node).aliasedColumn(RANK_TABLE, "pre");
+    String componentID1 = tables(node).aliasedColumn(COMPONENT_TABLE, "id");
+    String corpusRef1 = tables(node).aliasedColumn(NODE_TABLE, "corpus_ref");
+    
     String parent = tas.column("children", tas.columnName(RANK_TABLE, "parent"));
     String pre = tas.column("children", tas.columnName(RANK_TABLE, "pre"));
+    String componentID = tas.column("children", tas.columnName(COMPONENT_TABLE, "id"));;
+    String corpusRef = tas.column("children", tas.columnName(NODE_TABLE, "corpus_ref"));;
+    
+    
     StringBuffer sb = new StringBuffer();
     sb.append("(SELECT count(DISTINCT " + pre + ")\n");
     sb.append("\tFROM " + tas.tableName(RANK_TABLE) + " AS children\n");
-    sb.append("\tWHERE " + parent + " = " + pre1 + " AND toplevel_corpus IN("
+    sb.append("\tWHERE " + parent + " = " + pre1 
+      + " AND " + componentID1 + " = " + componentID
+      + " AND " + corpusRef1 + " = " + corpusRef
+      + " AND toplevel_corpus IN("
       + (corpusList.isEmpty() ? "NULL" : StringUtils.join(corpusList, ","))
       + ")" + ")");
     QueryNode.Range arity = node.getArity();
