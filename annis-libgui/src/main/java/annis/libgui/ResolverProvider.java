cc2502329836e9d0f0f99bdf7db2958481ac15b0
Benjamin Weißenfels <b.pixeldrama@gmail.com>
2013-08-30 15:13:36 +0200

    Refactor ResolverProvider. This interface is moved to annis-libgui and the implementation is seperated from ResultPanel.

--- annis-libgui/src/main/java/annis/libgui/ResolverProvider.java
+++ annis-libgui/src/main/java/annis/libgui/ResolverProvider.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package annis.gui.resultview;
+package annis.libgui;
 
 import annis.resolver.ResolverEntry;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SDocument;
