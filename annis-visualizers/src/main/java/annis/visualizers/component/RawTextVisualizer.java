2d3919205649836f335e495f88ee2f7297250044
Benjamin Weißenfels <b.pixeldrama@gmail.com>
2013-10-30 14:49:53 +0100

    Check, wether the token layer is artificial or not.

--- annis-visualizers/src/main/java/annis/visualizers/component/RawTextVisualizer.java
+++ annis-visualizers/src/main/java/annis/visualizers/component/RawTextVisualizer.java
@@ -24,19 +24,33 @@ import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Layout;
 import com.vaadin.ui.Panel;
+import com.vaadin.ui.VerticalLayout;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.SaltProject;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.STextualDS;
+import java.util.regex.Pattern;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 /**
- * Renders the plain text from the text table. Therefore this visualizer is
- * faster, than a visualizer which has to traverse a {@link SaltProject} for
- * getting the spanned {@link STextualDS}.
+ * Renders the plain text from the text table.
+ *
+ * <p>
+ * Therefore this visualizer is faster, than a visualizer which has to traverse
+ * a {@link SaltProject} for getting the spanned {@link STextualDS}.</p>
+ *
+ * <p>
+ * <strong>Mappings</strong>: <code>vertical: true | false</code> - Defines the
+ * alignment of multiple text, e. g. for parralel corpora it is convenient
+ * way.</p>
  *
  * @author Benjamin Weißenfels <b.pixeldrama@gmail.com>
  */
 @PluginImplementation
 public class RawTextVisualizer extends AbstractVisualizer<Panel> {
+
+    private final String NO_TEXT = "no text available";
+
+    // pattern for checking the token layer
+    private final Pattern whiteSpaceMatcher = Pattern.compile("^\\s+$");
 
     @Override
     public String getShortName() {
@@ -51,31 +65,84 @@ public class RawTextVisualizer extends AbstractVisualizer<Panel> {
     @Override
     public Panel createComponent(VisualizerInput visInput, VisualizationToggle visToggle) {
 
+        // get config for alignment
+        boolean vertical = Boolean.parseBoolean(visInput.getMappings().getProperty("vertical", "true"));
+
+        // get the texts
         RawTextWrapper texts = visInput.getRawText();
+
+        // create the main panel
         Panel p = new Panel();
+        p.setSizeFull();
 
-        if (texts != null && texts.hasMultipleTexts()) {
+        Layout l;
 
-            Layout l = new HorizontalLayout();
-            l.setSizeFull();
-
-            for (int i = 0; i < texts.getTexts().size(); i++) {
-                Label text = new Label(texts.getTexts().get(i), ContentMode.TEXT);
-                text.setCaption("text " + (i + 1));
-                l.addComponent(text);
-            }
-
-            p.setContent(l);
-
-        } else if (texts != null && visInput.getRawText().hasTexts()) {
-            Label text = new Label(texts.getFirstText(), ContentMode.TEXT);
+        if (texts == null) {
+            Label text = new Label(NO_TEXT);
+            text.setSizeFull();
             p.setContent(text);
-        } else {
-            Label text = new Label("n/a");
-            p.setContent(text);
+            return p;
         }
 
+        if (texts.hasMultipleTexts()) {
+
+            // set the aligmnent
+            if (vertical) {
+                l = new VerticalLayout();
+            } else {
+                l = new HorizontalLayout();
+            }
+
+            // limit the size to the parent panel.
+            l.setSizeFull();
+
+            // add the texts to the layout
+            for (int i = 0; i < texts.getTexts().size(); i++) {
+
+                String s = texts.getTexts().get(i);
+                Label lblText;
+
+                // check if the text is empty
+                if (s == null || hasOnlyWhiteSpace(s)) {
+                    lblText = new Label(NO_TEXT);
+                } else {
+                    lblText = new Label(s, ContentMode.TEXT);
+                }
+
+                lblText.setCaption("text " + (i + 1));
+                l.addComponent(lblText);
+            }
+
+            // apply the panel
+            p.setContent(l);
+            return p;
+
+        }
+
+        Label lblText;
+        if (texts.hasTexts() && !hasOnlyWhiteSpace(texts.getFirstText())) {
+            lblText = new Label(texts.getFirstText(), ContentMode.TEXT);
+        } else {
+            lblText = new Label(NO_TEXT);
+        }
+
+        lblText.setSizeFull();
+        p.setContent(lblText);
+
         return p;
+    }
+
+    /**
+     * Checks whether a string contains only whitespace. This is the cases for
+     * corpora with an artificial token layer.
+     *
+     * @param text The text which is checked. Throws
+     * {@link NullPointerException} when text is empty.
+     *
+     * @return true if only whitespace in there.
+     */
+    public boolean hasOnlyWhiteSpace(String text) {
+        return whiteSpaceMatcher.matcher(text).matches();
     }
 
 }
---
+++ annis-visualizers/src/test/java/annis/visualizers/component/RawTextVisualizerTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2013 SFB 632.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package annis.visualizers.component;
+
+import annis.libgui.VisualizationToggle;
+import annis.libgui.visualizers.VisualizerInput;
+import com.vaadin.ui.Panel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author Benjamin Weißenfels <b.pixeldrama@gmail.com>
+ */
+public class RawTextVisualizerTest {
+
+    private RawTextVisualizer vis;
+
+    @Before
+    public void setup() {
+        vis = new RawTextVisualizer();
+    }
+
+    @Test
+    public void testIsUsingRawText() {
+        assertTrue(
+                vis.getShortName() + " must use the raw text",
+                vis.isUsingRawText());
+    }
+
+    @Test
+    public void testIsUsingText() {
+        assertFalse(
+                vis.getShortName() + " never uses the text from a salt project, for a dramatic performance increase",
+                vis.isUsingText());
+    }
+
+
+    /**
+     * Test of hasOnlyWhiteSpace method, of class RawTextVisualizer.
+     */
+    @Test
+    public void testHasOnlyWhiteSpace() {
+        assertTrue(vis.hasOnlyWhiteSpace("     "));
+        assertFalse(vis.hasOnlyWhiteSpace(" x "));
+        assertFalse(vis.hasOnlyWhiteSpace("x "));
+        assertFalse(vis.hasOnlyWhiteSpace(" x"));
+    }
+
+}
