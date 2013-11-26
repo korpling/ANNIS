e2702fadcaa2aedecefaf8f2361f680f036ae16f
Thomas Krause <thomaskrause@posteo.de>
2013-11-26 15:54:07 +0100

    use a request handler for binary files since session handling got more complex in Vaadin 7.1 and we can't be sure that the session is correct

--- annis-gui/src/main/java/annis/gui/requesthandler/BinaryRequestHandler.java
+++ annis-gui/src/main/java/annis/gui/requesthandler/BinaryRequestHandler.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2009-2011 Collaborative Research Centre SFB 632
+ * Copyright 2013 Corpuslinguistic working group Humboldt University Berlin.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,57 +13,61 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package annis.gui.servlets;
 
-import annis.libgui.Helper;
+package annis.gui.requesthandler;
+
 import annis.libgui.AnnisBaseUI;
 import annis.libgui.AnnisUser;
+import annis.libgui.Helper;
 import annis.service.objects.AnnisBinaryMetaData;
 import com.sun.jersey.api.client.ClientHandlerException;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.UniformInterfaceException;
 import com.sun.jersey.api.client.WebResource;
+import com.vaadin.server.RequestHandler;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinResponse;
+import com.vaadin.server.VaadinSession;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URLEncoder;
 import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Map;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This Servlet provides binary-files with a stream of partial-content. The
- * first GET-request is answered with the status-code 206 Partial Content.
- *
+ * This request handler provides binary-files with a stream of partial-content. 
+ * The first GET-request is answered with the status-code 206 Partial Content.
+ * 
+ * @author Thomas Krause <krauseto@hu-berlin.de>
  * @author benjamin
- *
  */
-public class BinaryServlet extends HttpServlet
+public class BinaryRequestHandler implements RequestHandler
 {
-
-  private final static Logger log = LoggerFactory.getLogger(BinaryServlet.class);
-
-  private static final int MAX_LENGTH = 1024 * 1024; // max portion which is transfered over REST at once: 1MB
+  
+  private final static Logger log = LoggerFactory.getLogger(BinaryRequestHandler.class);
 
   @Override
-  public void init(ServletConfig config) throws ServletException
+  public boolean handleRequest(VaadinSession session, VaadinRequest request,
+    VaadinResponse response) throws IOException
   {
-    super.init(config);
+    if(request.getPathInfo().startsWith("/Binary") 
+      && "GET".equalsIgnoreCase(request.getMethod()))
+    {
+      doGet(session, request, response);
+      return true;
+    }
+    return false;
   }
-
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException
+  
+  public void doGet(VaadinSession session, VaadinRequest request, VaadinResponse response)
   {
     Map<String, String[]> binaryParameter = request.getParameterMap();
     String toplevelCorpusName = binaryParameter.get("toplevelCorpusName")[0];
@@ -72,17 +76,16 @@ public class BinaryServlet extends HttpServlet
 
     try
     {
-      ServletOutputStream out = response.getOutputStream();
+      OutputStream out = response.getOutputStream();
 
       String range = request.getHeader("Range");
 
-      HttpSession session = request.getSession();
       Object annisServiceURLObject = session.getAttribute(
-        AnnisBaseUI.WEBSERVICEURL_KEY);
+        Helper.KEY_WEB_SERVICE_URL);
 
       if (annisServiceURLObject == null || !(annisServiceURLObject instanceof String))
       {
-        throw new ServletException(
+        throw new IllegalStateException(
           "AnnisWebService.URL was not set as init parameter in web.xml");
       }
 
@@ -112,12 +115,11 @@ public class BinaryServlet extends HttpServlet
       {
         responseStatus200(metaBinaryRes, binaryRes, mimeType, out, response);
       }
-
-      out.flush();
     }
     catch (IOException ex)
     {
-      log.debug("IOException in BinaryServlet", ex);
+      log.error("IOException in BinaryRequestHandler", ex);
+      response.setStatus(500);
     }
     catch (ClientHandlerException ex)
     {
@@ -130,10 +132,10 @@ public class BinaryServlet extends HttpServlet
       response.setStatus(500);
     }
   }
-
+  
   private void responseStatus206(WebResource metaBinaryRes,
-    WebResource binaryRes, String mimeType, ServletOutputStream out,
-    HttpServletResponse response, String range) throws RemoteException, IOException
+    WebResource binaryRes, String mimeType, OutputStream out,
+    VaadinResponse response, String range) throws RemoteException
   {
     List<AnnisBinaryMetaData> allMeta = metaBinaryRes.get(
       new AnnisBinaryMetaDataListType());
@@ -171,15 +173,16 @@ public class BinaryServlet extends HttpServlet
         + (bm.getLength() - 1) + "/" + bm.getLength());
       response.setContentType(bm.getMimeType());
       response.setStatus(206);
-      response.setContentLength(lengthToFetch);
+//      response.setHeader(range, range);
+      response.setHeader("Content-Length", "" + lengthToFetch);
 
       writeFromServiceToClient(offset, lengthToFetch, binaryRes, out, mimeType);
     }
   }
 
   private void responseStatus200(WebResource metaBinaryRes,
-    WebResource binaryRes, String mimeType, ServletOutputStream out,
-    HttpServletResponse response) throws RemoteException, IOException
+    WebResource binaryRes, String mimeType, OutputStream out,
+    VaadinResponse response) throws RemoteException, IOException
   {
 
 
@@ -204,14 +207,14 @@ public class BinaryServlet extends HttpServlet
       response.setHeader("Content-Range",
         "bytes 0-" + (binaryMeta.getLength() - 1)
         + "/" + binaryMeta.getLength());
-      response.setContentLength(binaryMeta.getLength());
+      response.setHeader("Content-Length", "" + binaryMeta.getLength());
 
       getCompleteFile(binaryRes, mimeType, out);
     }
   }
 
   private void getCompleteFile(WebResource binaryRes, String mimeType,
-    ServletOutputStream out)
+    OutputStream out)
     throws RemoteException, IOException
   {
 
@@ -239,15 +242,23 @@ public class BinaryServlet extends HttpServlet
   }
 
   private void writeFromServiceToClient(int offset, int completeLength,
-    WebResource binaryRes, ServletOutputStream out, String mimeType) throws IOException
+    WebResource binaryRes, OutputStream out, String mimeType)
   {
 
-    ClientResponse response = binaryRes.path("" + offset).
-      path("" + completeLength)
-      .accept(mimeType).get(ClientResponse.class);
-    int copiedBytes = IOUtils.copy(response.getEntityInputStream(), out);
-    Validate.isTrue(copiedBytes == completeLength);
-    out.flush();
+    try
+    {
+      ClientResponse response = binaryRes.path("" + offset).
+        path("" + completeLength)
+        .accept(mimeType).get(ClientResponse.class);
+      int copiedBytes = IOUtils.copy(response.getEntityInputStream(), out);
+      Validate.isTrue(copiedBytes == completeLength);
+      out.flush();
+    }
+    catch(IOException ex)
+    {
+      log.debug("writing to client failed", ex);
+    }
+    
   }
 
   private static class AnnisBinaryMetaDataListType extends GenericType<List<AnnisBinaryMetaData>>
@@ -257,4 +268,5 @@ public class BinaryServlet extends HttpServlet
     {
     }
   }
+  
 }
