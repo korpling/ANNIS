korpling/ANNIS

commit 981e0dae66b3d2437e74d165d3d7b2039e2f65f0

output node_name and toplevel corpus only when needed

annis-service/src/main/java/annis/sqlgen/FindSqlGenerator.java
@@ -76,10 +76,11 @@ public class FindSqlGenerator extends AbstractUnionSqlGenerator<List<Match>>
 
       TableAccessStrategy tblAccessStr = tables(node);
       ids.add(tblAccessStr.aliasedColumn(NODE_TABLE, "id") + " AS id" + i);
-      ids.add(tblAccessStr.aliasedColumn(NODE_TABLE, "node_name")
-        + " AS node_name" + i);
       if(outputCorpusPath)
       {
+        ids.add(tblAccessStr.aliasedColumn(NODE_TABLE, "node_name")
+          + " AS node_name" + i);
+      
         ids.add(tblAccessStr.aliasedColumn(CORPUS_TABLE, "path_name")
           + " AS path_name" + i);
       }
@@ -99,12 +100,14 @@ public class FindSqlGenerator extends AbstractUnionSqlGenerator<List<Match>>
       }
     }
 
-    ids.add(tables(alternative.get(0)).aliasedColumn(NODE_TABLE,
-      "toplevel_corpus"));
-
+    if(outputCorpusPath)
+    {
+      ids.add(tables(alternative.get(0)).aliasedColumn(NODE_TABLE,
+        "toplevel_corpus"));
+    }
+    
     ids.add(tables(alternative.get(0)).aliasedColumn(NODE_TABLE,
       "corpus_ref"));
-
 
     return (isDistinct ? "DISTINCT" : "") + "\n" + indent + TABSTOP
       + StringUtils.join(ids, ", ");

commit 345aa0b3165ba9c3d928de5add88b13e161fded0

revision 981e0dae66b3d2437e74d165d3d7b2039e2f65f0 introduced a regression that made the frequency analysis unusable

annis-service/src/main/distribution/conf/spring/CommonSqlGenerator.xml
@@ -235,7 +235,7 @@ limitations under the License.
   <bean id="frequencySqlGeneratorBase" class="annis.sqlgen.FrequencySqlGenerator"
         parent="tableAccessStrategyFactory" lazy-init="true">
     
-    <property name="innerQuerySqlGenerator" ref="noCorpusPathFindSqlGenerator" />
+    <property name="innerQuerySqlGenerator" ref="frequencyFindSqlGenerator" />
 
     <property name="selectClauseSqlGenerator" ref="frequencySqlGenerator" />
     <property name="fromClauseSqlGenerators">
@@ -369,6 +369,7 @@ limitations under the License.
     <property name="optimizeDistinct" value="${annis.optimize-distinct}" />
     <property name="sortSolutions" value="${annis.annotate.sort-solutions}" />
     <property name="outputCorpusPath" value="true" />
+    <property name="outputToplevelCorpus" value="true" />
 
     <property name="selectClauseSqlGenerator" ref="findSqlGenerator" />
     <property name="corpusPathExtractor" ref="arrayCorpusPathExtractor"/>
@@ -378,12 +379,28 @@ limitations under the License.
     <property name="orderByClauseSqlGenerator" ref="findSqlGenerator" />
   </bean>
   
+  <bean id="frequencyFindSqlGenerator" class="annis.sqlgen.FindSqlGenerator"
+          parent="tableAccessStrategyFactory" lazy-init="true">
+
+    <property name="optimizeDistinct" value="${annis.optimize-distinct}" />
+    <property name="sortSolutions" value="false" />
+    <property name="outputCorpusPath" value="false" />
+    <property name="outputToplevelCorpus" value="true" />
+
+    <property name="selectClauseSqlGenerator" ref="frequencyFindSqlGenerator" />
+    <property name="limitOffsetClauseSqlGenerator" ref="commonLimitOffsetGenerator" />
+    <property name="fromClauseSqlGenerators" ref="defaultFromClauseGenerators" />
+    <property name="whereClauseSqlGenerators" ref="defaultWhereClauseGenerators" />
+    <property name="orderByClauseSqlGenerator" ref="frequencyFindSqlGenerator" />
+  </bean>
+  
   <bean id="noCorpusPathFindSqlGenerator" class="annis.sqlgen.FindSqlGenerator"
         parent="tableAccessStrategyFactory" lazy-init="true">
 
     <property name="optimizeDistinct" value="${annis.optimize-distinct}" />
     <property name="sortSolutions" value="${annis.annotate.sort-solutions}" />
     <property name="outputCorpusPath" value="false" />
+    <property name="outputToplevelCorpus" value="false" />
 
     <property name="selectClauseSqlGenerator" ref="noCorpusPathFindSqlGenerator" />
     <property name="limitOffsetClauseSqlGenerator" ref="commonLimitOffsetGenerator" />

annis-service/src/main/java/annis/sqlgen/FindSqlGenerator.java
@@ -56,6 +56,7 @@ public class FindSqlGenerator extends AbstractUnionSqlGenerator<List<Match>>
   private boolean optimizeDistinct;
   private boolean sortSolutions;
   private boolean outputCorpusPath;
+  private boolean outputToplevelCorpus;
   private CorpusPathExtractor corpusPathExtractor;
 
   @Override
@@ -100,7 +101,7 @@ public class FindSqlGenerator extends AbstractUnionSqlGenerator<List<Match>>
       }
     }
 
-    if(outputCorpusPath)
+    if(outputToplevelCorpus)
     {
       ids.add(tables(alternative.get(0)).aliasedColumn(NODE_TABLE,
         "toplevel_corpus"));
@@ -276,6 +277,16 @@ public class FindSqlGenerator extends AbstractUnionSqlGenerator<List<Match>>
   public void setOutputCorpusPath(boolean outputCorpusPath)
   {
     this.outputCorpusPath = outputCorpusPath;
+  }
+
+  public boolean isOutputToplevelCorpus()
+  {
+    return outputToplevelCorpus;
+  }
+
+  public void setOutputToplevelCorpus(boolean outputToplevelCorpus)
+  {
+    this.outputToplevelCorpus = outputToplevelCorpus;
   }
   
   