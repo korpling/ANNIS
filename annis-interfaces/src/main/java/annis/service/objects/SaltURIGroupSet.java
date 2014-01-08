4755d725ea44c55d475915f161469c88949624fe
Thomas Krause <thomaskrause@posteo.de>
2014-01-08 15:54:15 +0100

    allow to parse raw strings in SaltURIGroupSet

--- annis-interfaces/src/main/java/annis/service/objects/SaltURIGroupSet.java
+++ annis-interfaces/src/main/java/annis/service/objects/SaltURIGroupSet.java
@@ -15,11 +15,20 @@
  */
 package annis.service.objects;
 
+import annis.CommonHelper;
+import annis.model.QueryNode;
 import java.io.Serializable;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import javax.xml.bind.annotation.XmlRootElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class is only a wrapper to transport the salt ids with the
@@ -30,7 +39,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class SaltURIGroupSet implements Serializable
 {
+  private static final Logger log = LoggerFactory.getLogger(SaltURIGroupSet.class);
+  
   private Map<Integer, SaltURIGroup> groups;
+  
   
   public SaltURIGroupSet()
   {
@@ -45,6 +57,53 @@ public class SaltURIGroupSet implements Serializable
   public void setGroups(Map<Integer, SaltURIGroup> groups)
   {
     this.groups = groups;
+  }
+  
+  /**
+   * Parses a string representation of a {@link SaltURIGroupSet}. 
+   * Each group is separated with a ";" and each Salt ID with a ",".
+   * Example: 
+   * {@code 
+   * salt://id1,salt://id2,salt://id3;salt://id4,salt://id5;salt://id6
+   * }
+   * 
+   * @param raw
+   * @return 
+   */
+  public static SaltURIGroupSet fromString(String raw)
+  {
+    SaltURIGroupSet saltIDs = new SaltURIGroupSet();
+
+
+    int i = 0;
+    for (String group : raw.split("\\s*;\\s*"))
+    {
+      SaltURIGroup urisForGroup = new SaltURIGroup();
+
+      for (String id : group.split("[,\\s]+"))
+      {
+        java.net.URI uri;
+        try
+        {
+          uri = new java.net.URI(id);
+
+          if (!"salt".equals(uri.getScheme()) || uri.getFragment() == null)
+          {
+            throw new URISyntaxException("not a Salt id", uri.toString());
+          }
+        }
+        catch (URISyntaxException ex)
+        {
+          log.error("Invalid syntax for ID " + id, ex);
+          continue;
+        }
+        urisForGroup.getUris().add(uri);
+      }
+      
+      saltIDs.getGroups().put(++i, urisForGroup);
+    }
+    
+    return saltIDs;
   }
   
   
