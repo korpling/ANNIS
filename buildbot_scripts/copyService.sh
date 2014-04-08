98255e7bb1ae40696852c9de22319074bd380a07
Thomas Krause <thomaskrause@posteo.de>
2014-04-08 16:54:37 +0200

    update version

--- buildbot_scripts/copyService.sh
+++ buildbot_scripts/copyService.sh
@@ -4,7 +4,7 @@
 export ANNIS_HOME=$1
 export ANNIS_PORT=$2
 
-ANNIS_VERSION=3.1.0-SNAPSHOT
+ANNIS_VERSION=3.1.0
 DIST_DIR=annis-service/target/annis-service-$ANNIS_VERSION-distribution/annis-service-$ANNIS_VERSION
 
 #code
