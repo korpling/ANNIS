1679ca89bea1d615678f67b62ca11cb451829b6f
Thomas Krause <thomaskrause@posteo.de>
2014-04-10 09:21:01 +0200

    ensure that a token always has it's own token index as left_token and right_token value (and not the one of another overlapping token)

--- annis-service/src/main/distribution/sql/left_token_right_token.sql
+++ annis-service/src/main/distribution/sql/left_token_right_token.sql
@@ -2,18 +2,28 @@ ALTER TABLE _node ADD left_token integer, ADD right_token integer;
 
 UPDATE _node AS parent SET 
 left_token = (
-  SELECT min(token_index) FROM _node AS child 
-  WHERE 
-    parent.left = child.left 
-    AND parent.corpus_ref = child.corpus_ref 
-    AND parent.text_ref = child.text_ref 
-    AND child.token_index IS NOT NULL
+  CASE
+  WHEN parent.token_index IS NULL THEN (
+    SELECT min(token_index) FROM _node AS child 
+    WHERE 
+      parent.left = child.left 
+      AND parent.corpus_ref = child.corpus_ref 
+      AND parent.text_ref = child.text_ref 
+      AND child.token_index IS NOT NULL
+  ) 
+  ELSE parent.token_index
+  END
 ), 
 right_token = (
-  SELECT max(token_index) FROM _node AS child 
-  WHERE 
-    parent.right = child.right 
-    AND parent.corpus_ref = child.corpus_ref 
-    AND parent.text_ref = child.text_ref 
-    AND child.token_index IS NOT NULL
+  CASE
+    WHEN parent.token_index IS NULL THEN (
+    SELECT max(token_index) FROM _node AS child 
+    WHERE 
+      parent.right = child.right 
+      AND parent.corpus_ref = child.corpus_ref 
+      AND parent.text_ref = child.text_ref 
+      AND child.token_index IS NOT NULL
+  )
+  ELSE parent.token_index
+  END
 );
