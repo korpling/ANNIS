7cb55b70b81204713efa6666651988f87824641c
Thomas Krause <thomaskrause@posteo.de>
2014-08-19 10:51:50 +0200

    use fragments for tab navigation

--- annis-gui/src/main/java/annis/gui/AdminUI.java
+++ annis-gui/src/main/java/annis/gui/AdminUI.java
@@ -30,8 +30,10 @@ import annis.libgui.AnnisBaseUI;
 import annis.libgui.Helper;
 import com.sun.jersey.api.client.WebResource;
 import com.vaadin.annotations.Theme;
+import com.vaadin.server.Page;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.VerticalLayout;
@@ -43,48 +45,61 @@ import java.util.List;
  * @author Thomas Krause <krauseto@hu-berlin.de>
  */
 @Theme("annis")
-public class AdminUI extends AnnisBaseUI implements UIView, LoginListener
+public class AdminUI extends AnnisBaseUI implements UIView, LoginListener,
+  Page.UriFragmentChangedListener, TabSheet.SelectedTabChangeListener
 {
 
   private VerticalLayout layout;
-  
+
   private UserController userController;
 
   private GroupController groupManagementController;
 
   private CorpusController corpusController;
-  
+
   private final List<UIView.Listener> listeners = new LinkedList<>();
-  
+
+  private TabSheet tabSheet;
+
+  private ImportPanel importPanel;
+
+  private CorpusAdminPanel corpusAdminPanel;
+
+  private UserManagementPanel userManagementPanel;
+
+  private GroupManagementPanel groupManagementPanel;
+
   @Override
   protected void init(VaadinRequest request)
   {
     super.init(request);
-    
+
     WebResource rootResource = Helper.getAnnisWebResource();
-    
+
     UserManagement userManagement = new UserManagement();
     userManagement.setRootResource(rootResource);
     GroupManagement groupManagement = new GroupManagement();
     groupManagement.setRootResource(rootResource);
     CorpusManagement corpusManagement = new CorpusManagement();
     corpusManagement.setRootResource(rootResource);
-    
-    CorpusAdminPanel corpusAdminPanel = new CorpusAdminPanel();
+
+    corpusAdminPanel = new CorpusAdminPanel();
     corpusController = new CorpusController(corpusManagement, corpusAdminPanel,
       this);
-    
-    UserManagementPanel userManagementPanel = new UserManagementPanel();
+
+    userManagementPanel = new UserManagementPanel();
     userController = new UserController(userManagement,
       userManagementPanel, this);
-    
-    GroupManagementPanel groupManagementPanel = new GroupManagementPanel();
+
+    groupManagementPanel = new GroupManagementPanel();
     groupManagementController = new GroupController(groupManagement,
       corpusManagement,
       groupManagementPanel, this, userManagementPanel);
-    
-    TabSheet tabSheet = new TabSheet();
-    tabSheet.addTab(new ImportPanel(), "Import Corpus", new ThemeResource(
+
+    importPanel = new ImportPanel();
+
+    tabSheet = new TabSheet();
+    tabSheet.addTab(importPanel, "Import Corpus", new ThemeResource(
       "images/tango-icons/16x16/document-save.png"));
     tabSheet.addTab(corpusAdminPanel, "Corpus management", new ThemeResource(
       "images/tango-icons/16x16/system-file-manager.png"));
@@ -93,65 +108,111 @@ public class AdminUI extends AnnisBaseUI implements UIView, LoginListener
     tabSheet.addTab(groupManagementPanel, "Group management", new ThemeResource(
       "images/tango-icons/16x16/system-users.png"));
     tabSheet.setSizeFull();
-    
-    tabSheet.addSelectedTabChangeListener(
-      new TabSheet.SelectedTabChangeListener()
-      {
-        
-        @Override
-        public void selectedTabChange(TabSheet.SelectedTabChangeEvent event)
-        {
-          for (UIView.Listener l : listeners)
-          {
-            l.selectedTabChanged(event.getTabSheet().getSelectedTab());
-          }
-        }
-      });
-    
+
+    tabSheet.addSelectedTabChangeListener(this);
+
     MainToolbar toolbar = new MainToolbar(null);
     addExtension(toolbar.getScreenshotExtension());
     toolbar.addLoginListener(AdminUI.this);
-    
+
     layout = new VerticalLayout(toolbar, tabSheet);
     layout.setSizeFull();
-    
+
     layout.setExpandRatio(toolbar, 0.0f);
     layout.setExpandRatio(tabSheet, 1.0f);
-    
+
     setContent(layout);
+
+    getPage().addUriFragmentChangedListener(this);
     
+    selectTabFromFragment(getPage().getUriFragment());
+
+  }
+
+  @Override
+  public void uriFragmentChanged(Page.UriFragmentChangedEvent event)
+  {
+    selectTabFromFragment(event.getUriFragment());
   }
   
+  private void selectTabFromFragment(String fragment)
+  {
+    switch (fragment)
+    {
+      case "import":
+        tabSheet.setSelectedTab(importPanel);
+        break;
+      case "corpora":
+        tabSheet.setSelectedTab(corpusAdminPanel);
+        break;
+      case "users":
+        tabSheet.setSelectedTab(userManagementPanel);
+        break;
+      case "groups":
+        tabSheet.setSelectedTab(groupManagementPanel);
+        break;
+    }
+  }
+
+  @Override
+  public void selectedTabChange(TabSheet.SelectedTabChangeEvent event)
+  {
+    Component selected = event.getTabSheet().getSelectedTab();
+    
+    for (UIView.Listener l : listeners)
+    {
+      l.selectedTabChanged(selected);
+    }
+    
+    if(selected == importPanel)
+    {
+      getPage().setUriFragment("import", false);
+    }
+    else if(selected == corpusAdminPanel)
+    {
+      getPage().setUriFragment("corpora", false);
+    }
+    else if(selected == userManagementPanel)
+    {
+      getPage().setUriFragment("users", false);
+    }
+    else if(selected == groupManagementPanel)
+    {
+      getPage().setUriFragment("groups", false);
+    }
+    
+  }
+
   @Override
   public void addListener(UIView.Listener listener)
   {
     listeners.add(listener);
   }
-  
+
   @Override
   public void showInfo(String info, String description)
   {
     Notification.show(info, description, Notification.Type.HUMANIZED_MESSAGE);
   }
-  
+
   @Override
   public void showBackgroundInfo(String info, String description)
   {
     Notification.show(info, description, Notification.Type.TRAY_NOTIFICATION);
   }
-  
+
   @Override
   public void showWarning(String error, String description)
   {
     Notification.show(error, description, Notification.Type.WARNING_MESSAGE);
   }
-  
+
   @Override
   public void showError(String error, String description)
   {
     Notification.show(error, description, Notification.Type.ERROR_MESSAGE);
   }
-  
+
   @Override
   public void onLogin()
   {
@@ -160,7 +221,7 @@ public class AdminUI extends AnnisBaseUI implements UIView, LoginListener
       l.loginChanged(Helper.getAnnisWebResource());
     }
   }
-  
+
   @Override
   public void onLogout()
   {
@@ -169,5 +230,5 @@ public class AdminUI extends AnnisBaseUI implements UIView, LoginListener
       l.loginChanged(Helper.getAnnisWebResource());
     }
   }
-  
+
 }
