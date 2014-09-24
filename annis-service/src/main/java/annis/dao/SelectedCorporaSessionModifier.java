f3ef82f73cbafed2ac758d71e22f4cdcc1d3c9a2
Thomas Krause <thomaskrause@posteo.de>
2014-09-24 10:36:55 +0200

    initial implemention of using a view of "UNION ALL" instead of selecting the facts parent table (which causes several locks)

---
+++ annis-service/src/main/java/annis/dao/SelectedCorporaSessionModifier.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2014 SFB 632.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package annis.dao;
+
+import annis.ql.parser.QueryData;
+import com.google.common.base.Joiner;
+import java.util.LinkedList;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ *
+ * @author Thomas Krause <krauseto@hu-berlin.de>
+ */
+public class SelectedCorporaSessionModifier implements SqlSessionModifier
+{
+
+  @Override
+  public void modifySqlSession(JdbcTemplate jdbc,
+    QueryData queryData)
+  {
+    List<String> tables = new LinkedList<>();
+    for(Long corpusID : queryData.getCorpusList())
+    {
+      tables.add("SELECT * FROM facts_" + corpusID);
+    }
+    
+    jdbc.execute(
+      "CREATE OR REPLACE TEMPORARY VIEW selected_facts AS ("
+      + Joiner.on("\nUNION ALL\n").join(tables) + ")");
+    
+  }
+  
+}
