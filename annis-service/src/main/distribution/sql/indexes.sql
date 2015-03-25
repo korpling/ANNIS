39ee393a0b1bf2ca438940d720cbf836e8467a95
Thomas Krause <thomaskrause@posteo.de>
2015-03-25 19:36:35 +0100

    use a fillfactor of 100% for the btree indexes since the facts table is static

--- annis-service/src/main/distribution/sql/indexes.sql
+++ annis-service/src/main/distribution/sql/indexes.sql
@@ -5,154 +5,186 @@
 CREATE INDEX idx__facts_component_id__:id
   ON facts_:id
   USING btree
-  (component_id, corpus_ref);
+  (component_id, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 
 CREATE INDEX idx__facts_edge_name__:id
   ON facts_:id
   USING btree
-  (edge_name varchar_pattern_ops, corpus_ref);
+  (edge_name varchar_pattern_ops, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 
 CREATE INDEX idx__facts_edge_namespace__:id
   ON facts_:id
   USING btree
-  (edge_namespace varchar_pattern_ops, corpus_ref);
+  (edge_namespace varchar_pattern_ops, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 
 CREATE INDEX idx__facts_edge_type__:id
   ON facts_:id
   USING btree
-  (edge_type, corpus_ref);
+  (edge_type, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 
 CREATE INDEX idx__facts_id__:id
   ON facts_:id
   USING btree
-  (id);
+  (id)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_is_token__:id
   ON facts_:id
   USING btree
-  (is_token, corpus_ref);
+  (is_token, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_left__:id
   ON facts_:id
   USING btree
-  ("left", corpus_ref);
+  ("left", corpus_ref)
+  WITH (FILLFACTOR=100);
 
 
 CREATE INDEX idx__facts_left_token__:id
   ON facts_:id
   USING btree
-  (left_token, corpus_ref);
+  (left_token, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 
 CREATE INDEX idx__facts_level__:id
   ON facts_:id
   USING btree
-  ("level", corpus_ref);
+  ("level", corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_node_annotext__:id
   ON facts_:id
   USING btree
-  (node_annotext varchar_pattern_ops, corpus_ref);
+  (node_annotext varchar_pattern_ops, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts__q_node_qannotext__:id
   ON facts_:id
   USING btree
-  (node_qannotext varchar_pattern_ops, corpus_ref);
+  (node_qannotext varchar_pattern_ops, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_edge_annotext__:id
   ON facts_:id
   USING btree
-  (edge_annotext varchar_pattern_ops, corpus_ref);
+  (edge_annotext varchar_pattern_ops, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts__edge_qannotext__:id
   ON facts_:id
   USING btree
-  (edge_qannotext varchar_pattern_ops, corpus_ref);
+  (edge_qannotext varchar_pattern_ops, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_node_name__:id
   ON facts_:id
   USING btree
-  (node_name varchar_pattern_ops, corpus_ref);
+  (node_name varchar_pattern_ops, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_node_namespace__:id
   ON facts_:id
   USING btree
-  (node_namespace varchar_pattern_ops, corpus_ref);
+  (node_namespace varchar_pattern_ops, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_parent__:id
   ON facts_:id
   USING btree
-  (parent, corpus_ref);
+  (parent, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 
 CREATE INDEX idx__facts_post__:id
   ON facts_:id
   USING btree
-  (post, corpus_ref);
+  (post, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 
 CREATE INDEX idx__facts_pre__:id
   ON facts_:id
   USING btree
-  (pre, corpus_ref);
+  (pre, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_right__:id
   ON facts_:id
   USING btree
-  ("right", corpus_ref);
+  ("right", corpus_ref)
+  WITH (FILLFACTOR=100);
 
 
 CREATE INDEX idx__facts_right_token__:id
   ON facts_:id
   USING btree
-  (right_token, corpus_ref);
+  (right_token, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_root__:id
   ON facts_:id
   USING btree
-  (root, corpus_ref);
+  (root, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_seg_name_index__:id
   ON facts_:id
   USING btree
-  (seg_name);
+  (seg_name)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_seg_index_index__:id
   ON facts_:id
   USING btree
-  (seg_index);
+  (seg_index)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_span__:id
   ON facts_:id
   USING btree
-  (span varchar_pattern_ops, corpus_ref);
+  (span varchar_pattern_ops, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_token_index__:id
   ON facts_:id
   USING btree
-  (token_index, corpus_ref);
+  (token_index, corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_corpus_ref_index__:id
   ON facts_:id
   USING btree
-  (corpus_ref);
+  (corpus_ref)
+  WITH (FILLFACTOR=100);
 
 CREATE INDEX idx__facts_text_ref_index__:id
   ON facts_:id
   USING btree
-  (text_ref);
+  (text_ref)
+  WITH (FILLFACTOR=100);
 
 ----- 2nd query
-CREATE INDEX idx__2nd_query_:id ON facts_:id (corpus_ref, text_ref,left_token, right_token);
+CREATE INDEX idx__2nd_query_:id ON facts_:id (corpus_ref, text_ref,left_token, right_token)
+  WITH (FILLFACTOR=100);
 
 -- optimize the select distinct
-CREATE INDEX idx_distinct_helper_:id ON facts_:id(id, corpus_ref, text_ref, left_token, right_token);
+CREATE INDEX idx_distinct_helper_:id ON facts_:id(id, corpus_ref, text_ref, left_token, right_token)
+  WITH (FILLFACTOR=100);
 
 -- allow simple searches (node, tok etc)
-CREATE INDEX idx__sample_n__:id ON facts_:id(n_sample);
-CREATE INDEX idx__sample_n_tok__:id ON facts_:id(n_sample) WHERE is_token = TRUE;
-CREATE INDEX idx__sample_n_na__:id ON facts_:id(n_na_sample);
+CREATE INDEX idx__sample_n__:id ON facts_:id(n_sample)
+  WITH (FILLFACTOR=100);
+CREATE INDEX idx__sample_n_tok__:id ON facts_:id(n_sample)
+  WITH (FILLFACTOR=100) WHERE is_token = TRUE;
+CREATE INDEX idx__sample_n_na__:id ON facts_:id(n_na_sample)
+  WITH (FILLFACTOR=100);
