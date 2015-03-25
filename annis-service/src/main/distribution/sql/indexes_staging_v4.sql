dee7d9a6ec42cd584861d3185f4fc95492c966fa
Thomas Krause <thomaskrause@posteo.de>
2015-03-25 19:52:56 +0100

    import will fail if corpus ID is 0 and we are updating the IDs

--- annis-service/src/main/distribution/sql/indexes_staging_v4.sql
+++ annis-service/src/main/distribution/sql/indexes_staging_v4.sql
@@ -1,10 +1,10 @@
-CREATE UNIQUE INDEX tmpidx_pk_node ON _node (id);
-CREATE UNIQUE INDEX tmpidx_pk_node_token ON _node(id) WHERE token_index is not null;
+CREATE INDEX tmpidx_pk_node ON _node (id);
+CREATE INDEX tmpidx_pk_node_token ON _node(id) WHERE token_index is not null;
 CREATE INDEX tmpidx_unique_name_appendix_helper ON _node ("name", corpus_ref, id); 
 
-CREATE UNIQUE INDEX tmpidx_pk_corpus ON _corpus (id);
+CREATE INDEX tmpidx_pk_corpus ON _corpus (id);
 
-CREATE UNIQUE INDEX tmpidx_pk_component ON _component (id);
+CREATE INDEX tmpidx_pk_component ON _component (id);
 
 CREATE INDEX tmpidx_fk1_rank ON _rank (node_ref);
 CREATE INDEX tmpidx__continuous_helper1 ON _rank (component_ref, parent, node_ref);
