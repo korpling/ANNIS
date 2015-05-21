6f37efc47193abc24f16f4ac553e1d489e6dbcd5
Thomas Krause <thomaskrause@posteo.de>
2015-05-21 13:09:15 +0200

    fix: was using local static ID instead of ":id" template

--- annis-service/src/main/distribution/sql/indexes_annotations.sql
+++ annis-service/src/main/distribution/sql/indexes_annotations.sql
@@ -2,5 +2,5 @@
 -- node_annotation --
 ---------------------
 CREATE INDEX idx__annotations__order_:id ON annotations_:id(namespace, name, occurences);
-CREATE INDEX idx__annotations__order2_:id ON annotations_1157 (namespace, "name", edge_namespace, edge_name, occurences DESC);
+CREATE INDEX idx__annotations__order2_:id ON annotations_:id (namespace, "name", edge_namespace, edge_name, occurences DESC);
 CREATE INDEX idx__annotations__value_:id ON annotations_:id(value);
