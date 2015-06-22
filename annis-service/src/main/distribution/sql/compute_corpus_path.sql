9b5e3b48054bb854183fa3e174d1d1cb852a85e9
Thomas Krause <thomaskrause@posteo.de>
2015-06-22 18:04:11 +0200

    also update toplevel corpus and make toplevel corpus part of the path

--- annis-service/src/main/distribution/sql/compute_corpus_path.sql
+++ annis-service/src/main/distribution/sql/compute_corpus_path.sql
@@ -7,10 +7,10 @@ UPDATE corpus AS child SET path_name =
     FROM corpus as ancestor
     WHERE
       child.pre >= ancestor.pre AND child.post <= ancestor.post
-      AND top_corpus_pre(:id) < ancestor.pre AND ancestor.post < top_corpus_post(:id)
+      AND top_corpus_pre(:id) <= ancestor.pre AND ancestor.post <= top_corpus_post(:id)
     ORDER BY ancestor.pre DESC
   ) AS sub
   GROUP BY child_id 
 )
 WHERE
-pre > top_corpus_pre(:id) AND post < top_corpus_post(:id);
+pre >= top_corpus_pre(:id) AND post <= top_corpus_post(:id);
