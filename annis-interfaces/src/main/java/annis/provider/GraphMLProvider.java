5a97586a3038735d1af27045da46d07594fd808f
Thomas Krause <thomaskrause@posteo.de>
2015-08-05 17:13:57 +0200

    use a separate class for converting Salt to GraphXML

--- annis-interfaces/src/main/java/annis/provider/GraphMLProvider.java
+++ annis-interfaces/src/main/java/annis/provider/GraphMLProvider.java
@@ -15,6 +15,7 @@
  */
 package annis.provider;
 
+import annis.utils.Salt2GraphML;
 import de.hu_berlin.german.korpling.saltnpepper.salt.SaltFactory;
 import de.hu_berlin.german.korpling.saltnpepper.salt.graph.Edge;
 import de.hu_berlin.german.korpling.saltnpepper.salt.graph.IdentifiableElement;
@@ -32,8 +33,10 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -92,250 +95,22 @@ public class GraphMLProvider implements MessageBodyWriter<SaltProject>
     OutputStream entityStream) throws IOException, WebApplicationException
   {
 
-    XMLOutputFactory factory = XMLOutputFactory.newFactory();
-    try
+    // collect document graphs from the salt project
+    List<SDocument> docs = new LinkedList<>();
+    List<SCorpusGraph> corpusGraphs = t.getSCorpusGraphs();
+    if(corpusGraphs != null)
     {
-      XMLStreamWriter w = factory.createXMLStreamWriter(entityStream);
-      w.setDefaultNamespace(NS);
-
-      w.writeStartDocument();
-
-      w.writeStartElement(NS, "graphml");
-      w.writeDefaultNamespace(NS);
-      w.writeNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
-      w.writeAttribute("http://www.w3.org/2001/XMLSchema-instance", 
-        "schemaLocation", NS + " http://graphml.graphdrawing.org/xmlns/1.1/graphml.xsd");
-      
-      Set<String> existingKeys = new HashSet<>();
-      IDManager ids = new IDManager();
-
-      // we always use the "salt::type" label 
-      w.writeStartElement(NS, "key");
-      w.writeAttribute(NS, "id", "salt::type");
-      w.writeAttribute(NS, "for", "all");
-      w.writeAttribute(NS, "attr.type", "string");
-      w.writeEndElement();
-      existingKeys.add("salt::type");
-      
-      EList<SCorpusGraph> corpusGraphs = t.getSCorpusGraphs();
-      if (corpusGraphs != null)
+      for(SCorpusGraph c : corpusGraphs)
       {
-        // first get all possible label names and write them as "key"
-        for(SCorpusGraph corpusGraph : corpusGraphs)
+        if(c.getSDocuments() != null)
         {
-          writeKeys(w, corpusGraph.getNodes(), existingKeys);
-          writeKeys(w, corpusGraph.getEdges(), existingKeys);
-          List<SDocument> docs = corpusGraph.getSDocuments();
-          if(docs != null)
-          {
-            for(SDocument d : docs)
-            {
-              writeKeys(w, d.getSDocumentGraph().getSNodes(), existingKeys);
-              writeKeys(w, d.getSDocumentGraph().getSRelations(), existingKeys);
-            }
-          }
-        }
-        
-        // write out the corpus structure as the "root" graph
-        for (SCorpusGraph corpusGraph : corpusGraphs)
-        {
-          List<SDocument> docs = corpusGraph.getSDocuments();
-          if(docs != null)
-          {
-            for(SDocument d : docs)
-            {
-              writeSDocumentGraph(w, d.getSDocumentGraph(), ids, existingKeys, true);
-            }
-          }
-        }
-      }
-      w.writeEndDocument();
-      w.close();
-    }
-    catch (XMLStreamException ex)
-    {
-      log.error("Could not write GraphML", ex);
-    }
-    entityStream.flush();
-  }
-  
-  private void writeKeys(XMLStreamWriter w, List<? extends LabelableElement> elements, Set<String> existing)
-    throws XMLStreamException
-  {
-    if (elements != null && !elements.isEmpty())
-    {
-      for (LabelableElement e : elements)
-      {
-        List<Label> labels = e.getLabels();
-        if (labels != null && !labels.isEmpty())
-        {
-          for (Label l : labels)
-          {
-            String id = l.getQName();
-            if (!existing.contains(id))
-            {
-              Object o = l.getValue();
-              String type = null;
-              if(o instanceof Boolean)
-              {
-                type = "boolean";
-              }
-              else if(o instanceof Integer)
-              {
-                type = "int";
-              }
-              else if(o instanceof Long)
-              {
-                type = "long";
-              }
-              else if(o instanceof Float)
-              {
-                type = "float";
-              }
-              else if(o instanceof Double)
-              {
-                type = "double";
-              }
-              else if(o instanceof String)
-              {
-                type = "string";
-              }
-              if(type != null)
-              {
-                w.writeStartElement(NS, "key");
-                w.writeAttribute(NS, "id", l.getQName());
-                w.writeAttribute(NS, "for", "all");
-                w.writeAttribute(NS, "attr.type", type);
-
-                w.writeEndElement();
-                existing.add(id);
-              }
-            }
-          }
+          docs.addAll(c.getSDocuments());
         }
       }
     }
-  }
-
-  private void writeLabels(XMLStreamWriter w, List<Label> labels,
-    Set<String> existingKeys) 
-    throws XMLStreamException
-  {
-    if(labels != null && !labels.isEmpty())
-    {
-      for(Label l : labels)
-      {
-        String key = l.getQName();
-        if(existingKeys.contains(key))
-        {
-          w.writeStartElement(NS, "data");
-          w.writeAttribute(NS, "key", key);
-          w.writeCharacters("" + l.getValue());
-          w.writeEndElement();
-        }
-      }
-    }
-  }
-  
-  /**
-   * Writes a data element describing the type of the object.
-   * @param w
-   * @param o
-   * @throws XMLStreamException 
-   */
-  private void writeType(XMLStreamWriter w, EObject o)
-    throws XMLStreamException
-  {
-    HashSet<STYPE_NAME> types = SaltFactory.eINSTANCE.convertClazzToSTypeName(o.getClass());
-    if(!types.isEmpty())
-    {
-      w.writeStartElement(NS, "data");
-      w.writeAttribute(NS, "key", "salt::type");
-      w.writeCharacters(types.iterator().next().getName());
-      w.writeEndElement();
-    }
-  }
-
-  private void writeNode(XMLStreamWriter w, Node c, 
-    IDManager ids,
-    Set<String> existingKeys) throws XMLStreamException
-  {
-    w.writeStartElement(NS, "node");
-    w.writeAttribute(NS, "id", ids.getID(c));
-    
-    writeType(w, c);
-    writeLabels(w, c.getLabels(), existingKeys);
-    w.writeEndElement();
-  }
-  
-  private void writeEdge(XMLStreamWriter w, Edge e, 
-    IDManager ids, Set<String> existingKeys) throws XMLStreamException
-  {
-    w.writeStartElement(NS, "edge");
-    w.writeAttribute(NS, "id", ids.getID(e));
-    w.writeAttribute(NS, "source", ids.getID(e.getSource()));
-    w.writeAttribute(NS, "target", ids.getID(e.getTarget()));
-    
-    writeType(w, e);
-    writeLabels(w, e.getLabels(), existingKeys);
-    
-    w.writeEndElement();
+    Salt2GraphML.convert(entityStream, docs);
   }
   
   
-  private void writeSDocumentGraph(XMLStreamWriter w, 
-    SDocumentGraph g, 
-    IDManager ids, Set<String> existingKeys, boolean includeDocLabels) throws XMLStreamException
-  {
-    List<SNode> nodes = g.getSNodes();
-    List<SRelation> relations = g.getSRelations();
-    // graphs without nodes are not allowed
-    if(nodes != null && !nodes.isEmpty())
-    {
-      w.writeStartElement(NS, "graph");
-      w.writeAttribute(NS, "id", ids.getID(g));
-      w.writeAttribute(NS, "edgedefault", "directed");
-      
-      if(includeDocLabels && g.getSDocument() != null)
-      {
-        writeLabels(w, g.getSDocument().getLabels(), existingKeys);
-      }
-      
-      for(SNode n : nodes)
-      {
-        writeNode(w, n, ids, existingKeys);
-      }
-      
-      if(relations != null)
-      {
-        for(SRelation e : relations)
-        {
-          writeEdge(w, e, ids, existingKeys);
-        }
-      }
-
-      w.writeEndElement();
-    }
-  }
-  
-  private static class IDManager
-  {
-    private final AtomicLong counter = new AtomicLong(0);
-    
-    private final Map<IdentifiableElement, String> existing = new HashMap<>();
-    
-    private String getID(IdentifiableElement e)
-    {
-     
-      String id = existing.get(e);
-      if(id == null)
-      {
-        id = "_" + counter.getAndIncrement();
-        existing.put(e, id);
-      }
-      return id;
-    }
-    
-  }
 
 }
