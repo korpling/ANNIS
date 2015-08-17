2af05a63d8a1caad3f9d895676a33c7bf58dbcec
Thomas Krause <thomaskrause@posteo.de>
2015-08-17 19:08:06 +0200

    update jenkins script to use the new python upgrade script

--- Misc/buildbot_scripts/hudson_script.sh
+++ Misc/buildbot_scripts/hudson_script.sh
@@ -1,21 +1,14 @@
 cd $WORKSPACE
 
-# Stop the AnnisService
-chmod u+x Misc/buildbot_scripts/stopService.sh
-./Misc/buildbot_scripts/stopService.sh /opt/annis/annis3-snapshot
-
-# Copy the the Annis webapp
+# Stop the old web application
 rm -Rf /home/annis/tomcat-annis/webapps/annis3-snapshot*
-cp annis-gui/target/annis-gui.war /home/annis/tomcat-annis/webapps/annis3-snapshot.war
 
-# Copy Annis Service
-chmod u+x Misc/buildbot_scripts/copyService.sh
-Misc/buildbot_scripts/copyService.sh /opt/annis/annis3-snapshot 5713
-
-# copy the original configuration
+# copy the original user configuration
 rm -R /home/annis/annis_snapshot_users/*
 cp -R /etc/annis/user_config/ /home/annis/annis_snapshot_users/
 
-# start service
-chmod u+x Misc/buildbot_scripts/startService.sh
-BUILD_ID=allow_to_run_as_daemon Misc/buildbot_scripts/startService.sh /opt/annis/annis3-snapshot/
+# deploy new service (including restart)
+BUILD_ID=allow_to_run_as_daemon Misc/upgrade_service.py /opt/annis/annis3-snapshot annis-service/target/annis-service-*-distribution.tar.gz
+
+# deploy the new web application
+cp annis-gui/target/annis-gui.war /home/annis/tomcat-annis/webapps/annis3-snapshot.war
