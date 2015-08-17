d9200593a6e3d5936375e209a759f9e9935c09ac
Thomas Krause <thomaskrause@posteo.de>
2015-08-17 21:08:53 +0200

    don't copy the sub-directory but only its files

--- Misc/buildbot_scripts/hudson_script.sh
+++ Misc/buildbot_scripts/hudson_script.sh
@@ -5,7 +5,7 @@ rm -Rf /home/annis/tomcat-annis/webapps/annis3-snapshot*
 
 # copy the original user configuration
 rm -R /home/annis/annis_snapshot_users/*
-cp -R /etc/annis/user_config/ /home/annis/annis_snapshot_users/
+cp -R /etc/annis/user_config/* /home/annis/annis_snapshot_users/
 
 # deploy new service (including restart)
 BUILD_ID=dontKillMe Misc/upgrade_service.py /opt/annis/annis3-snapshot annis-service/target/annis-service-*-distribution.tar.gz
