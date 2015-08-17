583f8e46bac1e1ac90ed133354f4c97ff0c35d77
Thomas Krause <thomaskrause@posteo.de>
2015-08-17 12:13:15 +0200

    adjust jenkins script to new path

--- Misc/buildbot_scripts/hudson_script.sh
+++ Misc/buildbot_scripts/hudson_script.sh
@@ -2,15 +2,23 @@ cd $WORKSPACE
 
 # Stop the AnnisService
 chmod u+x buildbot_scripts/stopService.sh
-./buildbot_scripts/stopService.sh /opt/annis/trunk
+./Misc/buildbot_scripts/stopService.sh /opt/annis/annis3-snapshot
 
 # Copy the the Annis webapp
-cp annis-gui/target/annis-gui.war /srv/tomcat/annis3.war
+#/home/annis/tomcat-annis/bin/shutdown.sh
+#sleep 5
+rm -Rf /home/annis/tomcat-annis/webapps/annis3-snapshot*
+cp annis-gui/target/annis-gui.war /home/annis/tomcat-annis/webapps/annis3-snapshot.war
+#BUILD_ID=allow_to_run_as_daemon /home/annis/tomcat-annis/bin/startup.sh
 
 # Copy Annis Service
-chmod u+x buildbot_scripts/copyService.sh
-buildbot_scripts/copyService.sh /opt/annis/trunk
+chmod u+x Misc/buildbot_scripts/copyService.sh
+Misc/buildbot_scripts/copyService.sh /opt/annis/annis3-snapshot 5713
+
+# copy the original configuration
+rm -R /home/annis/annis_snapshot_users/*
+cp -R /etc/annis/user_config/ /home/annis/annis_snapshot_users/
 
 # start service
-chmod u+x buildbot_scripts/startService.sh
-BUILD_ID=allow_to_run_as_daemon buildbot_scripts/startService.sh /opt/annis/trunk/
+chmod u+x Misc/buildbot_scripts/startService.sh
+BUILD_ID=allow_to_run_as_daemon buildbot_scripts/startService.sh /opt/annis/annis3-snapshot/
