417a6fa1c72ce1290ff56e0647f13c1f0182a049
Thomas Krause <thomaskrause@posteo.de>
2015-08-17 12:25:18 +0200

    another path was wrong

--- Misc/buildbot_scripts/hudson_script.sh
+++ Misc/buildbot_scripts/hudson_script.sh
@@ -1,15 +1,12 @@
 cd $WORKSPACE
 
 # Stop the AnnisService
-chmod u+x buildbot_scripts/stopService.sh
+chmod u+x Misc/buildbot_scripts/stopService.sh
 ./Misc/buildbot_scripts/stopService.sh /opt/annis/annis3-snapshot
 
 # Copy the the Annis webapp
-#/home/annis/tomcat-annis/bin/shutdown.sh
-#sleep 5
 rm -Rf /home/annis/tomcat-annis/webapps/annis3-snapshot*
 cp annis-gui/target/annis-gui.war /home/annis/tomcat-annis/webapps/annis3-snapshot.war
-#BUILD_ID=allow_to_run_as_daemon /home/annis/tomcat-annis/bin/startup.sh
 
 # Copy Annis Service
 chmod u+x Misc/buildbot_scripts/copyService.sh
