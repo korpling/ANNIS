f81a17efcdbaa020b5fae1068a386efa8c831ef0
Thomas Krause <thomaskrause@posteo.de>
2015-09-03 20:02:24 +0200

    updated changelog

--- Misc/changelog.py
+++ Misc/changelog.py
@@ -4,7 +4,7 @@ import json
 import io
 from subprocess import call
 
-milestone_id = "28"
+milestone_id = "29"
 
 call(["curl", "-H", "Accept: application/vnd.github.beta.full+json", "-o", "issues.json", "https://api.github.com/repos/korpling/ANNIS/issues?state=closed&milestone=" + milestone_id + "&sort=created"])
 
