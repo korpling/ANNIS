c8e23945c2a4cd4c008100915d1c07c38a0e9fb4
Thomas Krause <thomaskrause@posteo.de>
2015-09-21 13:48:09 +0200

    move login window code into own class

--- annis-gui/src/main/java/annis/gui/MainToolbar.java
+++ annis-gui/src/main/java/annis/gui/MainToolbar.java
@@ -123,12 +123,10 @@ public class MainToolbar extends HorizontalLayout
 
   public static final String LOGIN_MAXIMIZED_KEY = "login-window-maximized";
 
-  private final String loginURL;
+  
 
   public MainToolbar()
   {
-    this.loginURL = (String) VaadinSession.getCurrent().getAttribute(
-      LOGIN_URL_KEY);
 
     String bugmail = (String) VaadinSession.getCurrent().getAttribute(
       BUG_MAIL_KEY);
@@ -691,32 +689,7 @@ public class MainToolbar extends HorizontalLayout
 
   private Window createLoginWindow()
   {
-    Resource loginRes;
-    if (loginURL == null || loginURL.isEmpty())
-    {
-      loginRes = new ExternalResource(
-        Helper.getContext() + "/login");
-    }
-    else
-    {
-      loginRes = new ExternalResource(loginURL);
-    }
-
-    BrowserFrame frame = new BrowserFrame("login", loginRes);
-    frame.setWidth("100%");
-    frame.setHeight("100%");
-
-    windowLogin = new Window("ANNIS Login", frame);
-    windowLogin.setModal(true);
-
-    windowLogin.setWidth("400px");
-    windowLogin.setHeight("250px");
-    String loginMaximizedRaw = (String) getSession().getAttribute(
-      LOGIN_MAXIMIZED_KEY);
-    if (Boolean.parseBoolean(loginMaximizedRaw))
-    {
-      windowLogin.setWindowMode(WindowMode.MAXIMIZED);
-    }
+    windowLogin = new LoginWindow();
     windowLogin.center();
 
     return windowLogin;
