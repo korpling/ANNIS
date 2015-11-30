106d0de1f13590a32257b3f675b63d1af4c92a1a
Thomas Krause <thomaskrause@posteo.de>
2015-11-30 16:32:16 +0100

    document the new features of the "show_ns" mapping

--- annis-visualizers/src/main/java/annis/visualizers/component/grid/GridComponent.java
+++ annis-visualizers/src/main/java/annis/visualizers/component/grid/GridComponent.java
@@ -41,7 +41,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
