7a63f7249ae4a0bd0f1f5f3314ae297f8214f942
irina <irina.glushanok@gmail.com>
2016-03-10 19:20:06 +0100

    added option for vertical scroll bar

--- annis-visualizers/src/main/java/annis/visualizers/component/visjs/VisJsPanel.java
+++ annis-visualizers/src/main/java/annis/visualizers/component/visjs/VisJsPanel.java
@@ -7,9 +7,11 @@ import com.vaadin.ui.Panel;
 public class VisJsPanel extends Panel{
 	
 	VisJsPanel (VisualizerInput visInput){
-		this.setHeight("300px");
+		this.setHeight("400px");
+		//this.setHeight("30%");
 		this.setWidth("100%");
 		this.setScrollLeft(10);
+		this.setScrollTop(10);
 		VisJsComponent visjsComponent = new VisJsComponent(visInput);		
 		this.setContent(visjsComponent);
 	}
