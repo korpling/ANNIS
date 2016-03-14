b14ae4a0575e603adca896dd4aaf2be10eb68d7b
Thomas Krause <thomaskrause@posteo.de>
2016-03-14 14:28:59 +0100

    fix various issues with the dependency JS code, this should also fix #453

--- annis-visualizers/src/main/resources/annis/visualizers/iframe/dependency/vakyartha/vakyarthaDependency.js
+++ annis-visualizers/src/main/resources/annis/visualizers/iframe/dependency/vakyartha/vakyarthaDependency.js
@@ -16,45 +16,45 @@
 
 ///////////////////////// parameters ////////////////
 ////////////////////////////////////////////////////
-tab=8; 			// space between tokens
-line=25 		// line height
-dependencyspace=180; 	// y-space for dependency representation
-xoff=8; 		// placement of the start of a depdency link in relation to the center of the word
-linedeps=15; 		// y distance between two arrows arriving at the same token (rare case)
-pois=4; 		// size of arrow pointer
-tokdepdist=15; 		// distance between tokens and depdendency relation
-funccurvedist=8;	// distance between the function name and the curves highest point
-depminh = 20; 		// minimum height for dependency
+tab=8;  // space between tokens
+line=25;  // line height
+dependencyspace=180;  // y-space for dependency representation
+xoff=8; // placement of the start of a depdency link in relation to the center of the word
+linedeps=15;  // y distance between two arrows arriving at the same token (rare case)
+pois=4; // size of arrow pointer
+tokdepdist=15;  // distance between tokens and depdendency relation
+funccurvedist=8;  // distance between the function name and the curves highest point
+depminh = 20; // minimum height for dependency
 worddistancefactor = 8; // distant words get higher curves. this factor fixes how much higher.
 defaultattris={
   "font": '14px "Arial"',
   "text-anchor":'start'
 };
 attris = {
-  "t":		{
+  "t":  {
     "font": '18px "Arial"',
     "text-anchor":'start'
   },
-  "cat":	{
+  "cat": {
     "font": '14px "Times"',
     "text-anchor":'start',
     "fill": '#036'
   },
-  "lemma":	{
+  "lemma": {
     "font": '14px "Times"',
     "text-anchor":'start',
     "fill": '#036'
   },
-  "depline":	{
+  "depline": {
     "stroke": '#999',
     "stroke-width":'1'
   },
-  "deptext":	{
+  "deptext": {
     "font": '12px "Times"',
     "font-style":'italic',
     "fill": '#999'
   },
-  "form":	{
+  "form": {
     "font-style":'italic'
   }
 };
@@ -65,12 +65,12 @@ function Pnode(c,b){
   this.index=c;
   this.govs={};
 
-  if("govs"in b)this.govs=b["govs"];
+  if("govs"in b) {this.govs=b["govs"];}
   this.width=0;
   this.texts=new Object();
   this.svgs=new Object();
   var d=dependencyspace;
-  for(var h in shownfeatures){
+  for (var h in shownfeatures){
     var e=shownfeatures[h];
     this.texts[e]= b[e];
     var f=paper.text(currentx,d,b[e]);
@@ -83,19 +83,20 @@ function Pnode(c,b){
     f.index=c;
     this.svgs[e]=f;
     if(g>this.width)this.width=g;
-    d=d+line
-  };
+    d=d+line;
+  }
 
   svgwi=svgwi+this.width+tab;
   this.x=0;
   this.y=0;
   this.svgdep={};
 
-  this.deplineattris={},f.deptextattris={};
+  this.deplineattris={};
+  this.deptextattris={};
 
   if("attris"in b&&"depline"in b["attris"])this.deplineattris=b["attris"]["depline"];
-  if("attris"in b&&"deptext"in b["attris"])this.deptextattris=b["attris"]["deptext"]
-};
+  if("attris"in b&&"deptext"in b["attris"])this.deptextattris=b["attris"]["deptext"];
+}
 
 drawsvgDep=function(c,b,d,h,e,f,g,i,p){
   var l=paper.set();
@@ -118,7 +119,7 @@ drawsvgDep=function(c,b,d,h,e,f,g,i,p){
     var k=a.x+t.getBBox().width/2+funccurvedist/2;
     if(k+t.getBBox().width/2>svgwi)k=a.x-t.getBBox().width/2-funccurvedist/2;
     if(k-t.getBBox().width/2<0)k=a.x;
-    t.attr("x",k)
+    t.attr("x",k);
   }
   if(g in fcolors){
     var o="#"+fcolors[g];
@@ -130,50 +131,66 @@ drawsvgDep=function(c,b,d,h,e,f,g,i,p){
     });
     t.attr({
       fill:o
-    })
-  };
+    });
+  }
 
   j.attr(i);
   n.attr(i);
   l.push(t);
   l.push(j);
   l.push(n);
-  return l
+  return l;
 };
 
-drawDep=function(c,b,d,h){
+drawDep=function(c,b,d,h)
+{
   c=parseInt(c);
-  if(b=="root"){
+  var e;
+  var f;
+  var g;
+  var i;
+  if(b=="root")
+  {
     b=c;
     node2=words[c];
-    var e=node2.svgs[shownfeatures[0]].attr("x")+node2.svgs[shownfeatures[0]].width/2;
-    var f=node2.svgs[shownfeatures[0]].attr("y")-tokdepdist;
-    var g=e;
-    var i=0
-  }else{
+    e=node2.svgs[shownfeatures[0]].attr("x")+node2.svgs[shownfeatures[0]].width/2;
+    f=node2.svgs[shownfeatures[0]].attr("y")-tokdepdist;
+    g=e;
+    i=0;
+  }
+  else
+  {
     b=parseInt(b);
     var node1=words[b];
     var node2=words[c];
-    if(node1==null){
+    if(node1==null)
+    {
       delete node2.govs[b];
-      return
+      return;
     }
-    if(c<b)var g=node1.svgs[shownfeatures[0]].attr("x")+node1.svgs[shownfeatures[0]].width/2-xoff;else var g=node1.svgs[shownfeatures[0]].attr("x")+node1.svgs[shownfeatures[0]].width/2+xoff;
-    var e=node2.svgs[shownfeatures[0]].attr("x")+node2.svgs[shownfeatures[0]].width/2;
-    var i=node1.svgs[shownfeatures[0]].attr("y")+pois-tokdepdist;
-    var f=node2.svgs[shownfeatures[0]].attr("y")-tokdepdist-h*linedeps
-  };
+    if(c<b)
+    {
+      g=node1.svgs[shownfeatures[0]].attr("x")+node1.svgs[shownfeatures[0]].width/2-xoff;
+    }
+    else
+    {
+      g=node1.svgs[shownfeatures[0]].attr("x")+node1.svgs[shownfeatures[0]].width/2+xoff;
+    }
+    e=node2.svgs[shownfeatures[0]].attr("x")+node2.svgs[shownfeatures[0]].width/2;
+    i=node1.svgs[shownfeatures[0]].attr("y")+pois-tokdepdist;
+    f=node2.svgs[shownfeatures[0]].attr("y")-tokdepdist-h*linedeps;
+  }
 
-  node2.svgdep[b]=drawsvgDep(c,b,g,i,e,f,d,node2.deplineattris,node2.deptextattris)
+  node2.svgdep[b]=drawsvgDep(c,b,g,i,e,f,d,node2.deplineattris,node2.deptextattris);
 };
 
 drawalldeps=function(){
-  for(var c in words){
-    var b=words[c];
+  for(var w in words){
+    var b=words[w];
     var d=0;
-    for(var c in b.govs){
-      drawDep(b.index,c,b.govs[c],d);
-      d+=1
+    for(var key in b.govs){
+      drawDep(b.index,key,b.govs[key],d);
+      d+=1;
     }
   }
 };
@@ -185,9 +202,9 @@ makewords=function(){
   for(var c in tokens){
     var b=new Pnode(c,tokens[c]);
     words[c]=b;
-    currentx=currentx+b.width+tab
+    currentx=currentx+b.width+tab;
   }
-}
+};
 
 
 Raphael.fn.pointer=function(c,b,d,h){
@@ -196,8 +213,8 @@ Raphael.fn.pointer=function(c,b,d,h){
   var g=(d/2)+","+(d/2)+" "+(d/2)+","+(d/2)+" "+(d)+",0";
   var i=this.path("M"+e+"c"+f+"c"+g+"z");
   i.rotate(h);
-  return i
-}
+  return i;
+};
 
 
 /**
