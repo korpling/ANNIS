875166d8e6a1179d39b39a0765d8da24f1d079da
irina <irina.glushanok@gmail.com>
2016-05-06 18:51:07 +0200

    Panel contains VerticalSplitPanel so as to integrate the legend

--- annis-visualizers/src/main/java/annis/visualizers/component/visjs/VisJsPanel.java
+++ annis-visualizers/src/main/java/annis/visualizers/component/visjs/VisJsPanel.java
@@ -2,17 +2,25 @@ package annis.visualizers.component.visjs;
 
 import annis.libgui.visualizers.VisualizerInput;
 
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
+import com.vaadin.ui.VerticalSplitPanel;
 
 public class VisJsPanel extends Panel{
 	
 	VisJsPanel (VisualizerInput visInput){
-		this.setHeight("600px");
+		this.setHeight("700px");
 		this.setWidth("100%");
 		//this.setScrollLeft(10);
 		//this.setScrollTop(10);
+		
+		
+		VerticalSplitPanel vsplit = new VerticalSplitPanel();
 		VisJsComponent visjsComponent = new VisJsComponent(visInput);		
-		this.setContent(visjsComponent);
+		vsplit.setFirstComponent(visjsComponent);
+		vsplit.addComponent(new Label("Lower panel"));
+		vsplit.setSplitPosition((float) 90.0);
+		this.setContent(vsplit);
 	}
 
 }
