0ff47ccb18dfe54e1418c0639d6eaf34f0b263c9
Thomas Krause <thomaskrause@posteo.de>
2016-05-11 13:23:12 +0200

    hold state in object and make the constructor private

--- annis-interfaces/src/main/java/annis/TimelineReconstructor.java
+++ annis-interfaces/src/main/java/annis/TimelineReconstructor.java
@@ -1,17 +1,24 @@
 package annis;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
 import org.corpus_tools.salt.common.SDocumentGraph;
 import org.corpus_tools.salt.common.SOrderRelation;
 import org.corpus_tools.salt.common.SSpan;
 import org.corpus_tools.salt.common.SSpanningRelation;
+import org.corpus_tools.salt.common.SStructuredNode;
 import org.corpus_tools.salt.common.STextualDS;
 import org.corpus_tools.salt.common.STimeline;
 import org.corpus_tools.salt.common.SToken;
+import org.corpus_tools.salt.core.SAnnotation;
 import org.corpus_tools.salt.core.SRelation;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
-import com.google.common.collect.TreeMultimap;
 
 /**
  * Allows to reconstruct a proper {@link SDocumentGraph} with an {@link STimeline} and
@@ -20,11 +27,24 @@ import com.google.common.collect.TreeMultimap;
  * @author Thomas Krause <thomaskrause@posteo.de>
  *
  */
-public class TimelineReconstructor {
+public class TimelineReconstructor 
+{
   
-  private static Multimap<SSpan, Integer> addTimeline(SDocumentGraph graph)
+  private final SDocumentGraph graph;
+  
+  private final Map<String, STextualDS> textsByName = new HashMap<>();
+  private final Multimap<SStructuredNode, Integer> spans2TimelinePos = HashMultimap.create();
+  
+  private final Set<SStructuredNode> nodesToDelete = new HashSet<>();
+  
+  private TimelineReconstructor(SDocumentGraph graph)
   {
-    Multimap<SSpan, Integer> spans2TimelinePos = HashMultimap.create();
+    this.graph = graph;
+  }
+  
+  private void addTimeline()
+  {
+    
     STimeline timeline = graph.createTimeline();
     for(SToken virtualTok : graph.getSortedTokenByText())
     {
@@ -43,8 +63,60 @@ public class TimelineReconstructor {
         }
       }
     }
-    return spans2TimelinePos;
   }
+  
+  private void createTokenFromSOrder()
+  {    
+    for(SOrderRelation orderRel : graph.getOrderRelations())
+    {
+      SStructuredNode source = orderRel.getSource();
+      SStructuredNode target = orderRel.getTarget();
+      String orderName = orderRel.getType();
+      
+      convertSpanToToken(source, orderName);
+      convertSpanToToken(target, orderName);
+    }
+  }
+  
+  private SToken convertSpanToToken(SStructuredNode span, String orderName)
+  {
+    if(!nodesToDelete.contains(span))
+    {
+      nodesToDelete.add(span);
+      
+      if(textsByName.get(orderName) == null)
+      {
+        STextualDS newText = graph.createTextualDS("");
+        newText.setName(orderName);
+        textsByName.put(orderName, newText);
+      }
+      STextualDS textDS = textsByName.get(orderName);
+      TreeSet<Integer> coveredIdx = new TreeSet<>(spans2TimelinePos.get(span));
+      if(!coveredIdx.isEmpty())
+      {
+        SToken newToken = graph.createToken(textDS, coveredIdx.first(), coveredIdx.last());
+        SAnnotation textValueAnno = span.getAnnotation(null, orderName);
+        if(textValueAnno != null)
+        {
+          String textValue = textValueAnno.getValue_STEXT();
+          // TODO: Howto extends the existing text? we don't know the order of the sorder relation for sure.
+          return newToken;
+        }
+      }
+    }
+    
+    return null;
+  }
+  
+  private void cleanup()
+  {
+    for(SStructuredNode node : nodesToDelete)
+    {
+      graph.removeNode(node);
+    }
+  }
+  
+  
   
   /**
    * Removes the virtual tokenization from a {@link SDocumentGraph} and replaces it with an
@@ -58,9 +130,15 @@ public class TimelineReconstructor {
   {
     if(graph.getTimeline() != null)
     {
+      // do nothing if the graph does not contain any virtual tokenization
       return;
     }
     
-    Multimap<SSpan, Integer> spans2TimelinePos = addTimeline(graph);
+    TimelineReconstructor reconstructor = new TimelineReconstructor(graph);
+    reconstructor.addTimeline();
+    reconstructor.createTokenFromSOrder();
+    
+    reconstructor.cleanup();
+    
   }
 }
