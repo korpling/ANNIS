395b9f0c07ba2ff1d00a0fe0d9369fece76df1f0
irina <irina.glushanok@gmail.com>
2016-05-13 19:16:30 +0200

    added legend components

--- annis-visualizers/src/main/java/annis/visualizers/component/visjs/VisJsPanel.java
+++ annis-visualizers/src/main/java/annis/visualizers/component/visjs/VisJsPanel.java
@@ -1,10 +1,25 @@
 package annis.visualizers.component.visjs;
 
+import org.corpus_tools.salt.util.VisJsVisualizer;
+
 import annis.libgui.visualizers.VisualizerInput;
 
+import com.vaadin.annotations.StyleSheet;
+import com.vaadin.server.Page;
+import com.vaadin.server.Page.Styles;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalSplitPanel;
+
+
+@StyleSheet(
+		  {
+			  	"visjs_legend.css"
+			  	
+		  })
+
 
 public class VisJsPanel extends Panel{
 	
@@ -18,8 +33,43 @@ public class VisJsPanel extends Panel{
 		VerticalSplitPanel vsplit = new VerticalSplitPanel();
 		VisJsComponent visjsComponent = new VisJsComponent(visInput);		
 		vsplit.setFirstComponent(visjsComponent);
-		vsplit.addComponent(new Label("Lower panel"));
 		vsplit.setSplitPosition((float) 90.0);
+		HorizontalLayout hlayout = new HorizontalLayout();
+		hlayout.setHeight("100%");
+		hlayout.setWidth("100%");
+		//hlayout.setSizeUndefined();
+		hlayout.setSpacing(false);
+
+		TextField tfToken = new TextField();
+		tfToken.setValue("token");
+		tfToken.setReadOnly(true);		
+		tfToken.addStyleName("legend");
+		tfToken.addStyleName("legend-token");	
+		
+		TextField tfSpan = new TextField();
+		tfSpan.setValue("spanning\n node");
+		tfSpan.setReadOnly(true);		
+		tfSpan.addStyleName("legend");
+		tfSpan.addStyleName("legend-span");	
+		
+		TextField tfStruct = new TextField();
+		tfStruct.setValue("structure\n node");
+		tfStruct.setReadOnly(true);		
+		tfStruct.addStyleName("legend");
+		tfStruct.addStyleName("legend-struct");	
+		
+		
+		
+		hlayout.addComponent(tfToken);
+		hlayout.addComponent(tfSpan);
+		hlayout.addComponent(tfStruct);
+		
+		hlayout.setExpandRatio(tfToken, 0.2f);
+		hlayout.setExpandRatio(tfSpan, 0.2f);
+		hlayout.setExpandRatio(tfStruct, 0.6f);
+		
+		vsplit.addComponent(hlayout);
+		
 		this.setContent(vsplit);
 	}
 
