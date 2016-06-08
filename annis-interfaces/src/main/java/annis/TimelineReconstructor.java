bf9db7cc2bbdec143dcf8d8835ee2d4d3eea0d36
Thomas Krause <thomaskrause@posteo.de>
2016-06-08 10:30:34 +0200

    Adding code to create the text for a segmentation.

--- annis-interfaces/src/main/java/annis/TimelineReconstructor.java
+++ annis-interfaces/src/main/java/annis/TimelineReconstructor.java
@@ -2,10 +2,13 @@ package annis;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.corpus_tools.salt.SALT_TYPE;
+import org.corpus_tools.salt.SaltFactory;
 import org.corpus_tools.salt.common.SDocumentGraph;
 import org.corpus_tools.salt.common.SOrderRelation;
 import org.corpus_tools.salt.common.SSpan;
@@ -13,8 +16,12 @@ import org.corpus_tools.salt.common.SSpanningRelation;
 import org.corpus_tools.salt.common.SStructuredNode;
 import org.corpus_tools.salt.common.STextualDS;
 import org.corpus_tools.salt.common.STimeline;
+import org.corpus_tools.salt.common.STimelineRelation;
 import org.corpus_tools.salt.common.SToken;
+import org.corpus_tools.salt.core.GraphTraverseHandler;
 import org.corpus_tools.salt.core.SAnnotation;
+import org.corpus_tools.salt.core.SGraph.GRAPH_TRAVERSE_TYPE;
+import org.corpus_tools.salt.core.SNode;
 import org.corpus_tools.salt.core.SRelation;
 
 import com.google.common.collect.HashMultimap;
@@ -33,6 +40,7 @@ public class TimelineReconstructor
   private final SDocumentGraph graph;
   
   private final Map<String, STextualDS> textsByName = new HashMap<>();
+  private final Map<String, StringBuilder> textDataByName = new HashMap<>();
   private final Multimap<SStructuredNode, Integer> spans2TimelinePos = HashMultimap.create();
   
   private final Set<SStructuredNode> nodesToDelete = new HashSet<>();
@@ -67,14 +75,74 @@ public class TimelineReconstructor
   
   private void createTokenFromSOrder()
   {    
-    for(SOrderRelation orderRel : graph.getOrderRelations())
+    List<SNode> orderRelRoots = graph.getRootsByRelation(SALT_TYPE.SORDER_RELATION);
+    if(orderRelRoots != null) 
     {
-      SStructuredNode source = orderRel.getSource();
-      SStructuredNode target = orderRel.getTarget();
-      String orderName = orderRel.getType();
+      // convert all root nodes to span
+      for(SNode root : orderRelRoots)
+      {
+        if(root instanceof SSpan)
+        {
+          String orderName = null;
+          for(SRelation<?,?> outRel : root.getOutRelations())
+          {
+            if(outRel instanceof SOrderRelation)
+            {
+              orderName = ((SOrderRelation) outRel).getType();
+            }
+          }
+          if(orderName != null)
+          {
+            convertSpanToToken((SSpan) root, orderName);
+          }
+        }
+      }
       
-      convertSpanToToken(source, orderName);
-      convertSpanToToken(target, orderName);
+      // traverse through all SOrderRelations in order
+      graph.traverse(orderRelRoots, GRAPH_TRAVERSE_TYPE.TOP_DOWN_DEPTH_FIRST, "TimeReconstructSOrderRelations",
+          new GraphTraverseHandler()
+          {
+            
+            @Override
+            public void nodeReached(GRAPH_TRAVERSE_TYPE traversalType, String traversalId, SNode currNode,
+                SRelation relation, SNode fromNode, long order)
+            {
+              if(relation instanceof SOrderRelation && currNode instanceof SSpan)
+              {
+                convertSpanToToken((SSpan) currNode, ((SOrderRelation) relation).getType());
+              }
+            }
+            
+            @Override
+            public void nodeLeft(GRAPH_TRAVERSE_TYPE traversalType, String traversalId, SNode currNode,
+                SRelation relation, SNode fromNode, long order)
+            {              
+            }
+            
+            @Override
+            public boolean checkConstraint(GRAPH_TRAVERSE_TYPE traversalType, String traversalId,
+                SRelation relation, SNode currNode, long order)
+            {
+              if(relation == null || relation instanceof SOrderRelation)
+              {
+                return true;
+              }
+              else
+              {
+                return false;
+              }
+            }
+          });
+    }
+    
+    // update the text of the TextualDSs
+    for(Map.Entry<String, StringBuilder> textDataEntry : textDataByName.entrySet())
+    {
+      STextualDS textDS = textsByName.get(textDataEntry.getKey());
+      if(textDS != null)
+      {
+        textDS.setText(textDataEntry.getValue().toString());
+      }
     }
   }
   
@@ -89,17 +157,32 @@ public class TimelineReconstructor
         STextualDS newText = graph.createTextualDS("");
         newText.setName(orderName);
         textsByName.put(orderName, newText);
+        textDataByName.put(orderName, new StringBuilder());
       }
       STextualDS textDS = textsByName.get(orderName);
+      StringBuilder textData = textDataByName.get(orderName);
+      
       TreeSet<Integer> coveredIdx = new TreeSet<>(spans2TimelinePos.get(span));
       if(!coveredIdx.isEmpty())
       {
-        SToken newToken = graph.createToken(textDS, coveredIdx.first(), coveredIdx.last());
         SAnnotation textValueAnno = span.getAnnotation(null, orderName);
         if(textValueAnno != null)
         {
           String textValue = textValueAnno.getValue_STEXT();
-          // TODO: Howto extends the existing text? we don't know the order of the sorder relation for sure.
+          
+          int startTextIdx = textData.length();
+          textData.append(textValue);
+          int endTextIdx = textData.length();
+          SToken newToken = graph.createToken(textDS, startTextIdx, endTextIdx);
+          
+          STimelineRelation timeRel = SaltFactory.createSTimelineRelation();
+          timeRel.setSource(newToken);
+          timeRel.setTarget(graph.getTimeline());
+          timeRel.setStart(coveredIdx.first());
+          timeRel.setEnd(coveredIdx.last());
+          
+          graph.addRelation(timeRel);
+          
           return newToken;
         }
       }
