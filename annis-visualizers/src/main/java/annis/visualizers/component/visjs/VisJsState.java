da391f21017290a0bd1dc10e218ea6114ca3ba1d
irina <irina.glushanok@gmail.com>
2016-07-13 13:56:44 +0200

    removed unnecessary comment

--- annis-visualizers/src/main/java/annis/visualizers/component/visjs/VisJsState.java
+++ annis-visualizers/src/main/java/annis/visualizers/component/visjs/VisJsState.java
@@ -4,7 +4,8 @@ import com.vaadin.shared.ui.JavaScriptComponentState;
 
 public class VisJsState extends JavaScriptComponentState 
 {
-	//public String visId; 
+	private static final long serialVersionUID = 4279583317977888123L;
+	
 	public String strNodes;
 	public String strEdges;
 
