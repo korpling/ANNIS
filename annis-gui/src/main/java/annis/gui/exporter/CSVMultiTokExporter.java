e251573f66d0223b1ed96227eb8967c40e495f79
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2016-10-24 18:38:13 +0200

    Collect annotation names in first iteration

--- annis-gui/src/main/java/annis/gui/exporter/CSVMultiTokExporter.java
+++ annis-gui/src/main/java/annis/gui/exporter/CSVMultiTokExporter.java
@@ -5,17 +5,22 @@ import annis.libgui.Helper;
 import annis.model.AnnisConstants;
 import annis.model.Annotation;
 import annis.model.RelannisNodeFeature;
+import annis.service.objects.CorpusConfig;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.List;
 import java.util.Map;
 
 import annis.service.objects.SubgraphFilter;
+import com.google.common.eventbus.EventBus;
+import com.sun.jersey.api.client.WebResource;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 import org.apache.commons.lang3.StringUtils;
 import org.corpus_tools.salt.common.SDocumentGraph;
@@ -56,14 +61,51 @@ public class CSVMultiTokExporter extends SaltBasedExporter
   }
 
   private Set<String> metakeys;
-  private List<List<String>> annotationsForMatchedNodes;
+  private List<TreeSet<String>> annotationsForMatchedNodes;
+
+  private boolean firstIteration;
+
+  @Override
+  public Exception convertText(String queryAnnisQL, int contextLeft, int contextRight,
+    Set<String> corpora, List<String> keys, String argsAsString,
+    WebResource annisResource, Writer out, EventBus eventBus, Map<String, CorpusConfig> corpusConfigs)
+  {
+    this.firstIteration = true;
+    Writer nullWriter = new OutputStreamWriter(
+        new OutputStream()
+        {
+          @Override
+          public void write(int b)
+          {}
+        });
+    Exception ex = super.convertText(queryAnnisQL, contextLeft, contextRight, corpora,
+      keys, argsAsString, annisResource,
+      nullWriter,  // avoid empty line at the beginning
+      null,  // don't show export update during the first iteration
+      corpusConfigs);
+    if (ex != null)
+      return ex;
+    this.firstIteration = false;
+    return super.convertText(queryAnnisQL, contextLeft, contextRight, corpora,
+      keys, argsAsString, annisResource, out, eventBus, corpusConfigs);
+  }
 
   @Override
   public void convertText(SDocumentGraph graph, List<String> annoKeys,
     Map<String, String> args, int matchNumber,
     Writer out) throws IOException
   {
-    // first match - collect some data
+    if(this.firstIteration)
+      this.collectHeader(graph, annoKeys, args, matchNumber, out);
+    else
+      this.writeText(graph, annoKeys, args, matchNumber, out);
+  }
+
+  private void collectHeader(SDocumentGraph graph, List<String> annoKeys,
+    Map<String, String> args, int matchNumber,
+    Writer out) throws IOException
+  {
+    // first match
     if (matchNumber == -1)
     {
       // get list of metakeys to export
@@ -72,29 +114,32 @@ public class CSVMultiTokExporter extends SaltBasedExporter
       {
         metakeys.addAll(Arrays.asList(args.get("metakeys").split(",")));
       }
-
-      // get list of annotations for the matched nodes
-      // TODO should iterate over all matches to compute
+      // initialize list of annotations for the matched nodes
       annotationsForMatchedNodes = new ArrayList<>();
-      for(String matchid: graph
-        .getFeature(AnnisConstants.ANNIS_NS, AnnisConstants.FEAT_MATCHEDIDS)
-        .getValue_STEXT().split(","))
-      {
-        List<SAnnotation> annots = new ArrayList<>(graph.getNode(matchid).getAnnotations());
-        java.util.Collections.sort(annots, new Comparator<SAnnotation>() {
-          @Override
-          public int compare(SAnnotation a, SAnnotation b)
-          {
-            return a.getName().compareTo(b.getName());
-          }
-        });
-        List<String> annoNames = new ArrayList<>();
-        for (SAnnotation annot: annots) {
-          annoNames.add(annot.getNamespace() + "::" + annot.getName());
-        }
-        annotationsForMatchedNodes.add(annoNames);
+    }
+    // get list of annotations for the nodes in the current match
+    String[] matchIDs = graph
+      .getFeature(AnnisConstants.ANNIS_NS, AnnisConstants.FEAT_MATCHEDIDS)
+      .getValue_STEXT().split(",");
+    for (int i=0; i < matchIDs.length; i++)
+    {
+      if(i >= annotationsForMatchedNodes.size())
+        annotationsForMatchedNodes.add(i, new TreeSet<String>());
+      List<SAnnotation> annots = new ArrayList<>(graph.getNode(matchIDs[i]).getAnnotations());
+      Set<String> annoNames = annotationsForMatchedNodes.get(i);
+      for (SAnnotation annot: annots) {
+        annoNames.add(annot.getNamespace() + "::" + annot.getName());
       }
+    }
+  }
+  private void writeText(SDocumentGraph graph, List<String> annoKeys,
+    Map<String, String> args, int matchNumber,
+    Writer out) throws IOException
+  {
 
+    // first match
+    if (matchNumber == -1)
+    {
       // output header
       List<String> headerLine = new ArrayList<>();
       for(int i=0; i < annotationsForMatchedNodes.size(); i++)
