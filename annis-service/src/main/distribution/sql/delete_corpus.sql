fbf19df05ef5a0b5ed86826f22187b7156e717e2
Thomas Krause <thomaskrause@posteo.de>
2018-04-09 15:51:30 +0200

    Remove unussed SQL files

--- annis-service/src/main/distribution/sql/delete_corpus.sql
+++
@@ -1,9 +0,0 @@
----
---- delete entries from source tables
----
-
--- corpus
-DELETE FROM corpus child 
-USING corpus toplevel 
-WHERE toplevel."name" IN ( :names ) AND toplevel.top_level IS TRUE 
-AND toplevel.pre <= child.pre AND toplevel.post >= child.pre;
