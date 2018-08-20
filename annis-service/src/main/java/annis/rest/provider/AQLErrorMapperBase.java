844ffdeefde72ba9863b80696d8a8c0b5e28bd75
Thomas Krause <thomaskrause@posteo.de>
2018-08-20 15:44:19 +0200

    Allow multiple errors

--- annis-service/src/main/java/annis/rest/provider/AQLErrorMapperBase.java
+++ annis-service/src/main/java/annis/rest/provider/AQLErrorMapperBase.java
@@ -26,47 +26,50 @@ public abstract class AQLErrorMapperBase<T extends GraphANNISException> implemen
 
     @Override
     public Response toResponse(T exception) {
-        
-        String[] msgLines = exception.getMessage().split("\n");
+     
         List<AqlParseError> errors = new LinkedList<>();
+        String[] errorMessageBlocks = exception.getMessage().split("---+\n");
         
-        // TODO: split multiple error messages
-        
-        AqlParseError e = new AqlParseError();
-        if(msgLines.length > 0) {
-            StringBuilder sb = new StringBuilder(msgLines[0]);
-            // append the hints as well
-            for(int i=2; i < msgLines.length; i++) {
-                sb.append("\n");
-                sb.append(msgLines[i]);
+        for(String block : errorMessageBlocks) {
+            
+            String[] msgLines = block.split("\n");
+            
+            AqlParseError e = new AqlParseError();
+            if(msgLines.length > 0) {
+                StringBuilder sb = new StringBuilder(msgLines[0]);
+                // append the hints as well
+                for(int i=2; i < msgLines.length; i++) {
+                    sb.append("\n");
+                    sb.append(msgLines[i]);
+                }
+                e.setMessage(sb.toString());
             }
-            e.setMessage(sb.toString());
-        }
-        
-        if(msgLines.length > 1) {
-           // get the position information
-           Matcher m = locationPattern.matcher(msgLines[1]);
-           if(m.matches()) {
-               ParsedEntityLocation loc = new ParsedEntityLocation();
-               String startLine = m.group("startline");
-               String startCol = m.group("startcol");
-               String endLine = m.group("endline");
-               String endCol = m.group("endcol");
-               
-               loc.setStartLine(Integer.parseInt(startLine));
-               loc.setStartColumn(Integer.parseInt(startCol)-1);
-               
-               if(endLine != null &&  endCol != null) {
-                   loc.setEndLine(Integer.parseInt(endLine));
-                   loc.setEndColumn(Integer.parseInt(endCol));
-               } else {
-                   loc.setEndLine(loc.getStartLine());
-                   loc.setEndColumn(loc.getStartColumn());
+            
+            if(msgLines.length > 1) {
+               // get the position information
+               Matcher m = locationPattern.matcher(msgLines[1]);
+               if(m.matches()) {
+                   ParsedEntityLocation loc = new ParsedEntityLocation();
+                   String startLine = m.group("startline");
+                   String startCol = m.group("startcol");
+                   String endLine = m.group("endline");
+                   String endCol = m.group("endcol");
+                   
+                   loc.setStartLine(Integer.parseInt(startLine));
+                   loc.setStartColumn(Integer.parseInt(startCol)-1);
+                   
+                   if(endLine != null &&  endCol != null) {
+                       loc.setEndLine(Integer.parseInt(endLine));
+                       loc.setEndColumn(Integer.parseInt(endCol));
+                   } else {
+                       loc.setEndLine(loc.getStartLine());
+                       loc.setEndColumn(loc.getStartColumn());
+                   }
+                   e.setLocation(loc);
                }
-               e.setLocation(loc);
-           }
+            }
+            errors.add(e);
         }
-        errors.add(e);
     
         return Response.status(Response.Status.BAD_REQUEST)
                 .entity(new GenericEntity<List<AqlParseError>>(errors) {
