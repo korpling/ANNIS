9c7b70dc5b2460ba1166d166d78643abfd40f7d5
Thomas Krause <thomaskrause@posteo.de>
2018-08-20 17:53:18 +0200

    Adapt to new (simpler) format of the error messsages

--- annis-service/src/main/java/annis/rest/provider/AQLErrorMapperBase.java
+++ annis-service/src/main/java/annis/rest/provider/AQLErrorMapperBase.java
@@ -18,11 +18,13 @@ import annis.model.ParsedEntityLocation;
 public abstract class AQLErrorMapperBase<T extends GraphANNISException> implements ExceptionMapper<T> {
 
     private static final Pattern locationPattern = Pattern.compile(
-                "\\[(?<startline>[0-9]+):(?<startcol>[0-9]+)(-(?<endline>[0-9]+):(?<endcol>[0-9]+))?\\]");
+                "\\[(?<startline>[0-9]+):(?<startcol>[0-9]+)(-(?<endline>[0-9]+):(?<endcol>[0-9]+))?\\]( (?<msg>.+))?");
 
+    
     public AQLErrorMapperBase() {
         super();
     }
+    
 
     @Override
     public Response toResponse(T exception) {
@@ -35,39 +37,46 @@ public abstract class AQLErrorMapperBase<T extends GraphANNISException> implemen
             String[] msgLines = block.split("\n");
             
             AqlParseError e = new AqlParseError();
-            if(msgLines.length > 0) {
-                StringBuilder sb = new StringBuilder(msgLines[0]);
-                // append the hints as well
-                for(int i=2; i < msgLines.length; i++) {
-                    sb.append("\n");
-                    sb.append(msgLines[i]);
-                }
-                e.setMessage(sb.toString());
-            }
             
-            if(msgLines.length > 1) {
-               // get the position information
-               Matcher m = locationPattern.matcher(msgLines[1]);
-               if(m.matches()) {
-                   ParsedEntityLocation loc = new ParsedEntityLocation();
-                   String startLine = m.group("startline");
-                   String startCol = m.group("startcol");
-                   String endLine = m.group("endline");
-                   String endCol = m.group("endcol");
-                   
-                   loc.setStartLine(Integer.parseInt(startLine));
-                   loc.setStartColumn(Integer.parseInt(startCol));
-                   
-                   if(endLine != null &&  endCol != null) {
-                       loc.setEndLine(Integer.parseInt(endLine));
-                       loc.setEndColumn(Integer.parseInt(endCol));
-                   } else {
-                       loc.setEndLine(loc.getStartLine());
-                       loc.setEndColumn(loc.getStartColumn());
-                   }
-                   e.setLocation(loc);
-               }
-            }
+            StringBuilder msg = new StringBuilder();
+            
+            for(int i=0; i < msgLines.length; i++) {
+                if(i == 0) {
+                    // check for optional location
+                    Matcher m = locationPattern.matcher(msgLines[i]);
+                    if(m.matches()) {
+                        ParsedEntityLocation loc = new ParsedEntityLocation();
+                        String startLine = m.group("startline");
+                        String startCol = m.group("startcol");
+                        String endLine = m.group("endline");
+                        String endCol = m.group("endcol");
+                        
+                        loc.setStartLine(Integer.parseInt(startLine));
+                        loc.setStartColumn(Integer.parseInt(startCol));
+                        
+                        if(endLine != null &&  endCol != null) {
+                            loc.setEndLine(Integer.parseInt(endLine));
+                            loc.setEndColumn(Integer.parseInt(endCol));
+                        } else {
+                            loc.setEndLine(loc.getStartLine());
+                            loc.setEndColumn(loc.getStartColumn());
+                        }
+                        e.setLocation(loc);
+                        
+                        String msgPart = m.group("msg");
+                        if(msgPart != null) {
+                            msg.append(msgPart);
+                        }
+                    }
+                } else {
+                    // append whole line
+                    msg.append(msgLines[i]);
+                }
+                if(i < msgLines.length-1) {
+                    msg.append('\n');
+                }
+            } // end for each line
+            e.setMessage(msg.toString());
             errors.add(e);
         }
     
