c7f54ef1c2db87ace87ca54c99a9708af52c2fd4
Thomas Krause <thomaskrause@posteo.de>
2018-09-19 12:07:25 +0200

    update documentation for version 3.5.0-rc2 (doc-develop)
    Original revision is 8b8975e873.

--- doc-develop/classannis_1_1sqlgen_1_1_frequency_sql_generator.js
+++ doc-develop/classannis_1_1sqlgen_1_1_frequency_sql_generator.js
@@ -1,7 +1,7 @@
 var classannis_1_1sqlgen_1_1_frequency_sql_generator =
 [
     [ "extractData", "classannis_1_1sqlgen_1_1_frequency_sql_generator.html#a6c9ce1316bad604d60289bcb3e402153", null ],
-    [ "fromClause", "classannis_1_1sqlgen_1_1_frequency_sql_generator.html#a19ea7c5109d29b01ce6070d75bf5b074", null ],
+    [ "fromClause", "classannis_1_1sqlgen_1_1_frequency_sql_generator.html#a98ae2d686e4a2deef132eb513ce63acc", null ],
     [ "getSolutionSqlGenerator", "classannis_1_1sqlgen_1_1_frequency_sql_generator.html#a3d02e95d26e6e81f533186658155305a", null ],
     [ "groupByAttributes", "classannis_1_1sqlgen_1_1_frequency_sql_generator.html#ae8e99caa7f813e9c4db65e00f1988400", null ],
     [ "selectClause", "classannis_1_1sqlgen_1_1_frequency_sql_generator.html#a5f9a73bc404c55e7ab1b78d59e1f675f", null ],
