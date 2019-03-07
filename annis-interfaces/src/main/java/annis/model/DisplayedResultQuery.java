f7df716906d0d5cb4722adc969cf6e467f30d17f
Thomas Krause <thomaskrause@posteo.de>
2019-03-07 15:22:50 +0100

    Encode arguments only toCitatationFragment function, not before when creating the map

--- annis-interfaces/src/main/java/annis/model/DisplayedResultQuery.java
+++ annis-interfaces/src/main/java/annis/model/DisplayedResultQuery.java
@@ -16,7 +16,7 @@
 package annis.model;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.net.URLDecoder;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -25,6 +25,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.base.Joiner;
@@ -63,28 +64,35 @@ public class DisplayedResultQuery extends PagedResultQuery {
     public String toCitationFragment() throws UnsupportedEncodingException {
         Map<String, String> result = getCitationFragmentArguments();
         List<String> fragmentParts = new LinkedList<String>();
-        for(Map.Entry<String, String> e : result.entrySet()) {
-            fragmentParts.add(e.getKey() + "=" + e.getValue());
+        for (Map.Entry<String, String> e : result.entrySet()) {
+            String value;
+            // every name that starts with "_" is base64 encoded
+            if (e.getKey().startsWith("_")) {
+                value = new String(Base64.decodeBase64(e.getValue()), "UTF-8");
+            } else {
+                value = URLDecoder.decode(e.getValue(), "UTF-8");
+            }
+            fragmentParts.add(e.getKey() + "=" + value);
         }
 
         return StringUtils.join(fragmentParts, "&");
     }
-    
-    public Map<String, String> getCitationFragmentArguments() throws UnsupportedEncodingException {
+
+    public Map<String, String> getCitationFragmentArguments() {
         Map<String, String> result = new LinkedHashMap<>();
-        result.put("_q", CommonHelper.encodeBase64URL(getQuery()));
-        result.put("ql", URLEncoder.encode(getQueryLanguage().name().toLowerCase(), "UTF-8"));
-        result.put("_c", CommonHelper.encodeBase64URL(StringUtils.join(getCorpora(), ",")));
-        result.put("cl", URLEncoder.encode("" + getLeftContext(), "UTF-8"));
-        result.put("cr", URLEncoder.encode("" + getRightContext(), "UTF-8"));
-        result.put("s", URLEncoder.encode("" + getOffset(), "UTF-8"));
-        result.put("l", URLEncoder.encode("" + getLimit(), "UTF-8"));
+        result.put("_q", getQuery());
+        result.put("ql", getQueryLanguage().name().toLowerCase());
+        result.put("_c", StringUtils.join(getCorpora(), ","));
+        result.put("cl", "" + getLeftContext());
+        result.put("cr", "" + getRightContext());
+        result.put("s", "" + getOffset());
+        result.put("l", "" + getLimit());
         if (getSegmentation() != null) {
             result.put("_seg", CommonHelper.encodeBase64URL(getSegmentation()));
         }
         // only output "bt" if it is not the same as the context segmentation
         if (!Objects.equals(getBaseText(), getSegmentation())) {
-            result.put("_bt", (getBaseText() == null ? "" : CommonHelper.encodeBase64URL(getBaseText())));
+            result.put("_bt", (getBaseText() == null ? "" : getBaseText()));
         }
         if (getOrder() != OrderType.ascending && getOrder() != null) {
             result.put("o", getOrder().toString());
