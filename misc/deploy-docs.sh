73406cadbb9dc1a92b39061f5dc9f8a5ceb681e0
Thomas Krause <thomaskrause@posteo.de>
2019-08-08 16:49:47 +0200

    Add script and configuration to deploy documentation

---
+++ misc/deploy-docs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Used by CI to deploy the existing documentation/book/<version> directory to Github Pages
+
+if [ -n "$GITHUB_API_KEY" ]; then
+    cd "$TRAVIS_BUILD_DIR"
+
+    echo "cloning gh-pages"
+    git clone -q  -b gh-pages https://$GITHUB_API_KEY@github.com/{TRAVIS_REPO_SLUG} gh-pages &>/dev/null
+    cd gh-pages
+   	cp -R ${TRAVIS_BUILD_DIR}/docs/*/book/* .
+    git add .
+    git -c user.name='travis' -c user.email='travis' commit -m "update documentation for version ${SHORT_VERSION}"
+    echo "pushing to gh-pages"
+    git push -q https://$GITHUB_API_KEY@github.com/{TRAVIS_REPO_SLUG} gh-pages &>/dev/null
+    cd "$TRAVIS_BUILD_DIR"
+else
+	>&2 echo "Cannot deploy documentation because GITHUB_API_KEY environment variable is not set"
+	exit 1
+fi
