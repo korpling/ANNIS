53bdf0ee150509a11c42d74d73a1ce0ab6534014
Thomas Krause <thomaskrause@posteo.de>
2019-08-08 18:40:03 +0200

    Use subdirectory for each documentation

--- misc/deploy-docs.sh
+++ misc/deploy-docs.sh
@@ -8,8 +8,17 @@ if [ -n "$GITHUB_API_KEY" ]; then
     echo "cloning gh-pages from ${TRAVIS_REPO_SLUG}"
     git clone -q -b gh-pages https://$GITHUB_API_KEY@github.com/${TRAVIS_REPO_SLUG} gh-pages &>/dev/null
     cd gh-pages
-   	cp -R ${TRAVIS_BUILD_DIR}/docs/*/book/* .
-    git add .
+    # delete old files for this version and recreate target directories
+    rm -Rf ${SHORT_VERSION}
+    mkdir -p ${SHORT_VERSION}/user-guide
+    mkdir -p ${SHORT_VERSION}/developer-guide
+    
+    # Copy files
+   	cp -R ${TRAVIS_BUILD_DIR}/docs/user-guide/book/* ${SHORT_VERSION}/user-guide/
+   	cp -R ${TRAVIS_BUILD_DIR}/docs/developer-guide/book/* ${SHORT_VERSION}/developer-guide/
+   	
+   	# commit and push changes
+    git add ${SHORT_VERSION}
     git -c user.name='travis' -c user.email='travis' commit -m "update documentation for version ${SHORT_VERSION}"
     echo "pushing to gh-pages"
     git push -q https://$GITHUB_API_KEY@github.com/${TRAVIS_REPO_SLUG} gh-pages &>/dev/null
