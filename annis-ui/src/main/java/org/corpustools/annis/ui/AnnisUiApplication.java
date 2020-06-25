4e3a02e8798799e6b81dcf6778bece2ed266f0bb
Thomas Krause <krauseto@hu-berlin.de>
2020-06-25 11:04:48 +0200

    Add a new empty project for the ANNIS UI which is a Spring Boot project

---
+++ annis-ui/src/main/java/org/corpustools/annis/ui/AnnisUiApplication.java
@@ -0,0 +1,13 @@
+package org.corpustools.annis.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnisUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnisUiApplication.class, args);
+	}
+
+}
