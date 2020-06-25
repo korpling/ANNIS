160eb1e3df72331c91701e9b214aa401ac606622
Thomas Krause <krauseto@hu-berlin.de>
2020-06-25 11:04:48 +0200

    Add a new empty project for the ANNIS UI which is a Spring Boot project

---
+++ annis-ui/src/test/java/org/corpustools/annis/ui/AnnisUiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.corpustools.annis.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnisUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
