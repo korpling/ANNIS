b9a4f0fe210c65d43a6bf620999c456360d1cbd4
Thomas Krause <krauseto@hu-berlin.de>
2020-07-31 18:29:50 +0200

    Use (persistant) H2 database and make sure the redirect also works on initial request

---
+++ annis-gui/src/main/java/annis/gui/query_references/UrlShortener.java
@@ -0,0 +1,74 @@
+package annis.gui.query_references;
+
+import annis.libgui.AnnisUser;
+import annis.libgui.Helper;
+import com.vaadin.ui.UI;
+import java.net.URI;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import javax.transaction.Transactional;
+import javax.ws.rs.core.UriBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UrlShortener {
+
+  @Autowired
+  private UrlShortenerRepository repo;
+
+  @Transactional
+  public String shortenURL(URI original, UI ui) {
+    String appContext = Helper.getContext(ui);
+
+    String path = original.getRawPath();
+    if (path.startsWith(appContext)) {
+      path = path.substring(appContext.length());
+    }
+
+    String localURLRaw = path;
+    if (original.getRawQuery() != null) {
+      localURLRaw = localURLRaw + "?" + original.getRawQuery();
+    }
+    if (original.getRawFragment() != null) {
+      localURLRaw = localURLRaw + "#" + original.getRawFragment();
+    }
+
+    URI localURL = URI.create(localURLRaw);
+    UUID shortID;
+    // Check if this URI has already been shortened
+    List<UrlShortenerEntry> existingEntry = repo.findByUrl(localURL);
+    if (existingEntry.isEmpty()) {
+
+      UrlShortenerEntry entry = new UrlShortenerEntry();
+      entry.setUrl(localURL);
+      AnnisUser user = Helper.getUser(ui);
+      if (user == null) {
+        entry.setOwner("anonymous");
+      } else {
+        entry.setOwner(user.getUserName());
+      }
+      entry.setCreated(new Date());
+
+      UrlShortenerEntry savedEntry = repo.save(entry);
+      // The UUID should be generated when the entry is save
+      shortID = savedEntry.getId();
+    } else {
+      shortID = existingEntry.get(0).getId();
+    }
+    return UriBuilder.fromUri(original).replacePath(appContext + "/").replaceQuery("").fragment("")
+        .queryParam("id", shortID.toString()).build().toASCIIString();
+  }
+
+  public Optional<URI> unshorten(UUID id) {
+    return repo.findById(id).map(e -> {
+      if(e.getTemporaryUrl() == null) {
+        return e.getUrl();
+      } else {
+        return e.getTemporaryUrl();
+      }
+    });
+  }
+}
