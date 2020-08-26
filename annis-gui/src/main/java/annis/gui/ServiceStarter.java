30d790c44ba10e269fbbc537d111f967efa2431e
Thomas Krause <krauseto@hu-berlin.de>
2020-08-26 08:21:42 +0200

    Download all release binaries for the graphANNIS REST service and package as (executable) WAR

--- annis-gui/src/main/java/annis/gui/ServiceStarter.java
+++ annis-gui/src/main/java/annis/gui/ServiceStarter.java
@@ -18,8 +18,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.commons.io.FileUtils;
