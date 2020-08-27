676ae119dce4526db74886ee76940f273e3fb12b
Thomas Krause <krauseto@hu-berlin.de>
2020-08-27 16:39:51 +0200

    Add (empty) annis-kickstarter project that depends on annis-gui

---
+++ annis-kickstarter/src/test/java/de/huberlin/german/korpling/annis/AnnisKickstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.huberlin.german.korpling.annis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnisKickstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
