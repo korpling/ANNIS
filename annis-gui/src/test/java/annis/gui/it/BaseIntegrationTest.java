81c6642cbf4322c55110bb85113426c5170d4593
Thomas Krause <krauseto@hu-berlin.de>
2020-10-02 17:08:33 +0200

    Cleanup integrationt tests and force kill after timeout

--- annis-gui/src/test/java/annis/gui/it/BaseIntegrationTest.java
+++ annis-gui/src/test/java/annis/gui/it/BaseIntegrationTest.java
@@ -1,29 +1,28 @@
 package annis.gui.it;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseIntegrationTest {
-    protected WebDriver driver;
-    protected JavascriptExecutor js;
-    protected WebDriverWait wait;
+    protected static WebDriver driver;
+    protected static WebDriverWait wait;
 
 
-    @BeforeEach
-    public void setUp() {
+    @BeforeAll
+    public static void setUpClass() {
         driver = new FirefoxDriver();
-        js = (JavascriptExecutor) driver;
-        wait = new WebDriverWait(driver, 30);
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().window().setSize(new Dimension(1024, 768));
     }
 
-    @AfterEach
-    public void tearDown() {
+    @AfterAll
+    public static void tearDownClass() {
         driver.quit();
     }
 
