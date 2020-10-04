e5c717b0ff917e81619bba0a3383115b003f407a
Thomas Krause <thomaskrause@posteo.de>
2020-10-04 15:44:42 +0200

    Add tests for clearing the query builder and using a regular expression span

--- src/test/java/annis/gui/flatquerybuilder/FlatQueryBuilderTest.java
+++ src/test/java/annis/gui/flatquerybuilder/FlatQueryBuilderTest.java
@@ -12,6 +12,7 @@ import com.github.mvysny.kaributesting.v8.MockVaadin;
 import com.vaadin.spring.internal.UIScopeImpl;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.MenuBar;
+import com.vaadin.v7.ui.CheckBox;
 import com.vaadin.v7.ui.ComboBox;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
@@ -34,13 +35,21 @@ class FlatQueryBuilderTest {
   private BeanFactory beanFactory;
 
   private AnnisUI ui;
-
+  private FlatQueryBuilder queryBuilder;
   @BeforeEach
   public void setup() {
     UIScopeImpl.setBeanStoreRetrievalStrategy(new SingletonBeanStoreRetrievalStrategy());
     this.ui = beanFactory.getBean(AnnisUI.class);
     MockVaadin.setup(() -> this.ui);
 
+    ui.getQueryState().setSelectedCorpora(Sets.newSet("pcc2"));
+
+    // Click on the query builder button and select the flat query builder
+    _click(_get(Button.class, spec -> spec.withCaption("Query<br />Builder")));
+
+    ComboBox queryBuilderChooser = _get(_get(QueryBuilderChooser.class), ComboBox.class);
+    queryBuilderChooser.select("Word sequences and meta information");
+    this.queryBuilder = _get(FlatQueryBuilder.class);
   }
 
   @AfterEach
@@ -48,27 +57,21 @@ class FlatQueryBuilderTest {
     MockVaadin.tearDown();
   }
 
-  @Test
-  void flatQueryBuilderLinguisticSequence() throws InterruptedException {
-    ui.getQueryState().setSelectedCorpora(Sets.newSet("pcc2"));
-
-    // Click on the query builder button and select the flat query builder
-    _click(_get(Button.class, spec -> spec.withCaption("Query<br />Builder")));
-
-    ComboBox queryBuilderChooser =
-        _get(_get(QueryBuilderChooser.class), ComboBox.class);
-    queryBuilderChooser.select("Word sequences and meta information");
-
-    // Initialize the query builder
-    FlatQueryBuilder queryBuilder = _get(FlatQueryBuilder.class);
+  private void initQueryBuilder(int buttonIndex) {
     List<Button> initButtons =
         _find(queryBuilder, Button.class, spec -> spec.withCaption("Initialize"));
     assertEquals(3, initButtons.size());
-    _click(initButtons.get(0));
+    _click(initButtons.get(buttonIndex));
 
     // The button caption should have been changed
     List<MenuBar> addButtons = _find(queryBuilder, MenuBar.class);
     assertEquals(3, addButtons.size());
+  }
+
+  @Test
+  void twoTokenInSentence() throws InterruptedException {
+
+    initQueryBuilder(0);
 
     // Add a two tokens
     queryBuilder.addLinguisticSequenceBox("tok");
@@ -83,13 +86,47 @@ class FlatQueryBuilderTest {
     // Add a scope
     queryBuilder.addSpanBox("Sent");
     SpanBox spanBox = _get(queryBuilder, SpanBox.class);
-    _get(spanBox, ComboBox.class).setValue("s");;
+    _get(spanBox, ComboBox.class).setValue("s");
 
     // Create the AQL query
     _click(_get(queryBuilder, Button.class, spec -> spec.withCaption("Create AQL Query")));
 
     assertEquals("tok=/Feigenblatt/ & tok=/Die/\n& Sent = \"s\"\n& #1 . #2\n"
         + "& #3_i_#1\n" + "& #3_i_#2",
+        ui.getQueryState().getAql().getValue());
+  }
+
+  @Test
+  void clear() throws InterruptedException {
+    initQueryBuilder(1);
+
+    // Clear it and initialize again
+    _click(_get(queryBuilder, Button.class, spec -> spec.withCaption("Clear the Query Builder")));
+    initQueryBuilder(2);
+
+    // The button caption should have been changed
+    List<MenuBar> addButtons = _find(queryBuilder, MenuBar.class);
+    assertEquals(3, addButtons.size());
+  }
+
+  @Test
+  void regularExpressionScope() {
+    initQueryBuilder(0);
+
+    // Add a token
+    queryBuilder.addLinguisticSequenceBox("tok");
+    _get(queryBuilder, SearchBox.class).setValue("Feigenblatt");
+    // Add a scope and add a regular expression as value
+    queryBuilder.addSpanBox("Sent");
+    SpanBox spanBox = _get(queryBuilder, SpanBox.class);
+    // Set first to regex mode (so the value is not escaped)
+    _get(spanBox, CheckBox.class).setValue(true);
+    spanBox.setValue(".*");
+    
+    // Create the AQL query
+    _click(_get(queryBuilder, Button.class, spec -> spec.withCaption("Create AQL Query")));
+
+    assertEquals("tok=/Feigenblatt/\n& Sent = /.*/\n& #2_i_#1",
         ui.getQueryState().getAql().getValue());
   }
 }
