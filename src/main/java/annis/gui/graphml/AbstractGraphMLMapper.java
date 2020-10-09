ab3204eca2fe4e385b7d1081bdf08bc2dafe43fb
Thomas Krause <thomaskrause@posteo.de>
2020-10-09 15:12:38 +0200

    Use XMLInputFactory constants

--- src/main/java/annis/gui/graphml/AbstractGraphMLMapper.java
+++ src/main/java/annis/gui/graphml/AbstractGraphMLMapper.java
@@ -10,7 +10,6 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import javax.xml.XMLConstants;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -82,8 +81,7 @@ public abstract class AbstractGraphMLMapper {
 
   protected void execute(File inputFile) throws IOException, XMLStreamException {
     XMLInputFactory factory = XMLInputFactory.newInstance();
-    factory.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
-    factory.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+    factory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
     factory.setProperty(XMLInputFactory.IS_VALIDATING, false);
 
     // 1. pass, check which nodes have an outgoing edge of a certain types
