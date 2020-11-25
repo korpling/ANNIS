c14bd1df50a433abf18f6a902fa62597e167a5fb
Thomas Krause <thomaskrause@posteo.de>
2020-11-25 18:20:42 +0100

    Import corpora for tests using disk

--- misc/import-test-corpora.sh
+++ misc/import-test-corpora.sh
@@ -4,6 +4,4 @@ curl -L -o pcc2.zip http://corpus-tools.org/corpora/pcc2_relANNIS.zip
 curl -L -o dialog.zip https://corpus-tools.org/corpora/dialog.demo_relANNIS.zip
 curl -L -o aeschylus.zip https://corpus-tools.org/corpora/Aeschylus.Persae.L1-18_relAnnis.zip
 mkdir -p  $HOME/.annis/v4/
-annis -c "import pcc2.zip"  $HOME/.annis/v4/
-annis -c "import dialog.zip"  $HOME/.annis/v4/
-annis -c "import aeschylus.zip"  $HOME/.annis/v4/
+annis -c "use_disk on" -c "import pcc2.zip" -c "import dialog.zip"  -c "import aeschylus.zip"  $HOME/.annis/v4/
