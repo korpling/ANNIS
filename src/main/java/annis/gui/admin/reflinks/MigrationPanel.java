50f73544ac30532a55527677d4111a172582a164
Thomas Krause <thomaskrause@posteo.de>
2021-01-08 14:14:46 +0100

    Add more fields to the migration panel

--- src/main/java/annis/gui/admin/reflinks/MigrationPanel.java
+++ src/main/java/annis/gui/admin/reflinks/MigrationPanel.java
@@ -1,8 +1,11 @@
 package annis.gui.admin.reflinks;
 
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 
 public class MigrationPanel extends Panel {
 
@@ -17,12 +20,19 @@ public class MigrationPanel extends Panel {
     super.attach();
 
 
+
     TextField serviceUrl = new TextField("ANNIS service URL");
     TextField serviceUsername = new TextField("Username for ANNIS service");
     TextField servicePassword = new TextField("Password for ANNIS service");
+    CheckBox skipExisting = new CheckBox("Skip existing UUIDs");
 
 
-    FormLayout layout = new FormLayout(serviceUrl, serviceUsername, servicePassword);
+    FormLayout formLayout =
+        new FormLayout(serviceUrl, serviceUsername, servicePassword, skipExisting);
+
+    Button btMigrate = new Button("Start migration");
+
+    VerticalLayout layout = new VerticalLayout(formLayout, btMigrate);
     setContent(layout);
   }
 
