3e343cafa27b83070828326e6a43e6fd60df1bb9
Thomas Krause <thomaskrause@posteo.de>
2021-01-11 12:25:46 +0100

    Rename enum variants to match style

--- src/main/java/annis/gui/admin/reflinks/QueryStatus.java
+++ src/main/java/annis/gui/admin/reflinks/QueryStatus.java
@@ -1,5 +1,5 @@
 package annis.gui.admin.reflinks;
 
 public enum QueryStatus {
-  Ok, UUIDExists, UnknownCorpus, CountDiffers, MatchesDiffer, Timeout, EmptyCorpusList, ServerError, Failed
+  OK, UUID_EXISTS, UNKNOWN_CORPUS, COUNT_DIFFERS, MATCHES_DIFFER, TIMEOUT, EMPTY_CORPUS_LIST, SERVER_ERROR, FAILED
 }
