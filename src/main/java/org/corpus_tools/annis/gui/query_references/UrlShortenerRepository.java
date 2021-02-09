e5a2adacdd18a70ba1252f6ba022a54ba0802108
Thomas Krause <thomaskrause@posteo.de>
2021-02-09 18:19:01 +0100

    Use a data provider for the reference link entries

--- src/main/java/org/corpus_tools/annis/gui/query_references/UrlShortenerRepository.java
+++ src/main/java/org/corpus_tools/annis/gui/query_references/UrlShortenerRepository.java
@@ -3,9 +3,9 @@ package org.corpus_tools.annis.gui.query_references;
 import java.net.URI;
 import java.util.List;
 import java.util.UUID;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UrlShortenerRepository extends CrudRepository<UrlShortenerEntry, UUID> {
+public interface UrlShortenerRepository extends JpaRepository<UrlShortenerEntry, UUID> {
 
   public List<UrlShortenerEntry> findByUrl(URI url);
 
