b662230d854f97b4cb0e76d4c02a3406d131af18
Thomas Krause <thomaskrause@posteo.de>
2021-02-17 13:43:12 +0100

    Add simple test if URL shortener entries are shown

--- src/main/java/org/corpus_tools/annis/gui/query_references/UrlShortenerEntry.java
+++ src/main/java/org/corpus_tools/annis/gui/query_references/UrlShortenerEntry.java
@@ -65,4 +65,53 @@ public class UrlShortenerEntry {
   public void setTemporaryUrl(URI temporaryUrl) {
     this.temporaryUrl = temporaryUrl;
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((created == null) ? 0 : created.hashCode());
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+    result = prime * result + ((temporaryUrl == null) ? 0 : temporaryUrl.hashCode());
+    result = prime * result + ((url == null) ? 0 : url.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    UrlShortenerEntry other = (UrlShortenerEntry) obj;
+    if (created == null) {
+      if (other.created != null)
+        return false;
+    } else if (!created.equals(other.created))
+      return false;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    if (owner == null) {
+      if (other.owner != null)
+        return false;
+    } else if (!owner.equals(other.owner))
+      return false;
+    if (temporaryUrl == null) {
+      if (other.temporaryUrl != null)
+        return false;
+    } else if (!temporaryUrl.equals(other.temporaryUrl))
+      return false;
+    if (url == null) {
+      if (other.url != null)
+        return false;
+    } else if (!url.equals(other.url))
+      return false;
+    return true;
+  }
 }
