67887ba196f70c8ded48dc9603130568a533af7c
Thomas Krause <thomaskrause@posteo.de>
2021-02-17 22:49:40 +0100

    Replace assertFalse(e1.equals(e2)) with assertNotEquals().
    For the null comparision, the actual function call is used to avoid any shortcuts from JUnit

--- src/test/java/org/corpus_tools/annis/gui/query_references/UrlShortenerEntryTest.java
+++ src/test/java/org/corpus_tools/annis/gui/query_references/UrlShortenerEntryTest.java
@@ -2,6 +2,7 @@ package org.corpus_tools.annis.gui.query_references;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.net.URI;
 import java.util.Date;
@@ -16,34 +17,34 @@ class UrlShortenerEntryTest {
 
     assertEquals(e1, e1);
     assertFalse(e1.equals(null));
-    assertFalse(e1.equals("Someobject"));
+    assertNotEquals(e1, "Someobject");
 
     UrlShortenerEntry e2 = new UrlShortenerEntry();
 
     assertEquals(e1, e2);
 
     e1.setCreated(new Date());
-    assertFalse(e1.equals(e2));
+    assertNotEquals(e1, e2);
     e2.setCreated(e1.getCreated());
     assertEquals(e1, e2);
 
     e1.setId(UUID.randomUUID());
-    assertFalse(e1.equals(e2));
+    assertNotEquals(e1, e2);
     e2.setId(e1.getId());
     assertEquals(e1, e2);
 
     e1.setOwner("someone");
-    assertFalse(e1.equals(e2));
+    assertNotEquals(e1, e2);
     e2.setOwner(e1.getOwner());
     assertEquals(e1, e2);
 
     e1.setTemporaryUrl(URI.create("/"));
-    assertFalse(e1.equals(e2));
+    assertNotEquals(e1, e2);
     e2.setTemporaryUrl(e1.getTemporaryUrl());
     assertEquals(e1, e2);
 
     e1.setUrl(URI.create("/target"));
-    assertFalse(e1.equals(e2));
+    assertNotEquals(e1, e2);
     e2.setUrl(e1.getUrl());
     assertEquals(e1, e2);
 
