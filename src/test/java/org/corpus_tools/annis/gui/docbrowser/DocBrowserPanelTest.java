bae0ef387741b3a8e5cd395b056fcc69a18b042d
Thomas Krause <thomaskrause@posteo.de>
2021-06-14 12:16:41 +0200

    Add shenoute.a22 to test corpus set and do not reverse corpus path
    This corpus has a document_browser.json with explicit ordering and meta data columns defined.

--- src/test/java/org/corpus_tools/annis/gui/docbrowser/DocBrowserPanelTest.java
+++ src/test/java/org/corpus_tools/annis/gui/docbrowser/DocBrowserPanelTest.java
@@ -10,6 +10,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.github.mvysny.kaributesting.v8.MockVaadin;
 import com.vaadin.spring.internal.UIScopeImpl;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.corpus_tools.annis.gui.AnnisUI;
 import org.corpus_tools.annis.gui.SingletonBeanStoreRetrievalStrategy;
 import org.junit.jupiter.api.AfterEach;
@@ -45,9 +47,8 @@ class DocBrowserPanelTest {
     MockVaadin.tearDown();
   }
 
-  @Test
-  void testCorpusPathListed() throws Exception {
-    ui.getSearchView().getDocBrowserController().openDocBrowser("pcc2");
+  private void openDocBrowser(String corpusName) throws Exception {
+    ui.getSearchView().getDocBrowserController().openDocBrowser(corpusName);
     // Panel should be immediately loaded
     DocBrowserPanel panel = _get(DocBrowserPanel.class);
     assertNotNull(panel);
@@ -57,12 +58,38 @@ class DocBrowserPanelTest {
       return !_find(DocBrowserTable.class).isEmpty();
     }, () -> "Document browser table did not appear");
 
+  }
+
+  @Test
+  void testCorpusPathListed() throws Exception {
+
+    openDocBrowser("pcc2");
+
     DocBrowserTable table = _get(DocBrowserTable.class);
     Collection<?> ids = table.getItemIds();
     assertEquals(2, ids.size());
 
     assertTrue(ids.contains("pcc2 > 11299"));
     assertTrue(ids.contains("pcc2 > 4282"));
+  }
+
+  @Test
+  void testDocumentOrder() throws Exception {
+    openDocBrowser("shenoute.a22");
+
+    DocBrowserTable table = _get(DocBrowserTable.class);
+
+    // Test order of entries, which is explicitly given in the document_browser.json of the corpus
+    // (Ordering by title, ascending)
+    List<String> ids =
+        table.getItemIds().stream().map(o -> o.toString()).collect(Collectors.toList());
+
+    assertEquals(4, ids.size());
+
+    assertEquals("shenoute.a22 > a22.YA421-428", ids.get(0));
+    assertEquals("shenoute.a22 > a22.YA517-518", ids.get(1));
+    assertEquals("shenoute.a22 > a22.YB307-320", ids.get(2));
+    assertEquals("shenoute.a22 > a22.ZC301-308", ids.get(3));
   }
 
 }
