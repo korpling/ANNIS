a75db367a505f8173686f9825dad5e9ec7e35928
Thomas Krause <krauseto@hu-berlin.de>
2021-08-19 12:26:37 +0200

    Add repository annotation

--- src/main/java/org/corpus_tools/annis/gui/query_references/UrlShortenerRepository.java
+++ src/main/java/org/corpus_tools/annis/gui/query_references/UrlShortenerRepository.java
@@ -4,7 +4,9 @@ import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UrlShortenerRepository extends JpaRepository<UrlShortenerEntry, UUID> {
 
   public List<UrlShortenerEntry> findByUrl(URI url);
