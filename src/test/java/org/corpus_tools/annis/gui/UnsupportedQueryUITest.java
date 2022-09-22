a66b41f793214d0086b8d9ee96c28a2f4d83f5a0
Thomas Krause <thomaskrause@posteo.de>
2022-09-22 13:50:24 +0200

    Dirty context before executing UnsupportedQueryUITest

--- src/test/java/org/corpus_tools/annis/gui/UnsupportedQueryUITest.java
+++ src/test/java/org/corpus_tools/annis/gui/UnsupportedQueryUITest.java
@@ -10,19 +10,21 @@ import com.github.mvysny.kaributesting.v8.MockVaadin;
 import com.vaadin.server.Page;
 import com.vaadin.spring.internal.UIScopeImpl;
 import com.vaadin.ui.Button;
-import org.corpus_tools.annis.gui.UnsupportedQueryUI;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
 @ActiveProfiles({"desktop", "test", "headless"})
 @WebAppConfiguration
+@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
 class UnsupportedQueryUITest {
 
   private static final String TARGET_URL = "http://localhost:8080?q=somequery";
