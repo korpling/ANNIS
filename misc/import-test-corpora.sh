89b695ffc12e77b648ddc47da852cbbe184eef8a
Thomas Krause <thomas.krause@hu-berlin.de>
2024-03-26 10:32:32 +0100

    Make sure the test runner can find the annis binary to run for importing the corpora

--- misc/import-test-corpora.sh
+++ misc/import-test-corpora.sh
@@ -6,6 +6,7 @@ curl -L -o aeschylus.zip https://corpus-tools.org/corpora/Aeschylus.Persae.L1-18
 curl -L -o shenoute.zip https://corpus-tools.org/corpora/shenoute.a22_ANNIS.zip 
 curl -L -o parallelsample.zip https://corpus-tools.org/corpora/parallel.sample_relAnnis.zip
 mkdir -p  $HOME/.annis/v4/
+export PATH=$PATH:$HOME/.cargo/bin
 annis $HOME/.annis/v4/ -c "import pcc2.zip"
 annis $HOME/.annis/v4/ -c "import dialog.zip"
 annis $HOME/.annis/v4/ -c "import aeschylus.zip"
