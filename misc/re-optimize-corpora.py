07627090c7d10ed98c9e1bc776409a8f5335925d
Thomas Krause <thomas.krause@hu-berlin.de>
2024-05-28 16:05:46 +0200

    Run re-optimize CLI command for the given corpora when executing the script.

--- misc/re-optimize-corpora.py
+++ misc/re-optimize-corpora.py
@@ -6,11 +6,16 @@ import argparse
 parser = argparse.ArgumentParser(
                     prog='re-optimize-corpora',
                     description='Runs the ANNIS CLI to re-optimize a list of corpora')
-parser.add_argument("corpus_list")
-parser.add_argument("data_dir")
+parser.add_argument("corpus_list", help="Path to a CSV file containing all the corpora to re-optimize.")
+parser.add_argument("data_dir", help="The graphANNIS data directory where the corpora are stored.")
+parser.add_argument("--disk-based", action="store_true", help="If given, use the disk-mode to store the corpora.")
+parser.add_argument("--annis", default="annis", help="Path to the ANNIS executable.")
 
 args = parser.parse_args()
 
-print(args.data_dir)
+corpora = []
+with open(args.corpus_list, "r", encoding="UTF-8") as f:
+    corpora = f.read().splitlines()
 
-#subprocess.run(["ls", "-l"])
+for c in corpora:
+    subprocess.run([args.annis, args.data_dir, "-c", "corpus " + c ,  "-c", "re-optimize"])
